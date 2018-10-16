418e06a6c479617f39d39a271b1ed4b2d9dcbe76
Jiri Prajz <jiri.prajz@advantages.cz>
2018-10-16 14:02:25 +0200

    INTERNAL: New Project MD exporter

---
+++ Origam.MdExporter/Program.cs
@@ -0,0 +1,26 @@
+﻿using Origam.DA;
+using Origam.Schema;
+using Origam.Workbench.Services;
+using System;
+using System.Collections.Generic;
+
+namespace Origam.MdExporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var DefaultFolders = new List<ElementName>
+            {
+                ElementNameFactory.Create(typeof(SchemaExtension)),
+                ElementNameFactory.Create(typeof(SchemaItemGroup))
+            };
+
+            string testFolderPath = "";
+            var persistenceService = new FilePersistenceService(DefaultFolders,
+                testFolderPath);
+
+        }
+    }
+}
