b836c891c77fff1e1ef20a6e16ae629a2d616675
jindrichSusen <jindrich.susen@gmail.com>
2018-10-17 20:22:05 +0200

    INTERNAL: Unused project CommandBar removed (ref #3)

--- CommandBar/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-[assembly: System.CLSCompliant(true)]
-[assembly: System.Runtime.InteropServices.ComVisibleAttribute(false)]
-[assembly: System.Reflection.AssemblyVersion("2.1.0.0")]
-//[assembly: System.Reflection.AssemblyKeyFile("..\\..\\KeyPair.snk")]
