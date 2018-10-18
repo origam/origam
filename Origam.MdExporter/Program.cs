e678fa55059aa406067bb736fc2b307b936b3bca
Jiri Prajz <jiri.prajz@advantages.cz>
2018-10-18 17:32:58 +0200

    NEW: Menu Xml Exporter(ref #6)

--- Origam.MdExporter/Program.cs
+++ Origam.MdExporter/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Origam.DA;
+using Origam.DA.Service;
 using Origam.Schema;
+using Origam.Schema.MenuModel;
 using Origam.Workbench.Services;
 using System;
 using System.Collections.Generic;
@@ -8,19 +10,61 @@ namespace Origam.MdExporter
 {
     class Program
     {
+        private static string schemapath = "D:\\OrigamProjects\\test";
+        //private static string schemapath = "D:\\gitave\\be-model";
+        
+        private static string xmlpath = "D:\\prace";
+       
+       
         static void Main(string[] args)
         {
-
+            loadArgs(args);
+            
+            MenuSchemaItemProvider menuprovider = new MenuSchemaItemProvider();
+           
             var DefaultFolders = new List<ElementName>
             {
                 ElementNameFactory.Create(typeof(SchemaExtension)),
                 ElementNameFactory.Create(typeof(SchemaItemGroup))
             };
+            var settings =new OrigamSettings();
+            ConfigurationManager.SetActiveConfiguration(settings);
 
-            string testFolderPath = "";
+            
             var persistenceService = new FilePersistenceService(DefaultFolders,
-                testFolderPath);
+                schemapath);
+           FilePersistenceProvider persprovider = (FilePersistenceProvider)persistenceService.SchemaProvider;
+           // IDocumentationService documentation = ServiceManager.Services.GetService(typeof(IDocumentationService)) as IDocumentationService;
 
+           var documentation =  new FileStorageDocumentationService(
+                persprovider,
+                persistenceService.FileEventQueue);
+
+            menuprovider.PersistenceProvider = persprovider;
+            var menulist = menuprovider.ChildItems.ToList();
+            XmlCreate xmlfile = new XmlCreate(xmlpath, "testovacifile", documentation);
+            xmlfile.WriteElement("Root");
+            foreach (AbstractSchemaItem menuitem in menulist[0].ChildItems)
+            {
+                xmlfile.CreateXml(menuitem);
+                System.Console.WriteLine(menuitem.Name);
+
+            }
+           // xmlfile.WriteEndElement();
+            xmlfile.CloseXml();
+
+            Console.ReadKey();
+
+            //System.Console.WriteLine(consolestring);
+        }
+
+        private static void loadArgs(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                schemapath = args[0];
+                xmlpath = args[1];
+            }
         }
     }
 }
