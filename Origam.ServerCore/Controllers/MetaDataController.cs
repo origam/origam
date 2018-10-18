dc119f7dfe32d99485ad23ede7b90bcd9e14588a
jindrichSusen <jindrich.susen@gmail.com>
2018-10-18 16:07:56 +0200

    INTERNAL: New server project added

---
+++ Origam.ServerCore/Controllers/MetaDataController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Origam.OrigamEngine.ModelXmlBuilders;
+
+namespace Origam.ServerCore.Controllers
+{
+    [Route("api/[controller]")]
+   // [ApiController]
+    public class MetaDataController: ControllerBase
+    {
+        [HttpGet("[action]")]
+        public string GetMenu()
+        {
+            return MenuXmlBuilder.GetMenu();
+        }
+    }
+}
