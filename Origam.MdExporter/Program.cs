d50cf4dbb6dd74ae2eeb6f5117013fc341008150
Jiri Prajz <jiri.prajz@advantages.cz>
2018-10-22 16:50:43 +0200

    NEW: working on  generate xml tree.

--- Origam.MdExporter/Program.cs
+++ Origam.MdExporter/Program.cs
@@ -10,18 +10,12 @@ namespace Origam.MdExporter
 {
     class Program
     {
-        private static string schemapath = "D:\\OrigamProjects\\test";
-        //private static string schemapath = "D:\\gitave\\be-model";
-        
+        //private static string schemapath = "D:\\OrigamProjects\\test";
+        private static string schemapath = "D:\\OrigamProjects\\be-model";
         private static string xmlpath = "D:\\prace";
-       
-       
         static void Main(string[] args)
         {
-            loadArgs(args);
-            
-            MenuSchemaItemProvider menuprovider = new MenuSchemaItemProvider();
-           
+            LoadArgs(args);
             var DefaultFolders = new List<ElementName>
             {
                 ElementNameFactory.Create(typeof(SchemaExtension)),
@@ -29,36 +23,18 @@ namespace Origam.MdExporter
             };
             var settings =new OrigamSettings();
             ConfigurationManager.SetActiveConfiguration(settings);
-
-            
             var persistenceService = new FilePersistenceService(DefaultFolders,
                 schemapath);
            FilePersistenceProvider persprovider = (FilePersistenceProvider)persistenceService.SchemaProvider;
-           // IDocumentationService documentation = ServiceManager.Services.GetService(typeof(IDocumentationService)) as IDocumentationService;
-
            var documentation =  new FileStorageDocumentationService(
                 persprovider,
                 persistenceService.FileEventQueue);
 
-            menuprovider.PersistenceProvider = persprovider;
-            var menulist = menuprovider.ChildItems.ToList();
-            XmlCreate xmlfile = new XmlCreate(xmlpath, "testovacifile", documentation);
-            xmlfile.WriteElement("Root");
-            foreach (AbstractSchemaItem menuitem in menulist[0].ChildItems)
-            {
-                xmlfile.CreateXml(menuitem);
-                System.Console.WriteLine(menuitem.Name);
-
-            }
-           // xmlfile.WriteEndElement();
-            xmlfile.CloseXml();
-
-            Console.ReadKey();
-
-            //System.Console.WriteLine(consolestring);
+            XmlCreate xmlfile = new XmlCreate(xmlpath, "testovacifile.xml", documentation, persprovider);
+            xmlfile.Run();
         }
 
-        private static void loadArgs(string[] args)
+        private static void LoadArgs(string[] args)
         {
             if (args.Length == 2)
             {
