aeb0646f408a94c3ed64331f0071ab7a85a2de94
Jiri Prajz <jiri.prajz@advantages.cz>
2018-10-23 16:09:01 +0200

    NEW: CreateXml with documentation.

--- Origam.MdExporter/Program.cs
+++ Origam.MdExporter/Program.cs
@@ -6,6 +6,7 @@ using Origam.Schema.WorkflowModel;
 using Origam.Workbench.Services;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Principal;
 using System.Threading;
 
@@ -24,32 +25,30 @@ namespace Origam.MdExporter
                 ElementNameFactory.Create(typeof(SchemaExtension)),
                 ElementNameFactory.Create(typeof(SchemaItemGroup))
             };
-            //ServiceManager smanager = ServiceManager.Services;
-
             ServiceManager sManager = ServiceManager.Services;
             SchemaService service = new SchemaService();
             IParameterService parameterService = new NullParameterService();
                 
-            sManager.AddService(service);
-            sManager.AddService(parameterService);
+                sManager.AddService(service);
+                sManager.AddService(parameterService);
 
             var settings =new OrigamSettings();
             ConfigurationManager.SetActiveConfiguration(settings);
             Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity("origam_server"), null);
-
-            
             StateMachineSchemaItemProvider StateMachineSchema = new StateMachineSchemaItemProvider();
-            
-
             var persistenceService = new FilePersistenceService(DefaultFolders,
                schemapath);
             sManager.AddService(persistenceService);
-
             service.AddProvider(StateMachineSchema);
 
-            FilePersistenceProvider persprovider = (FilePersistenceProvider)persistenceService.SchemaProvider;
             
-
+            FilePersistenceProvider persprovider = (FilePersistenceProvider)persistenceService.SchemaProvider;
+            var independentPackage = persprovider
+                 .RetrieveList<SchemaExtension>()
+                 .FirstOrDefault(package => package.IncludedPackages.Count == 0)
+                 ?? throw new Exception("cannot find..");
+            //independentPackage.Id
+            persistenceService.LoadSchema(new Guid("0fe05b88-11e4-4f28-81bc-7762afa76dc8"), false, false, "");
             var documentation =  new FileStorageDocumentationService(
                 persprovider,
                 persistenceService.FileEventQueue);
