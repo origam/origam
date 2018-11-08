20200cc4d05b684f92ce4630bc0d47afac56758b
jindrichSusen <jindrich.susen@gmail.com>
2018-11-08 09:57:54 +0100

    INTERNAL: NullPersistenceProvider and NullPersistenceService added for testing purposes

---
+++ Origam.Workbench.Services/NullPersistenceProvider.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using Origam.DA;
+using Origam.DA.ObjectPersistence;
+
+namespace Origam.Workbench.Services
+{
+    public class NullPersistenceProvider : IPersistenceProvider
+    {
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public event EventHandler InstancePersisted;
+        public void OnInstancePersisted(object sender)
+        {
+        }
+
+        public ICompiledModel CompiledModel { get; set; }
+        public object RetrieveInstance(Type type, Key primaryKey)
+        {
+            return null;
+        }
+
+        public object RetrieveInstance(Type type, Key primaryKey, bool useCache)
+        {
+            return null;
+        }
+
+        public object RetrieveInstance(Type type, Key primaryKey, bool useCache, bool throwNotFoundException)
+        {
+            return null;
+        }
+
+        public void RefreshInstance(IPersistent persistentObject)
+        {
+        }
+
+        public void RemoveFromCache(IPersistent instance)
+        {
+        }
+
+        public List<T> RetrieveList<T>(IDictionary<string, object> filter = null)
+        {
+            return new System.Collections.Generic.List<T>();
+        }
+
+        public List<T> RetrieveListByType<T>(string itemType)
+        {
+            return new System.Collections.Generic.List<T>();
+        }
+
+        public List<T> RetrieveListByPackage<T>(Guid packageId)
+        {
+            return new System.Collections.Generic.List<T>();
+        }
+
+        public List<T> FullTextSearch<T>(string text)
+        {
+            return new System.Collections.Generic.List<T>();
+        }
+
+        public List<T> RetrieveListByParent<T>(Key primaryKey, string parentTableName, string childTableName, bool useCache)
+        {
+            return new System.Collections.Generic.List<T>();
+        }
+
+        public List<T> RetrieveListByGroup<T>(Key primaryKey)
+        {
+            return new System.Collections.Generic.List<T>();
+        }
+
+        public void Persist(IPersistent obj)
+        { 
+        }
+
+        public void FlushCache()
+        {
+        }
+
+        public void DeletePackage(Guid packageId)
+        {
+        }
+
+        public void BeginTransaction()
+        {
+        }
+
+        public void EndTransaction()
+        {
+        }
+
+        public object RetrieveValue(Guid instanceId, Type parentType, string fieldName)
+        {
+            return null;
+        }
+
+        public void RestrictToLoadedPackage(bool b)
+        {
+        }
+
+        public ILocalizationCache LocalizationCache { get; }
+    }
+}
---
+++ Origam.Workbench.Services/NullPersistenceService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+using Origam.DA.ObjectPersistence;
+using Origam.Schema;
+
+namespace Origam.Workbench.Services
+{
+    public class NullPersistenceService : IPersistenceService
+    {
+        public void InitializeService()
+        {
+        }
+
+        public void UnloadService()
+        {
+        }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPersistenceProvider SchemaProvider => new NullPersistenceProvider();
+        public IPersistenceProvider SchemaListProvider => new NullPersistenceProvider();
+
+        public void LoadSchema(ArrayList extensions, bool append, bool loadDocumentation, bool loadDeploymentScripts,
+            string transactionId)
+        {
+        }
+
+        public SchemaExtension LoadSchema(Guid schemaExtension, bool loadDocumentation, bool loadDeploymentScripts,
+            string transactionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SchemaExtension LoadSchema(Guid schemaExtensionId, Guid extraExtensionId, bool loadDocumentation,
+            bool loadDeploymentScripts, string transactionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadSchemaList()
+        {
+        }
+
+        public void UpdateRepository()
+        {
+        }
+
+        public bool IsRepositoryVersionCompatible()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanUpdateRepository()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExportPackage(Guid extensionId, string fileName)
+        {
+        }
+
+        public void MergePackage(Guid extensionId, DataSet data, string transcationId)
+        {
+        }
+
+        public void MergeSchema(DataSet schema, Key activePackage)
+        {
+        }
+
+        public void InitializeRepository()
+        {
+        }
+    }
+}
