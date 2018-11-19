ff104287a1112c0f509a7e01e17450196b861209
jindrichSusen <jindrich.susen@gmail.com>
2018-11-19 16:36:19 +0100

    INTERNAL: Added a call to Control.Inoke when opening a MessageBox. All Calls to Control.Inoke moved to extension methods.

--- OrigamArchitect/ArchitectWorkbench.cs
+++ OrigamArchitect/ArchitectWorkbench.cs
@@ -1301,7 +1301,7 @@ namespace OrigamArchitect
 			}
 		}
 
-		void OnFilePersistServiceReloadRequested(object sender, ChangedFileEventArgs args)
+		void OnFilePersistServiceReloadRequested(object sender, FileSystemChangeEventArgs args)
 		{
 			FilePersistenceService filePersistenceService =
 				(FilePersistenceService) sender;
@@ -1310,15 +1310,7 @@ namespace OrigamArchitect
             bool reloadConfirmed = true;
             if (ShouldRaiseWarning(filePersistenceProvider, args.File))
             {
-                if (InvokeRequired)
-                {
-                    Invoke(new Action(() 
-                        => reloadConfirmed = ConfirmReload(args)));
-                } else
-                {
-                    reloadConfirmed = ConfirmReload(args);
-                }
-
+                reloadConfirmed = this.RunWithInvoke(() => ConfirmReload(args));
             }
             if (reloadConfirmed)
             {
@@ -1326,22 +1318,11 @@ namespace OrigamArchitect
                 {
                     return;
                 }
-                if (InvokeRequired)
-                {
-                    Task.Run(() => {
-                        Invoke(new Action(()
-                            => UpdateUIAfterReload(filePersistenceProvider, args)));
-                    });
-                }
-                else
-                {
-                    UpdateUIAfterReload(filePersistenceProvider, args);
-                }
-
+                this.RunWithInvokeAsync(() => UpdateUIAfterReload(filePersistenceProvider, args));
             }
 		}
 
-        private bool ConfirmReload(ChangedFileEventArgs args)
+	    private bool ConfirmReload(FileSystemChangeEventArgs args)
         {
             // try/catch block for unhandled exceptions 
             // main try/catch block doesn't show error message 
@@ -1362,7 +1343,7 @@ namespace OrigamArchitect
 
         private void UpdateUIAfterReload(
 	        FilePersistenceProvider filePersistenceProvider, 
-            ChangedFileEventArgs args)
+            FileSystemChangeEventArgs args)
         {
             // try/catch block for unhandled exceptions 
             // main try/catch block doesn't show error message 
@@ -1405,8 +1386,8 @@ namespace OrigamArchitect
 			switch (error.Type)	
 			{
 				case ErrType.XmlGeneralError:
-					MessageBox.Show(this, error.Message);
-					return false;
+				    this.RunWithInvoke(() => MessageBox.Show(this, error.Message));
+                    return false;
 				case ErrType.XmlVersionIsOlderThanCurrent:
 					return TryHandleOldVersionFound(filePersistService, error.Message);
 				default:
