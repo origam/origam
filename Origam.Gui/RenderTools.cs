11077443a99b4baa64060c4abd9ae146e8305a44
Zuzana Capkova <capovi@tiscali.cz>
2018-12-19 08:34:25 +0100

    CHG: renamed Panel to ScreenSection in Action Buttons attributes (ref #58)

--- Origam.Gui/RenderTools.cs
+++ Origam.Gui/RenderTools.cs
@@ -47,7 +47,7 @@ namespace Origam.Gui
 			{
 				return ShouldRender(action.Features, action.Roles) 
 					&& (action.ScreenId.Equals(Guid.Empty) || action.ScreenId.Equals(formId))
-					&& (action.PanelId.Equals(Guid.Empty) || action.PanelId.Equals(panelId));
+					&& (action.ScreenSectionId.Equals(Guid.Empty) || action.ScreenSectionId.Equals(panelId));
 			}
 		}
 
