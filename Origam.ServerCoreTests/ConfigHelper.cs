a39da448c71db7106732cccdb59a6e2b427cabc8
jindrichSusen <jindrich.susen@gmail.com>
2019-02-07 16:56:36 +0100

    INTERNAL: Origam.ServerCoreTests test database login and other confurables moved to configuration

---
+++ Origam.ServerCoreTests/ConfigHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace Origam.ServerCoreTests
+{
+    class ConfigHelper
+    {
+        public static IConfigurationRoot GetIConfigurationRoot(string outputPath)
+        {
+            var secretsIdConfig = new ConfigurationBuilder()
+                .SetBasePath(outputPath)
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+            IConfigurationSection configurationSection = secretsIdConfig.GetSection("UserSecretsId");
+
+            var userSecretsId = configurationSection.Value;
+            return new ConfigurationBuilder()
+                .SetBasePath(outputPath)
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddUserSecrets(userSecretsId)
+                //.AddEnvironmentVariables()
+                .Build();
+        }
+
+        public static ServerCoreTestConfiguration GetApplicationConfiguration(string outputPath)
+        {
+            var configuration = new ServerCoreTestConfiguration();
+
+            var iConfig = GetIConfigurationRoot(outputPath);
+
+            iConfig
+                .GetSection("ServerCoreTestConfiguration")
+                .Bind(configuration);
+
+            return configuration;
+        }
+    }
+
+    public class ServerCoreTestConfiguration
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string TestDbName { get; set; }
+        public string PathToBakFile { get; set; }
+    }
+}
--- Origam.ServerCoreTests/TestsSetupClass.cs
+++ Origam.ServerCoreTests/TestsSetupClass.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Primitives;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
 using NUnit.Framework;
 using Origam.ServerCore.Controllers;
 
@@ -13,13 +16,33 @@ namespace Origam.ServerCoreTests
         [OneTimeSetUp]
         public void GlobalSetup()
         {
-            Origam.OrigamEngine.OrigamEngine.ConnectRuntime();
+            var configuration = ConfigHelper.GetApplicationConfiguration(TestContext.CurrentContext.TestDirectory);
+            RestoreDatabase(
+                databaseName: configuration.TestDbName,
+                backUpFile: configuration.PathToBakFile, 
+                serverName: ".", 
+                userName: configuration.UserName, 
+                password: configuration.Password);
+            OrigamEngine.OrigamEngine.ConnectRuntime();
         }
 
         [OneTimeTearDown]
         public void GlobalTeardown()
         {
 
+        }
+
+        private void RestoreDatabase(String databaseName, String backUpFile, String serverName, String userName, String password)
+        {
+            ServerConnection connection = new ServerConnection(serverName, userName, password);
+            Microsoft.SqlServer.Management.Smo.Server sqlServer = new Microsoft.SqlServer.Management.Smo.Server(connection);
+            Restore rstDatabase = new Restore();
+            rstDatabase.Action = RestoreActionType.Database;
+            rstDatabase.Database = databaseName;
+            BackupDeviceItem bkpDevice = new BackupDeviceItem(backUpFile, DeviceType.File);
+            rstDatabase.Devices.Add(bkpDevice);
+            rstDatabase.ReplaceDatabase = true;
+            rstDatabase.SqlRestore(sqlServer);
         }
     }
 }
