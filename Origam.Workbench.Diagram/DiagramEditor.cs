d3c983b1a39e6ac46e9c5ce474225680e284c10a
Tomáš Vavrda <tomas.vavrda@advantages.cz>
2019-02-18 16:30:30 +0100

    INTERNAL: Migrated NuGet to PackageReferences

--- Origam.Workbench.Diagram/DiagramEditor.cs
+++ Origam.Workbench.Diagram/DiagramEditor.cs
@@ -113,8 +113,6 @@ namespace Origam.Workbench.Editors
             this.gViewer1.UndoRedoButtonsVisible = true;
             this.gViewer1.WindowZoomButtonPressed = false;
             this.gViewer1.ZoomF = 1D;
-            this.gViewer1.ZoomFraction = 0.5D;
-            this.gViewer1.ZoomWhenMouseWheelScroll = true;
             this.gViewer1.ZoomWindowThreshold = 0.05D;
             // 
             // DiagramEditor
