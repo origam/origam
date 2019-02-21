c1fbcec661959da0a376fd1beb37c8ff8e2baa23
jindrichSusen <jindrich.susen@gmail.com>
2019-02-21 10:14:28 +0100

    INTERNAL: added MiddleWare to to set Thread.CurrentPrincipal

---
+++ Origam.ServerCore/SetCurrentPrincipalMiddleWare.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Origam.ServerCore
+{
+    public class SetCurrentPrincipalMiddleWare
+    {
+        private readonly RequestDelegate next;
+
+        public SetCurrentPrincipalMiddleWare(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Thread.CurrentPrincipal = context.User;
+            await next(context);
+        }
+    }
+}
