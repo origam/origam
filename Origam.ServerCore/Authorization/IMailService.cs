103eaa3b141b44edf911029d09064c837ed14ce5
jindrichSusen <jindrich.susen@gmail.com>
2019-03-11 10:12:02 +0100

    INTERNAL: MailMan class removed

--- Origam.ServerCore/Authorization/IMailService.cs
+++ Origam.ServerCore/Authorization/IMailService.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Linq;
 using System.Net.Mail;
 using System.Threading.Tasks;
+using Origam.DA;
 using Origam.Security.Common;
+using Origam.Workbench.Services.CoreServices;
 
 namespace Origam.ServerCore
 {
@@ -13,8 +17,17 @@ namespace Origam.ServerCore
     {
         public void Send(MailMessage email)
         {
-            var mailMan = new MailMan("","");
-            mailMan.SendMailByAWorkflow(email);
+            // send mail - by a workflow located at root package			
+            QueryParameterCollection pms = new QueryParameterCollection();
+            pms.Add(new QueryParameter("subject", email.Subject));
+            pms.Add(new QueryParameter("body", email.Body));
+            pms.Add(new QueryParameter("recipientEmail", email.To.First().Address));
+            pms.Add(new QueryParameter("senderEmail", email.From.Address));
+            if (!string.IsNullOrWhiteSpace(email.From.DisplayName))
+            {
+                pms.Add(new QueryParameter("senderName", email.From.DisplayName));
+            }
+            WorkflowService.ExecuteWorkflow(new Guid("6e6d4e02-812a-4c95-afd1-eb2428802e2b"), pms, null);
         }
     }
 }
