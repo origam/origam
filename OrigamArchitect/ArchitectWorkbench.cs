b4b29f1b4bfa7f5bcd4ff013b53b022471f281fa
jindrichSusen <jindrich.susen@gmail.com>
2019-03-13 12:53:36 +0100

    FIX: New elements created from the navigation bar open in an undocked window (ref. #78)

--- OrigamArchitect/ArchitectWorkbench.cs
+++ OrigamArchitect/ArchitectWorkbench.cs
@@ -1108,7 +1108,7 @@ namespace OrigamArchitect
 			if (ActiveDocument is AbstractEditor editor)
             {
                 editor.ActionsBuilder = new SchemaActionsMenuBuilder();
-                editor.NewElementsBuilder = new SchemaItemEditorsMenuBuilder(true);
+                editor.NewElementsBuilder = new SchemaItemEditorsMenuBuilder(showDialog: false);
 			}
 			UpdateToolStrips();
         }
