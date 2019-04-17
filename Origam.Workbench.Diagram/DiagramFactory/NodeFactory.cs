bd4ee13fe83253eeaff8b7e14bb04b7e84efdc61
jindrichSusen <jindrich.susen@gmail.com>
2019-04-17 10:08:44 +0200

    INTERNAL: DiagramEditor nodes have grey background color, fixed height and bigger margin around label

--- Origam.Workbench.Diagram/DiagramFactory/NodeFactory.cs
+++ Origam.Workbench.Diagram/DiagramFactory/NodeFactory.cs
@@ -13,20 +13,21 @@ namespace Origam.Workbench.Diagram.DiagramFactory
     class NodeFactory: IDisposable
     {
         private readonly int margin = 3;
-        private readonly int marginLeft = 5;
+        private readonly int textSideMargin = 15;
         private readonly Font font = new Font("Arial", 12);
         private readonly SolidBrush drawBrush = new SolidBrush(System.Drawing.Color.Black);
         private readonly StringFormat drawFormat = new StringFormat();
         private readonly Graphics measurementGraphics = new Control().CreateGraphics();
         private readonly Pen boldBlackPen = new Pen(System.Drawing.Color.Black, 2);
         private readonly Pen blackPen =new Pen(System.Drawing.Color.Black, 1);
+        private readonly SolidBrush greyBrush = new SolidBrush(System.Drawing.Color.LightGray);
+        private readonly int nodeHeight = 30;
         private readonly GViewer viewer;
 
         public NodeFactory(GViewer viewer)
         {
             this.viewer = viewer;
         }
-
 
         public Node AddNode(Graph graph, ISchemaItem schemaItem)
         {
@@ -40,8 +41,7 @@ namespace Origam.Workbench.Diagram.DiagramFactory
         }
  
         private ICurve GetNodeBoundary(Node node) {
-            var image = GetImage(node);
-            var borderSize = CalculateBorderSize(node, image);
+            var borderSize = CalculateBorderSize(node);
             return CurveFactory.CreateRectangle(borderSize.Width, borderSize.Height, new Point());
         }
 
@@ -55,26 +55,29 @@ namespace Origam.Workbench.Diagram.DiagramFactory
             
             SizeF stringSize = editorGraphics.MeasureString(node.LabelText, font);
 
-            var borderSize = CalculateBorderSize(node, image);
+            var borderSize = CalculateBorderSize(node);
             var borderCorner = new System.Drawing.Point(
                 (int)node.GeometryNode.Center.X - borderSize.Width / 2,
                 (int)node.GeometryNode.Center.Y - borderSize.Height / 2);
             Rectangle border = new Rectangle(borderCorner, borderSize);
+            Rectangle imageBackground = new Rectangle(borderCorner, new Size(nodeHeight, nodeHeight));
 
             var labelPoint = new PointF(
-                (float)node.GeometryNode.Center.X - (float)border.Width / 2 + image.Width + margin * 2,
-                (float)node.GeometryNode.Center.Y - stringSize.Height / 2);
+                (float)node.GeometryNode.Center.X - (float)border.Width / 2 + nodeHeight + margin  + textSideMargin,
+                (float)node.GeometryNode.Center.Y - (int)stringSize.Height / 2);
 
+            var imageHorizontalBorder = (imageBackground.Width - image.Width) / 2;
+            var imageVerticalBorder = (imageBackground.Height - image.Height) / 2;
             var imagePoint = new PointF(
-                (float)(node.GeometryNode.Center.X - (float)border.Width / 2 + marginLeft),
-                (float)(node.GeometryNode.Center.Y - (float)image.Height / 2));
+                (float)(node.GeometryNode.Center.X - (float)border.Width / 2 + imageHorizontalBorder),
+                (float)(node.GeometryNode.Center.Y - (float)border.Height / 2 + imageVerticalBorder));
 
             editorGraphics.DrawUpSideDown(drawAction: graphics =>
-                {
-                    graphics.DrawString(node.LabelText, font, drawBrush,
-                        labelPoint, drawFormat);
-                    graphics.DrawRectangle(pen, border);
+                {                   
+                    graphics.DrawString(node.LabelText, font, drawBrush, labelPoint, drawFormat);
+                    graphics.FillRectangle(greyBrush, imageBackground);
                     graphics.DrawImage(image, imagePoint);
+                    graphics.DrawRectangle(pen, border);
                 }, 
                 yAxisCoordinate: (float)node.GeometryNode.Center.Y);
             
@@ -92,16 +95,14 @@ namespace Origam.Workbench.Diagram.DiagramFactory
             return image;
         }
 
-        private Size CalculateBorderSize(Node node, Image image)
+        private Size CalculateBorderSize(Node node)
         {
             SizeF stringSize = measurementGraphics.MeasureString(node.LabelText, font);
 
-            int totalWidth = (int) (margin + stringSize.Width + margin + image.Width + margin);
-            int totalHeight = stringSize.Height > image.Height
-                ? (int) stringSize.Height + margin * 2
-                : image.Height + margin * 2;
-            return  new Size(totalWidth, totalHeight);
+            int totalWidth = (int) (margin + nodeHeight + textSideMargin + stringSize.Width + textSideMargin);
+            return  new Size(totalWidth, nodeHeight);
         }
+       
 
         public void Dispose()
         {
