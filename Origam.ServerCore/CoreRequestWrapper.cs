36a7da7bc953be61ae586224cd07c6bf0f76fb23
Petr Hrehorovsky <washi@volny.cz>
2019-09-04 10:40:25 +0200

    INTERNAL: Addded license info.

--- Origam.ServerCore/CoreRequestWrapper.cs
+++ Origam.ServerCore/CoreRequestWrapper.cs
@@ -1,13 +1,32 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿#region license
+/*
+Copyright 2005 - 2019 Advantage Solutions, s. r. o.
+
+This file is part of ORIGAM (http://www.origam.org).
+
+ORIGAM is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ORIGAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using Microsoft.AspNetCore.Http;
 using Microsoft.Net.Http.Headers;
 using Origam.ServerCommon.Pages;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Origam.ServerCore
 {
