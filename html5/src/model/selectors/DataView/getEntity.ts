ca3fc45e3214dc11f75c3ccf6009a9cc24a92c55
Pavel Tomasko <ptomask@gmail.com>
2019-09-25 11:29:38 +0200

    INTERNAL: Fixed DataSource and Entity querying.

--- html5/src/model/selectors/DataView/getEntity.ts
+++ html5/src/model/selectors/DataView/getEntity.ts
@@ -1,5 +1,6 @@
 import { getDataView } from "./getDataView";
+import { getDataSource } from "../DataSources/getDataSource";
 
 export function getEntity(ctx: any) {
-  return getDataView(ctx).entity;
+  return getDataSource(ctx).entity;
 }
