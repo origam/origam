d89b02f983a754e15bb120308de91f8f7eacf49f
Vaclav Urbanek <vaclav.urbanek@advantages.cz>
2020-01-03 16:34:16 +0100

    NEW: ApiProcessor now firstly try to find the route in the list of parameter-less (static) routes. If it matches, than the route (page) is returned. Otherwise the parameter-full api routes are examined then.

---
+++ Origam.ServerCommon/UrlApiPageCache.cs
@@ -0,0 +1,60 @@
+﻿using Origam.Schema;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading;
+using Origam.Schema.GuiModel;
+
+namespace Origam.ServerCommon
+{
+    class UrlApiPageCache
+    {
+        private static readonly log4net.ILog log
+            = log4net.LogManager.GetLogger(
+            System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private Dictionary<string, AbstractPage> parameterlessPages = null;
+        private List<AbstractPage> parameterPages = null;
+
+        public UrlApiPageCache(List<AbstractSchemaItem> pages)
+        {
+            parameterlessPages = new Dictionary<string, AbstractPage>();
+            parameterPages = new List<AbstractPage>();
+            foreach (AbstractPage page in pages)
+            {
+                if (page.Path.Contains("{"))
+                {
+                    parameterPages.Add(page);
+                }
+                else
+                {
+                    if (parameterlessPages.ContainsKey(page.Path))
+                    {
+                        throw new OrigamException(
+                            string.Format(
+                                "Can't initialize API Url resolver. Duplicate API route '{0}'",
+                                page.Path));
+                    }
+                    parameterlessPages.Add(page.Path, page);
+                }
+            }
+            if (log.IsDebugEnabled)
+            {
+                log.Debug("parameterless URL Api resolver initialised.");
+            }
+        }
+
+        public AbstractPage GetParameterlessPage(string incommingPath)
+        {            
+            return parameterlessPages.ContainsKey(incommingPath) ?
+                parameterlessPages[incommingPath]
+                : null;
+        }
+        
+        public List<AbstractPage> GetParameterPages()
+        {
+            return parameterPages;
+        }
+    }
+}
