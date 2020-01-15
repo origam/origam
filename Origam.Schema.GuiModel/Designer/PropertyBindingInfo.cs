2dd4f74e8a3a332719fc99ada2da987eee6951f5
Vaclav Urbanek <vaclav.urbanek@advantages.cz>
2020-01-15 13:10:28 +0100

    FIX: Localization of PropertyValueItem didn't work well. It used to try to get XmlPersistedValue member from localization cache for the schema item, while there were only 'Value' members.

---
+++ Origam.Schema.GuiModel/Designer/PropertyBindingInfo.cs
@@ -0,0 +1,92 @@
+﻿#region license
+/*
+Copyright 2005 - 2020 Advantage Solutions, s. r. o.
+
+This file is part of ORIGAM (http://www.origam.org).
+
+ORIGAM is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ORIGAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using System;
+using System.ComponentModel;
+
+using Origam.DA.ObjectPersistence;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace Origam.Schema.GuiModel
+{
+    /// <summary>
+    /// Summary description for PropertyValueItem.
+    /// </summary>
+    /// 
+    [XmlModelRoot(ItemTypeConst)]
+    public class PropertyBindingInfo : AbstractPropertyValueItem
+	{
+		public const string ItemTypeConst = "PropertyBindingInfo";
+
+		public PropertyBindingInfo() : base(){}
+		
+		public PropertyBindingInfo(Guid schemaExtensionId) : base(schemaExtensionId) {}
+
+		public PropertyBindingInfo(Key primaryKey) : base(primaryKey)	{}
+
+        private string _value;
+
+        [EntityColumn("LS01")]
+        [Localizable(true)]
+        [XmlAttribute("value")]
+        public string Value
+        {
+            get
+            {
+                if (_value == null) return null;
+
+                return _value.Trim();
+            }
+            set
+            {
+                _value = value;
+            }
+
+        }
+
+        private string _designDataSetPath;
+
+		[EntityColumn("SS01")] 
+        [XmlAttribute("designDataSetPath")]
+		public string DesignDataSetPath
+		{
+			get
+			{
+				return _designDataSetPath;
+			}
+			set
+			{
+				_designDataSetPath=value;
+			}
+
+		}
+
+		[EntityColumn("ItemType")]
+		public override string ItemType
+		{
+			get
+			{
+				return PropertyBindingInfo.ItemTypeConst;
+			}
+		}
+	}
+}
