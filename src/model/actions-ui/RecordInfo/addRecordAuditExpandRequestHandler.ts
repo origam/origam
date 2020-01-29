29f383fa2a3327343d557b42eab834339a75e016
Pavel Tomasko <ptomask@gmail.com>
2020-01-29 09:13:38 +0100

    Internal: Fix crash when user tries to log out.

--- src/model/actions-ui/RecordInfo/addRecordAuditExpandRequestHandler.ts
+++ src/model/actions-ui/RecordInfo/addRecordAuditExpandRequestHandler.ts
@@ -2,6 +2,6 @@ import { getRecordInfo } from "model/selectors/RecordInfo/getRecordInfo";
 
 export function addRecordAuditExpandRequestHandler(ctx: any) {
   return function addRecordAuditExpandRequestHandler(handler: () => void) {
-    getRecordInfo(ctx).addAuditSectionExpandHandler(handler);
+    return getRecordInfo(ctx).addAuditSectionExpandHandler(handler);
   }
 }
--- src/model/actions-ui/RecordInfo/addRecordInfoExpandRequestHandler.ts
+++ src/model/actions-ui/RecordInfo/addRecordInfoExpandRequestHandler.ts
@@ -2,6 +2,6 @@ import { getRecordInfo } from "model/selectors/RecordInfo/getRecordInfo"
 
 export function addRecordInfoExpandRequestHandler(ctx: any) {
   return function addRecordInfoExpandRequestHandler(handler: () => void) {
-    getRecordInfo(ctx).addInfoSectionExpandHandler(handler);
+    return getRecordInfo(ctx).addInfoSectionExpandHandler(handler);
   }
 }
