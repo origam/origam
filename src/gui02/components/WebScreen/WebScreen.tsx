9f0ae4b21cbfe29d78dfb1c06925106e1a144255
Pavel Tomasko <ptomask@gmail.com>
2020-02-14 18:32:30 +0100

    INTERNAL: WebScreen loading indicator

--- src/gui02/components/WebScreen/WebScreen.tsx
+++ src/gui02/components/WebScreen/WebScreen.tsx
@@ -1,6 +1,25 @@
 import React from "react";
 import S from "./WebScreen.module.scss";
 
-export const WebScreen: React.FC<{url: string, refIFrame?: any}> = props => (
-  <iframe ref={props.refIFrame} className={S.root} src={props.url} />
+export const WebScreen: React.FC<{
+  url: string;
+  isLoading?: boolean;
+  refIFrame?: any;
+  onLoad?: any;
+  onLoadStart?: any;
+}> = props => (
+  <div className={S.root}>
+    {props.isLoading && (
+      <div className={S.progressIndicator}>
+        <div className={S.indefinite} />
+      </div>
+    )}
+    <iframe
+      onLoad={props.onLoad}
+      onLoadStart={props.onLoadStart}
+      ref={props.refIFrame}
+      className={S.webContent}
+      src={props.url}
+    />
+  </div>
 );
--- src/gui02/connections/CScreen.tsx
+++ src/gui02/connections/CScreen.tsx
@@ -1,12 +1,45 @@
 import { FormScreenBuilder } from "gui/Workbench/ScreenArea/FormScreenBuilder";
 import { observer, Provider } from "mobx-react";
 import { IOpenedScreen } from "model/entities/types/IOpenedScreen";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Screen } from "../components/Screen/Screen";
 import { ScreenContainer } from "gui02/components/Screen/ScreenContainer";
 import { CtxPanelVisibility } from "gui02/contexts/GUIContexts";
 import { WebScreen } from "gui02/components/WebScreen/WebScreen";
 import { IWebScreen } from "model/entities/types/IWebScreen";
+
+const WebScreenComposite: React.FC<{ openedScreen: IOpenedScreen }> = observer(props => {
+  const { openedScreen } = props;
+  const [isLoading, setLoading] = useState(false);
+  useEffect(() => {
+    if (openedScreen.screenUrl) {
+      setLoading(true);
+    }
+  }, []);
+  return (
+    <Screen isHidden={!openedScreen.isActive}>
+      <WebScreen
+        url={openedScreen.screenUrl || ""}
+        isLoading={isLoading}
+        onLoad={() => {
+          setLoading(false);
+        }}
+        refIFrame={(elm: any) => {
+          ((openedScreen as unknown) as IWebScreen).setReloader(
+            elm
+              ? {
+                  reload: () => {
+                    setLoading(true);
+                    elm.contentWindow.location.reload();
+                  }
+                }
+              : null
+          );
+        }}
+      />
+    </Screen>
+  );
+});
 
 @observer
 export class CScreen extends React.Component<{
@@ -15,24 +48,7 @@ export class CScreen extends React.Component<{
   render() {
     const { openedScreen } = this.props;
     if (openedScreen.screenUrl) {
-      return (
-        <Screen isHidden={!openedScreen.isActive}>
-          <WebScreen
-            url={openedScreen.screenUrl}
-            refIFrame={(elm: any) => {
-              ((openedScreen as unknown) as IWebScreen).setReloader(
-                elm
-                  ? {
-                      reload: () => {
-                        elm.contentWindow.location.reload();
-                      }
-                    }
-                  : null
-              );
-            }}
-          />
-        </Screen>
-      );
+      return <WebScreenComposite openedScreen={openedScreen} />;
     }
     if (!openedScreen.content) return null;
     const formScreen = openedScreen.content;
