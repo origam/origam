366d707ae37a4594747b5cf3c4d118803740c85c
Pavel Tomasko <ptomask@gmail.com>
2020-02-28 13:50:59 +0100

    FIX: Iframe no longer swallows mouse messages so that is possible to drag a splitter over it.

--- src/gui02/components/WebScreen/WebScreen.tsx
+++ src/gui02/components/WebScreen/WebScreen.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import S from "./WebScreen.module.scss";
+import cx from "classnames";
 
 export const WebScreen: React.FC<{
   url: string;
@@ -7,19 +8,36 @@ export const WebScreen: React.FC<{
   refIFrame?: any;
   onLoad?: any;
   onLoadStart?: any;
-}> = props => (
-  <div className={S.root}>
-    {props.isLoading && (
-      <div className={S.progressIndicator}>
-        <div className={S.indefinite} />
-      </div>
-    )}
-    <iframe
-      onLoad={props.onLoad}
-      onLoadStart={props.onLoadStart}
-      ref={props.refIFrame}
-      className={S.webContent}
-      src={props.url}
-    />
-  </div>
-);
+}> = props => {
+  const [mouseDowned, setMouseDowned] = useState(false);
+  const handleMousedown = () => {
+    setMouseDowned(true);
+    window.addEventListener("mouseup", handleMouseup);
+  };
+  const handleMouseup = () => {
+    setMouseDowned(false);
+    window.removeEventListener("mouseup", handleMouseup);
+  };
+  useEffect(() => {
+    window.addEventListener("mousedown", handleMousedown);
+    return () => window.removeEventListener("mousedown", handleMousedown);
+  }, []);
+
+  return (
+    <div className={S.root}>
+      {props.isLoading && (
+        <div className={S.progressIndicator}>
+          <div className={S.indefinite} />
+        </div>
+      )}
+      <iframe
+        onLoad={props.onLoad}
+        onLoadStart={props.onLoadStart}
+        ref={props.refIFrame}
+        className={S.webContent}
+        src={props.url}
+      />
+      <div className={cx(S.transparentOverlay, { isVisible: mouseDowned })} />
+    </div>
+  );
+};
