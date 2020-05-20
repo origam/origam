a2aa5e6d645b55f14f973a2e8b574be1c1b976e4
Pavel Tomasko <ptomask@gmail.com>
2020-05-20 11:07:17 +0200

    INTERNAL: ServerCoreUIService.GetRow now returns DataStructureEntity in its result.

--- Origam.ServerCore/Controller/UIServiceController.cs
+++ Origam.ServerCore/Controller/UIServiceController.cs
@@ -385,7 +385,7 @@ namespace Origam.ServerCore.Controller
         {
             return RunWithErrorHandler(() 
                 => Ok(sessionObjects.UIService.WorkflowRepeat(
-                    sessionFormIdentifier, localizer)));
+                    sessionFormIdentifier, localizer).Result));
         }
         [HttpPost("[action]")]
         public IActionResult AttachmentCount(
@@ -641,8 +641,14 @@ namespace Origam.ServerCore.Controller
             }
             else
             {
-                return sessionObjects.UIService.GetRow(
-                    input.SessionFormIdentifier, input.Entity, input.Id);
+                //  return sessionObjects.UIService.GetRow(
+                //     input.SessionFormIdentifier, input.Entity, input.Id);
+
+                return FindEntity(input.DataStructureEntityId)
+                    .Bind(dataStructureEntity =>
+                        sessionObjects.UIService.GetRow(
+                            input.SessionFormIdentifier, input.Entity,
+                            dataStructureEntity, input.Id));
             }
         }
         private static Hashtable DictionaryToHashtable(IDictionary<string, object> source)
