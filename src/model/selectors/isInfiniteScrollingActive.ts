f18557c7b3fdea336b74c2c2d9addedd9243a2aa
jindrichSusen <jindrich.susen@gmail.com>
2020-05-27 14:25:26 +0200

    INTERNAL: Scrolling condition simplified

--- src/model/selectors/isInfiniteScrollingActive.ts
+++ src/model/selectors/isInfiniteScrollingActive.ts
@@ -2,6 +2,5 @@ import {IDataView} from "../entities/types/IDataView";
 import {getDontRequestData} from "./getDontRequestData";
 
 export function isInfiniteScrollingActive(ctx: any, dataViewAttributes: any) {
-  const isNonRootGridWithRootEntity = dataViewAttributes.IsRootEntity && !dataViewAttributes.IsRootGrid && getDontRequestData(ctx);
-  return getDontRequestData(ctx) && dataViewAttributes.IsRootGrid && !isNonRootGridWithRootEntity;
+  return getDontRequestData(ctx) && dataViewAttributes.IsRootGrid;
 }
