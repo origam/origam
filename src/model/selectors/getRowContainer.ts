bf75564d6c9f0c1d728628f487582e827a533059
jindrichSusen <jindrich.susen@gmail.com>
2020-05-27 13:49:08 +0200

    INTERNAL: Dead code removed

--- src/model/selectors/getRowContainer.ts
+++ src/model/selectors/getRowContainer.ts
@@ -1,11 +1,7 @@
 import {ListRowContainer, ScrollRowContainer} from "../entities/RowsContainer";
-import {getDataView} from "./DataView/getDataView";
 import {isInfiniteScrollingActive} from "./isInfiniteScrollingActive";
-import {IDataView} from "../entities/types/IDataView";
-import {IFormScreenLifecycle02} from "../entities/types/IFormScreenLifecycle";
 
 export function getRowContainer(ctx: any, dataViewAttributes: any) {
-  // const dataView = getDataView(ctx);
   return isInfiniteScrollingActive(ctx, dataViewAttributes)
     ? new ScrollRowContainer()
     : new ListRowContainer();
