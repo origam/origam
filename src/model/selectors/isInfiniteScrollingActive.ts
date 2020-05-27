c0ef176ec52abd3cf4a930216c7064ee12d38564
jindrichSusen <jindrich.susen@gmail.com>
2020-05-27 14:38:18 +0200

    INTERNAL: isInfiniteScrollingActive fixed

--- src/model/selectors/isInfiniteScrollingActive.ts
+++ src/model/selectors/isInfiniteScrollingActive.ts
@@ -2,5 +2,5 @@ import {IDataView} from "../entities/types/IDataView";
 import {getDontRequestData} from "./getDontRequestData";
 
 export function isInfiniteScrollingActive(ctx: any, dataViewAttributes: any) {
-  return getDontRequestData(ctx) && dataViewAttributes.IsRootGrid;
+  return getDontRequestData(ctx) && dataViewAttributes.IsRootGrid === "true";
 }
