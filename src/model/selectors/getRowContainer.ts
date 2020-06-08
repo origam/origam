f2fe58ff5ac75c5409dc3788d7fcd5a8282c2b35
jindrichSusen <jindrich.susen@gmail.com>
2020-06-08 14:29:07 +0200

    INTERNAL: File renamed

--- src/model/selectors/getRowContainer.ts
+++ src/model/selectors/getRowContainer.ts
@@ -1,8 +1,8 @@
-import {ListRowContainer} from "../entities/RowsContainer";
 import {isInfiniteScrollingActive} from "./isInfiniteScrollingActive";
 import {IOrderingConfiguration} from "../entities/types/IOrderingConfiguration";
 import {IFilterConfiguration} from "../entities/types/IFilterConfiguration";
 import {ScrollRowContainer} from "../entities/ScrollRowContainer";
+import {ListRowContainer} from "../entities/ListRowContainer";
 
 export function getRowContainer(ctx: any, dataViewAttributes: any,
                                 orderingConfiguration: IOrderingConfiguration,
