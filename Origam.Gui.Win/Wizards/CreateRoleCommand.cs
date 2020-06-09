1fef2e36c6b068af58841399ac85eba89b006aae
Jiri Prajz <jiri.prajz@advantages.cz>
2020-06-09 14:38:39 +0200

    CHG: Create ActionWizard for CreateRoleCommand.

--- Origam.Gui.Win/Wizards/CreateRoleCommand.cs
+++ Origam.Gui.Win/Wizards/CreateRoleCommand.cs
@@ -20,13 +20,20 @@ along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
 #endregion
 
 using System;
+using System.Collections;
+using System.Windows.Forms;
 using Origam.Schema.DeploymentModel;
+using Origam.Schema.MenuModel;
 using Origam.UI;
+using Origam.UI.WizardForm;
+using Origam.Workbench;
 
 namespace Origam.Gui.Win.Wizards
 {
     class CreateRoleCommand : AbstractMenuCommand
     {
+		StructureForm structureForm;
+		SchemaBrowser _schemaBrowser = WorkbenchSingleton.Workbench.GetPad(typeof(SchemaBrowser)) as SchemaBrowser;
 		public override bool IsEnabled
 		{
 			get
@@ -43,10 +50,40 @@ namespace Origam.Gui.Win.Wizards
 
 		public override void Run()
 		{
-            IAuthorizationContextContainer obj = Owner as IAuthorizationContextContainer;
-            ServiceCommandUpdateScriptActivity activity = 
-                CreateRole(obj.AuthorizationContext);
-            GeneratedModelElements.Add(activity);
+			ServiceCommandUpdateScriptActivity scriptActivity = new ServiceCommandUpdateScriptActivity();
+			ArrayList list = new ArrayList();
+			list.Add(new ListViewItem(scriptActivity.ModelDescription(), scriptActivity.Icon));
+			Stack stackPage = new Stack();
+			stackPage.Push(PagesList.Finish);
+			stackPage.Push(PagesList.StartPage);
+
+			structureForm = new StructureForm()
+			{
+				Title = ResourceUtils.GetString("CreateDataStructureFromEntityWizardTitle"),
+				Description = ResourceUtils.GetString("CreateDataStructureFromEntityWizardDescription"),
+				ItemTypeList = list,
+				Pages = stackPage,
+				NameOfEntity = ((AbstractMenuItem)Owner).Name,
+				ImageList = _schemaBrowser.EbrSchemaBrowser.imgList,
+				Command = this
+			};
+			Wizard wizardscreen = new Wizard(structureForm);
+			if (wizardscreen.ShowDialog() != DialogResult.OK)
+			{
+				GeneratedModelElements.Clear();
+			}
         }
+		public override void Execute()
+		{
+			IAuthorizationContextContainer obj = Owner as IAuthorizationContextContainer;
+			ServiceCommandUpdateScriptActivity activity =
+				CreateRole(obj.AuthorizationContext);
+			GeneratedModelElements.Add(activity);
+		}
+
+		public override int GetImageIndex(string icon)
+		{
+			return _schemaBrowser.ImageIndex(icon);
+		}
 	}
 }
