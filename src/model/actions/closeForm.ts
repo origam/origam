3f4cedd5d64e0f4049039ff56a562bcdff7e5831
jindrichSusen <jindrich.susen@gmail.com>
2020-07-14 16:08:16 +0200

    INTERNAL: Debug code was throwing exceptions

--- src/model/actions/closeForm.ts
+++ src/model/actions/closeForm.ts
@@ -8,17 +8,19 @@ export function closeForm(ctx: any) {
     const lifecycle = getWorkbenchLifecycle(ctx);
     const openedScreen = getOpenedScreen(ctx);
 
-    const refreshOnReturnType = openedScreen.content.refreshOnReturnType;
-    switch(refreshOnReturnType){
-      case IRefreshOnReturnType.ReloadActualRecord:
-        break;
-      case IRefreshOnReturnType.RefreshCompleteForm:
-        break;
-      case IRefreshOnReturnType.MergeModalDialogChanges:
-        const api = getApi(ctx);
-        const data = yield api.pendingChanges({ sessionFormIdentifier: openedScreen.parent.activeItem.content!.formScreen!.sessionId});
-        // const data = yield api.pendingChanges({ sessionFormIdentifier: openedScreen.content!.formScreen!.sessionId});
-        break;
+    if(openedScreen.content){
+      const refreshOnReturnType = openedScreen.content.refreshOnReturnType;
+      switch(refreshOnReturnType){
+        case IRefreshOnReturnType.ReloadActualRecord:
+          break;
+        case IRefreshOnReturnType.RefreshCompleteForm:
+          break;
+        case IRefreshOnReturnType.MergeModalDialogChanges:
+          const api = getApi(ctx);
+          const data = yield api.pendingChanges({ sessionFormIdentifier: openedScreen.parent.activeItem.content!.formScreen!.sessionId});
+          // const data = yield api.pendingChanges({ sessionFormIdentifier: openedScreen.content!.formScreen!.sessionId});
+          break;
+      }
     }
     yield* lifecycle.closeForm(openedScreen);
 
