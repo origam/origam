ed7e373581e756d71a9d840313363828ed62f462
jindrichSusen <jindrich.susen@gmail.com>
2020-07-30 10:39:54 +0200

    INTERNAL: Localization files are of format json now

--- src/utils/translation.tsx
+++ src/utils/translation.tsx
@@ -1,27 +1,18 @@
 import React from "react";
 import axios from "axios";
 import {getLocaleFromCookie} from "./cookies";
-import PropertiesReader, {Value} from "properties-reader";
 
-const DEBUG_SHOW_TRANSLATIONS = window.localStorage.getItem('debugShowTranslations') === "true";
+const debugShowTranslations = window.localStorage.getItem('debugShowTranslations') === "true";
 
 let translations = {
-  this_is_a_text: "This is {0} a text {1}",
-} as {[k: string]: Value};
-
-
+} as {[k: string]: string};
 
 export async function translationsInit(){
   const locale = getLocaleFromCookie();
-  const localeFolder = locale.replace("-","_");
-  const translationData =  (
-    await axios.get(`locale/${localeFolder}/asap.properties`, {
+  translations =  (
+    await axios.get(`locale/localization_${locale}.json`, {
     })
   ).data;
-  const propertiesReader = PropertiesReader("");
-  propertiesReader.read(translationData);
-  translations = propertiesReader.getAllProperties();
-  console.log(translations);
 }
 
 export function T(defaultContent: any, translKey: string, ...p: any[]) {
@@ -36,7 +27,7 @@ export function T(defaultContent: any, translKey: string, ...p: any[]) {
   for (let i = 0; i < p.length; i++) {
     result = result.replace(`{${i}}`, p[i]);
   }
-  if (DEBUG_SHOW_TRANSLATIONS) {
+  if (debugShowTranslations) {
     if(showingDefault){
       console.error(`Could not find translation for: "${translKey}", showing default: "${result}"`);
       return (
