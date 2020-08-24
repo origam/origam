85a6ec47a911d7216093a844773b68e5b2db9aa2
jindrichSusen <jindrich.susen@gmail.com>
2020-08-24 13:48:45 +0200

    NEW: Checkboxes in a CheckList are focused using arrows

--- src/gui/Components/ScreenElements/Editors/CheckList.tsx
+++ src/gui/Components/ScreenElements/Editors/CheckList.tsx
@@ -1,14 +1,14 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, { RefObject, useContext, useEffect, useRef, useState } from "react";
 import S from "./CheckList.module.scss";
-import {MobXProviderContext, observer} from "mobx-react";
-import {action, computed, flow, observable} from "mobx";
-import {IApi} from "model/entities/types/IApi";
-import {getApi} from "model/selectors/getApi";
-import {getDataStructureEntityId} from "model/selectors/DataView/getDataStructureEntityId";
-import {getSelectedRowId} from "model/selectors/TablePanelView/getSelectedRowId";
-import {getMenuItemId} from "model/selectors/getMenuItemId";
-import {getEntity} from "model/selectors/DataView/getEntity";
-import {getSessionId} from "model/selectors/getSessionId";
+import { MobXProviderContext, observer } from "mobx-react";
+import { action, computed, flow, observable } from "mobx";
+import { IApi } from "model/entities/types/IApi";
+import { getApi } from "model/selectors/getApi";
+import { getDataStructureEntityId } from "model/selectors/DataView/getDataStructureEntityId";
+import { getSelectedRowId } from "model/selectors/TablePanelView/getSelectedRowId";
+import { getMenuItemId } from "model/selectors/getMenuItemId";
+import { getEntity } from "model/selectors/DataView/getEntity";
+import { getSessionId } from "model/selectors/getSessionId";
 
 export interface IRawCheckListProps {
   api: IApi;
@@ -30,20 +30,19 @@ export interface IRawCheckListProps {
 }
 
 export class CheckListControler {
-
   @observable lookupList: string[][] = [];
 
   @computed get items() {
-    return this.lookupList.map(llitem => ({
+    return this.lookupList.map((llitem) => ({
       value: llitem[0],
-      label: llitem[1]
+      label: llitem[1],
     }));
   }
 
   @action.bound
   loadLookupList() {
     const self = this;
-    flow(function*() {
+    flow(function* () {
       const lookupList = yield self.props.api.getLookupList({
         Entity: self.props.Entity,
         SessionFormIdentifier: self.props.SessionFormIdentifier,
@@ -57,7 +56,7 @@ export class CheckListControler {
         ShowUniqueValues: false,
         SearchText: "",
         PageSize: 10000,
-        PageNumber: 1
+        PageNumber: 1,
       });
       self.lookupList = lookupList;
     })();
@@ -65,7 +64,7 @@ export class CheckListControler {
 
   @action.bound handleClick(event: any, item: { value: string; label: string }) {
     event.preventDefault();
-    const currentIndex = this.props.value.findIndex(id => item.value === id);
+    const currentIndex = this.props.value.findIndex((id) => item.value === id);
     if (currentIndex > -1) {
       const newValue = [...this.props.value];
       newValue.splice(currentIndex, 1);
@@ -84,7 +83,7 @@ export const CheckList: React.FC<{
   value: string[];
   onChange?(newValue: string[]): void;
   tabIndex?: number;
-}> = observer(props => {
+}> = observer((props) => {
   const { property } = useContext(MobXProviderContext);
 
   return (
@@ -111,17 +110,62 @@ export const CheckListRaw: React.FC<IRawCheckListProps> = observer(props => {
   const [controller] = useState(() => new CheckListControler());
   controller.props = props;
   useEffect(controller.loadLookupList, [props.RowId]);
-  console.log(props.value);
+
+  const inputRefs: InputReference[] = [];
+
+  function focusLeft(x: number, y: number) {
+    const inputsOnTheLeft = inputRefs.filter(input => input.hasYEqualTo(y) && input.isOnTheLeftOf(x))
+      .sort((i1, i2) => i2.x - i1.x);
+
+    if (inputsOnTheLeft.length > 0) {
+      inputsOnTheLeft[0].focus();
+    }
+  }
+
+  function focusRight(x: number, y: number){
+    const inputsOnTheRight = inputRefs.filter(input => input.hasYEqualTo(y) && input.isOnTheRightOf(x))
+      .sort((i1, i2) => i1.x - i2.x);
+
+    if(inputsOnTheRight.length > 0){
+      inputsOnTheRight[0].focus();
+    }
+  }
+
+  function focusUp(x: number, y: number){
+    const inputsAbove = inputRefs.filter(input => input.hasXEqualTo(x) && input.isAbove(y))
+      .sort((i1, i2) => i2.y - i1.y);
+
+    if(inputsAbove.length > 0){
+      inputsAbove[0].focus();
+    }
+  }
+
+
+  function focusDown(x: number, y: number){
+    const inputsBelow = inputRefs.filter(input => input.hasXEqualTo(x) && input.isBelow(y))
+      .sort((i1, i2) => i1.y - i2.y);
+
+    if(inputsBelow.length > 0){
+      inputsBelow[0].focus();
+    }
+  }
+
+
   return (
-    <div className={S.root} >
+    <div className={S.root}>
       {controller.items.map((item, i) => (
         <CheckListItem
           key={item.value}
-          checked={!!props.value.find(v => v === item.value)}
-          onClick={event => {
+          checked={!!props.value.find((v) => v === item.value)}
+          onClick={(event) => {
             controller.handleClick(event, item);
           }}
-          tabIndex={ i === 0 ? props.tabIndex : -1}
+          tabIndex={i === 0 ? props.tabIndex : -1}
+          inputSetter={(inputRef: InputReference) => inputRefs.push(inputRef)}
+          focusLeft={focusLeft}
+          focusRight={focusRight}
+          focusUp={focusUp}
+          focusDown={focusDown}
         >
           {item.label}
         </CheckListItem>
@@ -134,15 +178,86 @@ export const CheckListItem: React.FC<{
   checked: boolean;
   onClick?(event: any): void;
   tabIndex?: number;
-}> = props => {
+  inputSetter: (inputRef: InputReference) => void;
+  focusLeft: (x: number, y: number)=>void;
+  focusRight: (x: number, y: number)=>void;
+  focusUp: (x: number, y: number)=>void;
+  focusDown: (x: number, y: number)=>void;
+}> = (props) => {
+  function onKeyDown(event: any) {
+    const boundingRect = refInput.current?.getBoundingClientRect()!;
+    event.preventDefault();
+    switch (event.key) {
+      case "ArrowUp":
+        props.focusUp(boundingRect.x, boundingRect.y);
+        break;
+      case "ArrowDown":
+        props.focusDown(boundingRect.x, boundingRect.y);
+        break;
+      case "ArrowRight":
+        props.focusRight(boundingRect.x, boundingRect.y);
+        break;
+      case "ArrowLeft":
+        props.focusLeft(boundingRect.x, boundingRect.y);
+        break;
+    }
+  }
+
+  const refInput = useRef<HTMLInputElement>(null);
+  props.inputSetter(new InputReference(refInput));
+
   return (
     <div className={S.item} onClick={props.onClick}>
-      <input type="checkbox"
-             className={"checkbox "+S.input}
-             checked={props.checked}
-             tabIndex={props.tabIndex ? props.tabIndex : undefined}
-             onKeyDown={onKeyDown}/>
+      <input
+        ref={refInput}
+        type="checkbox"
+        className={"checkbox " + S.input}
+        checked={props.checked}
+        tabIndex={props.tabIndex ? props.tabIndex : undefined}
+        onKeyDown={onKeyDown}
+      />
       <div className={"content"}>{props.children}</div>
     </div>
   );
 };
+
+class InputReference {
+  constructor(private inputRef: RefObject<HTMLInputElement>) {
+  }
+
+  get x() {
+    return this.inputRef.current?.getBoundingClientRect()!.x;
+  }
+
+  get y() {
+    return this.inputRef.current?.getBoundingClientRect()!.y;
+  }
+
+  hasXEqualTo(x: number) {
+    return Math.abs(this.x - x) < 1;
+  }
+
+  hasYEqualTo(y: number) {
+    return Math.abs(this.y - y) < 1;
+  }
+
+  isOnTheLeftOf(x: number) {
+    return this.x < x;
+  }
+
+  isOnTheRightOf(x: number) {
+    return this.x > x;
+  }
+
+  isAbove(y: number) {
+    return this.y < y;
+  }
+
+  isBelow(y: number) {
+    return this.y > y;
+  }
+
+  focus() {
+    this.inputRef.current?.focus();
+  }
+}
