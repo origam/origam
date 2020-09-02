890e14fd05527a11f946fb54aa084ed9ae1a0c69
jindrichSusen <jindrich.susen@gmail.com>
2020-09-02 16:06:37 +0200

    NEW: ReloadActualRecord return type implemented

--- src/model/actions/closeForm.ts
+++ src/model/actions/closeForm.ts
@@ -3,23 +3,34 @@ import {getOpenedScreen} from "model/selectors/getOpenedScreen";
 import {IRefreshOnReturnType} from "../entities/WorkbenchLifecycle/WorkbenchLifecycle";
 import {getApi} from "../selectors/getApi";
 import {ICRUDResult, processCRUDResult} from "./DataLoading/processCRUDResult";
+import {getDontRequestData} from "model/selectors/getDontRequestData";
+import {getDataViewLifecycle} from "model/selectors/DataView/getDataViewLifecycle";
+import {getFormScreen} from "model/selectors/FormScreen/getFormScreen";
 
 export function closeForm(ctx: any) {
   return function* closeForm(): Generator {
     const lifecycle = getWorkbenchLifecycle(ctx);
     const openedScreen = getOpenedScreen(ctx);
+    const parentScreen = getOpenedScreen(openedScreen.parentContext);
 
     yield* lifecycle.closeForm(openedScreen);
     if(openedScreen.content){
       const refreshOnReturnType = openedScreen.content.refreshOnReturnType;
       switch(refreshOnReturnType){
         case IRefreshOnReturnType.ReloadActualRecord:
+          if (getDontRequestData(ctx)) {
+            break;
+          }
+          const formScreen = getFormScreen(openedScreen.parentContext);
+          for (let dataView of formScreen.dataViews) {
+            const dataViewLifecycle = getDataViewLifecycle(dataView);
+            yield dataViewLifecycle.runRecordChangedReaction();
+          }
           break;
         case IRefreshOnReturnType.RefreshCompleteForm:
           break;
         case IRefreshOnReturnType.MergeModalDialogChanges:
           const api = getApi(ctx);
-          const parentScreen = getOpenedScreen(openedScreen.parentContext);
           const parentScreenSessionId = parentScreen!.content!.formScreen!.sessionId;
           const changes = (yield api.pendingChanges({ sessionFormIdentifier: parentScreenSessionId})) as ICRUDResult[];
           for (let change of changes) {
