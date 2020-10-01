435957730bccb59e17750e1d3defd2ad36af7248
Petr Hrehorovsky <washi@volny.cz>
2020-10-01 12:41:07 +0200

    CHG: Removed authorization check from InitUI.

--- Origam.ServerCore/Controller/UIServiceController.cs
+++ Origam.ServerCore/Controller/UIServiceController.cs
@@ -107,31 +107,12 @@ namespace Origam.ServerCore.Controller
         [HttpPost("[action]")]
         public IActionResult InitUI([FromBody]UIRequest request)
         {
-            return RunWithErrorHandler(() =>
-            {
-                if(request.Type == UIRequestType.WorkQueue)
-                {
-                    return Ok(sessionObjects.UIManager.InitUI(
-                        request: request,
-                        addChildSession: false,
-                        parentSession: null,
-                        basicUIService: sessionObjects.UIService));
-
-                }
-                else
-                {
-                    return FindItem<AbstractMenuItem>(
-                            new Guid(request.ObjectId))
-                        .Bind(Authorize)
-                        .Map(menuItem => sessionObjects.UIManager.InitUI(
-                            request: request,
-                            addChildSession: false,
-                            parentSession: null,
-                            basicUIService: sessionObjects.UIService))
-                        .Map(ToActionResult)
-                        .Finally(UnwrapReturnValue);
-                }
-            });
+            return RunWithErrorHandler(() => 
+                Ok(sessionObjects.UIManager.InitUI(
+                    request: request,
+                    addChildSession: false,
+                    parentSession: null,
+                    basicUIService: sessionObjects.UIService)));
         }
         [HttpGet("[action]/{sessionFormIdentifier:guid}")]
         public IActionResult DestroyUI(Guid sessionFormIdentifier)
