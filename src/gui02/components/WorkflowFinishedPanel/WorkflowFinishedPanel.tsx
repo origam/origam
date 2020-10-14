73152f06a923a3f2642970856a14afe2b4cd8ab7
jindrichSusen <jindrich.susen@gmail.com>
2020-10-14 10:32:19 +0200

    INTERNAL: Some forgotten buttons localized

--- src/gui02/components/WorkflowFinishedPanel/WorkflowFinishedPanel.tsx
+++ src/gui02/components/WorkflowFinishedPanel/WorkflowFinishedPanel.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import S from "./WorkflowFinishedPanel.module.scss";
+import {T} from "utils/translation";
 
 export const WorkflowFinishedPanel: React.FC<{
   isCloseButton: boolean;
@@ -9,8 +10,8 @@ export const WorkflowFinishedPanel: React.FC<{
   message: string;
 }> = (props) => (
   <div className={S.root}>
-    {props.isRepeatButton && <button onClick={props.onRepeatClick}>Repeat</button>}
-    {props.isCloseButton && <button onClick={props.onCloseClick}>Close</button>}
+    {props.isRepeatButton && <button onClick={props.onRepeatClick}>{T("Repeat","button_repeat")}</button>}
+    {props.isCloseButton && <button onClick={props.onCloseClick}>{T("Close","button_close")}</button>}
     {/*<iframe className={S.message} srcDoc={} />*/}
     <div className={S.message} dangerouslySetInnerHTML={{ __html: `${props.message}` }} />
   </div>
