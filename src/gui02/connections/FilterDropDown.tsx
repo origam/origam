22b2a709ff0c8fbe85fca10f8f2632bd82db7c45
jindrichSusen <jindrich.susen@gmail.com>
2020-10-29 09:33:28 +0100

    INTERNAL: Remember The Current Filter enabled

--- src/gui02/connections/FilterDropDown.tsx
+++ src/gui02/connections/FilterDropDown.tsx
@@ -91,10 +91,10 @@ export class FilterDropDown extends React.Component<{ ctx: any }> {
               {T("Remember The Current Filter", "filter_menu_set_default_filter")}
             </DropdownItem>
             <DropdownItem
-              isDisabled={true}
+              isDisabled={this.filterManager.activeFilters.length === 0}
               onClick={(event: any) => {
                 setDropped(false);
-                // onColumnConfigurationClickEvt(event);
+                this.filterManager.setSelectedFilterGroupAsDefault();
               }}
             >
               {T("Cancel Default Filter", "filter_menu_cancel_default_filter")}
