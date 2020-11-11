34dc5cf0138b7a3e9e4efadb21aa3a68de357701
Petr Hrehorovsky <washi@volny.cz>
2020-11-11 16:38:14 +0100

    NEW: Metamodel upgrade for WebReports - OpenMethod.NoUI.

---
+++ Origam.DA.Service/MetaModelUpgrade/UpdateScriptContainers/WebReportScriptContainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Origam.Schema.GuiModel;
+
+namespace Origam.DA.Service.MetaModelUpgrade.UpdateScriptContainers
+{
+    class WebReportScriptContainer : UpgradeScriptContainer
+    {
+        public override string FullTypeName { get; } = typeof(WebReport).FullName;
+        public override List<string> OldFullTypeNames { get; }
+        public override string[] OldPropertyXmlNames { get; }
+
+        public WebReportScriptContainer()
+        {
+            upgradeScripts.Add(new UpgradeScript(
+                new Version("6.0.0"),
+                new Version("6.0.1"),
+                (node, doc) => { }));
+        }
+    }
+}
