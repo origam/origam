31c402421b13b4260fe333ed760afcaf707a25e3
jindrichSusen <jindrich.susen@gmail.com>
2020-11-18 12:16:34 +0100

    FIX:  Sorting was attempted after clicking on a column header of a TagInput column

--- src/model/actions-ui/DataView/TableView/onColumnHeaderClick.ts
+++ src/model/actions-ui/DataView/TableView/onColumnHeaderClick.ts
@@ -1,10 +1,15 @@
 import {getOrderingConfiguration} from "model/selectors/DataView/getOrderingConfiguration";
 import {flow} from "mobx";
 import {handleError} from "model/actions/handleError";
+import { getProperties } from "model/selectors/DataView/getProperties";
 
 export function onColumnHeaderClick(ctx: any) {
   return flow(function* onColumnHeaderClick(event: any, column: string) {
     try {
+      const property = getProperties(ctx).find(prop => prop.id === column);
+      if(property?.column === "Blob" || property?.column === "TagInput"){
+        return;
+      }
       if (event.ctrlKey) {
         getOrderingConfiguration(ctx).addOrdering(column);
       } else {
