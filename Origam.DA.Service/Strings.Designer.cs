c1935bb20abb3ca98d00aa792e9c9e03e623c246
Tomáš Vavrda <tomas.vavrda@advantages.cz>
2020-12-08 14:53:16 +0100

    FIX: If all of the model class's properties were null, the class namespace name would not be saved into the model (.origam) file. Reading of such a file would fail with "Cannot find namespace: Origam.Schema.SomeModel.SomeClass in xml file." error.

--- Origam.DA.Service/Strings.Designer.cs
+++ Origam.DA.Service/Strings.Designer.cs
@@ -19,7 +19,7 @@ namespace Origam.DA.Service {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Strings {
@@ -57,15 +57,6 @@ namespace Origam.DA.Service {
             }
             set {
                 resourceCulture = value;
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Cannot find namespace: \&quot;{0}\&quot; in xml file.
-        /// </summary>
-        internal static string CannotFindNamespace {
-            get {
-                return ResourceManager.GetString("CannotFindNamespace", resourceCulture);
             }
         }
         
