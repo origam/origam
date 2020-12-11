7120b6297baba03ed3b3f1868ad312eaf3d31696
jindrichSusen <jindrich.susen@gmail.com>
2020-12-11 15:17:09 +0100

    FIX: No question was asked when trying to create a new row in a dirty form

--- src/model/actions-ui/DataView/onCreateRowClick.ts
+++ src/model/actions-ui/DataView/onCreateRowClick.ts
@@ -3,6 +3,8 @@ import { getEntity } from "model/selectors/DataView/getEntity";
 import { getFormScreenLifecycle } from "model/selectors/FormScreen/getFormScreenLifecycle";
 import { flow } from "mobx";
 import { handleError } from "model/actions/handleError";
+import { getDataView } from "model/selectors/DataView/getDataView";
+import { shouldProceedToChangeRow } from "./TableView/shouldProceedToChangeRow";
 
 export function onCreateRowClick(ctx: any) {
   return flow(function* onCreateRowClick(event: any) {
@@ -10,6 +12,10 @@ export function onCreateRowClick(ctx: any) {
       const gridId = getGridId(ctx);
       const entity = getEntity(ctx);
       const formScreenLifecycle = getFormScreenLifecycle(ctx);
+      const dataView = getDataView(ctx);
+      if (!(yield shouldProceedToChangeRow(dataView))) {
+        return;
+      }
       yield* formScreenLifecycle.onCreateRow(entity, gridId);
     } catch (e) {
       yield* handleError(ctx)(e);
