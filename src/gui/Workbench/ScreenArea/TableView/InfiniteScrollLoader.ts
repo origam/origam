68601e180c9ac8f6af04766f1e4e2c6c4a7a68ef
jindrichSusen <jindrich.susen@gmail.com>
2020-12-24 10:50:55 +0100

    INTERNAL: Debug code removed

--- src/gui/Workbench/ScreenArea/TableView/InfiniteScrollLoader.ts
+++ src/gui/Workbench/ScreenArea/TableView/InfiniteScrollLoader.ts
@@ -201,7 +201,6 @@ export class InfiniteScrollLoader implements IInfiniteScrollLoader {
 
     const api = getApi(this.ctx);
     const formScreenLifecycle = getFormScreenLifecycle(this.ctx);
-    debugger;
     const data = yield api.getRows({
       MenuId: getMenuItemId(this.ctx),
       SessionFormIdentifier: getSessionId(formScreenLifecycle),
