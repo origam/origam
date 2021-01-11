c818c577f9fea88888b3d14e9f7143b70abd2e4b
jindrichSusen <jindrich.susen@gmail.com>
2021-01-11 15:23:53 +0100

    INTERNAL: White space added

--- Origam.ServerCore/Authorization/CoreManagerAdapter.cs
+++ Origam.ServerCore/Authorization/CoreManagerAdapter.cs
@@ -105,7 +105,7 @@ namespace Origam.ServerCore.Authorization
         {
             var user = await FindByNameAsync(userName);
             if (user == null) { return false; }
-            return (await coreUserManager.SetLockoutEndDateAsync( user,null))
+            return (await coreUserManager.SetLockoutEndDateAsync( user, null))
                 .Succeeded;
         }
 
