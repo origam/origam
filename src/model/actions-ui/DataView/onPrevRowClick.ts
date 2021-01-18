8c91464a5da9135886e8663f48a8c3e6652b3cba
jindrichSusen <jindrich.susen@gmail.com>
2021-01-18 16:02:40 +0100

    FIX: It was not checked that row can be changed after pressing up/down keys and prev/next buttons

--- src/model/actions-ui/DataView/onPrevRowClick.ts
+++ src/model/actions-ui/DataView/onPrevRowClick.ts
@@ -1,10 +1,16 @@
 import {selectPrevRow} from "model/actions/DataView/selectPrevRow";
 import {flow} from "mobx";
 import {handleError} from "model/actions/handleError";
+import { getDataView } from "model/selectors/DataView/getDataView";
+import { shouldProceedToChangeRow } from "./TableView/shouldProceedToChangeRow";
 
 export function onPrevRowClick(ctx: any) {
   return flow(function* onPrevRowClick(event: any) {
     try {
+      const dataView = getDataView(ctx);
+      if (!(yield shouldProceedToChangeRow(dataView))) {
+        return;
+      }
       yield* selectPrevRow(ctx)();
     } catch (e) {
       yield* handleError(ctx)(e);
