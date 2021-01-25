761616a32d5e62e37205457e392f152e10c01382
jindrichSusen <jindrich.susen@gmail.com>
2021-01-25 17:39:03 +0100

    FIX: lockedColumns count was not red from the default configuration

--- src/xmlInterpreters/screenXml.ts
+++ src/xmlInterpreters/screenXml.ts
@@ -458,6 +458,14 @@ export function* interpretScreenXml(
 
       let groupingColumnCounter = 1;
       configuration.forEach((conf) => {
+        const defaultFixedColumns = findStopping(conf, (n) => n.name === "lockedColumns");
+        
+        const fixedColumnsStr = defaultFixedColumns?.[0]?.attributes?.["count"];
+        const fixedColumnsInt = parseInt(fixedColumnsStr,10);
+        if (!isNaN(fixedColumnsStr)){
+          dataViewInstance.tablePanelView.fixedColumnCount = fixedColumnsInt;
+        }
+        
         const defaultColumnConfigurations = findStopping(conf, (n) => n.name === "columnWidths");
         defaultColumnConfigurations.forEach((defaultColumnConfiguration) => {
           const columns = findStopping(defaultColumnConfiguration, (n) => n.name === "column");
