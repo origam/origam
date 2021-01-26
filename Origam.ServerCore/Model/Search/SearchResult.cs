7943a24045a98a1a17842ea944792697c74d39b1
jindrichSusen <jindrich.susen@gmail.com>
2021-01-26 09:26:23 +0100

    INTERNAL: Property name changed

--- Origam.ServerCore/Model/Search/SearchResult.cs
+++ Origam.ServerCore/Model/Search/SearchResult.cs
@@ -27,7 +27,7 @@ namespace Origam.ServerCore.Model.Search
     {
         public string Group { get; set; }
         public Guid DataSourceId { get; set; }
-        public string Name { get; set; }
+        public string Label { get; set; }
         public string Description { get; set; }
         public Guid DataSourceLookupId { get; set; }
         public string ReferenceId { get; set; }
