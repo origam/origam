3e11e8ca7332ced50466146a338f00a51f3e6b33
jindrichSusen <jindrich.susen@gmail.com>
2021-01-29 17:53:05 +0100

    INTERNAL: Usage of IMenuItemIcon made more obvoius

--- src/gui/getIconUrl.ts
+++ src/gui/getIconUrl.ts
@@ -1,12 +1,14 @@
-export function getIconUrl(iconName: string) {
+import { IMenuItemIcon } from "./Workbench/MainMenu/MainMenu";
+
+export function getIconUrl(iconName: string | IMenuItemIcon) {
   switch (iconName) {
-    case "menu_form.png":
+    case IMenuItemIcon.Form:
       return "./icons/document.svg";
-    case "menu_workflow.png":
+    case IMenuItemIcon.Workflow:
       return "./icons/settings.svg";
-    case "./icons/work-queue.svg": 
+    case IMenuItemIcon.WorkQueue: 
       return "./icons/work-queue.svg";
-    case "./icons/chat.svg": 
+    case IMenuItemIcon.Chat: 
       return "./icons/chat.svg";
     default:
       return "./icons/document.svg";
