45d306fc5b46166d76d7d5b311c4b5c46c1d898c
Pavel Tomasko <ptomask@gmail.com>
2021-03-01 11:39:34 +0100

    INTERNAL: Keyboard shortcuts for save / refresh.

--- src/gui/Components/ScreenToolbar/ScreenToolbarAction.tsx
+++ src/gui/Components/ScreenToolbar/ScreenToolbarAction.tsx
@@ -1,6 +1,8 @@
 import S from "gui/Components/ScreenToolbar/ScreenToolbarAction.module.scss";
 import React from "react";
 import cx from "classnames";
+import { action } from "mobx";
+import { withTooltip } from "react-tippy";
 
 export class ScreenToolbarAction extends React.Component<{
   icon?: React.ReactNode;
@@ -10,7 +12,51 @@ export class ScreenToolbarAction extends React.Component<{
   className?: string;
   onMouseDown?(event: any): void;
   onClick?(event: any): void;
+  onShortcut?(event: any): void;
+  shortcutPredicate?(event: any): boolean;
 }> {
+  @action.bound
+  handleWindowKeyDown(event: any) {
+    if (this.props.shortcutPredicate?.(event)) {
+      event.preventDefault();
+      this.props.onShortcut?.(event);
+    }
+  }
+
+  kbdHandlerUp() {
+    window.addEventListener("keydown", this.handleWindowKeyDown);
+  }
+
+  kbdHandlerDown() {
+    window.removeEventListener("keydown", this.handleWindowKeyDown);
+  }
+
+  componentDidMount() {
+    if (this.props.onShortcut && this.props.shortcutPredicate) {
+      this.kbdHandlerUp();
+    }
+  }
+
+  componentDidUpdate(prevProps: any) {
+    if (
+      (!prevProps.onShortcut || !prevProps.shortcutPredicate) &&
+      this.props.onShortcut &&
+      this.props.shortcutPredicate
+    ) {
+      this.kbdHandlerUp();
+    } else if (
+      prevProps.onShortcut &&
+      prevProps.shortcutPredicate &&
+      (!this.props.onShortcut || !this.props.shortcutPredicate)
+    ) {
+      this.kbdHandlerDown();
+    }
+  }
+
+  componentWillUnmount() {
+    this.kbdHandlerDown();
+  }
+
   render() {
     return (
       <div
