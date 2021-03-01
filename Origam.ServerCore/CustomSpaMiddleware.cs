d7fe548e35c3d4d07fd24325b30fd232c600a0bd
jindrichSusen <jindrich.susen@gmail.com>
2021-03-01 09:21:42 +0100

    FIX: Default page could not be loaded
    in browsers other than Firefox

---
+++ Origam.ServerCore/CustomSpaMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+
+namespace Origam.ServerCore
+{
+    public static class SpaApplicationBuilderExtensions
+    {
+        public static void UseCustomSpa(this IApplicationBuilder app, string pathToClientApp)
+        {
+            CustomSpaMiddleware.Attach(app, pathToClientApp);
+        }
+    }
+    
+    public static class CustomSpaMiddleware
+    {
+        public static void Attach(IApplicationBuilder app, string pathToClientApp)
+        {
+            app.Use((context, next) =>
+            {
+                if (context.GetEndpoint() != null)
+                {
+                    return next();
+                }
+
+                context.Request.Path = "/index.html";
+                return next();
+            });
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(pathToClientApp)
+            });
+        }
+    }
+}
