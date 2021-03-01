cb5e7e6b71d0980ae044566755418eb79efbe84d
jindrichSusen <jindrich.susen@gmail.com>
2021-03-01 18:00:01 +0100

    FIX: Error was thrown when filtering and grouping by the same column

--- src/model/entities/ServerSideGrouper.ts
+++ src/model/entities/ServerSideGrouper.ts
@@ -164,10 +164,9 @@ export class ServerSideGrouper implements IGrouper {
     }
   }
 
-
   composeFinalFilter(rowGroup: IGroupTreeNode){
     const groupingFilter = rowGroup.composeGroupingFilter();
-    const userFilters = getUserFilters(this);
+    const userFilters = getUserFilters(this, rowGroup.columnId);
 
     return userFilters 
       ? joinWithAND([groupingFilter, userFilters])
