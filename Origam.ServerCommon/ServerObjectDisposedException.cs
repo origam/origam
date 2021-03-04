8639b118597d2b57e64388151b9940028891a93e
jindrichSusen <jindrich.susen@gmail.com>
2021-03-04 11:20:38 +0100

    INTERNAL: Code 474 is returned in case someone tries to interact with a disposed SessionStore

---
+++ Origam.ServerCommon/ServerObjectDisposedException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Origam.ServerCommon
+{
+    public class ServerObjectDisposedException: Exception
+    {
+        public ServerObjectDisposedException(string message) : base(message)
+        {
+        }
+    }
+}
