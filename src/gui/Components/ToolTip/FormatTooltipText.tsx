11f851c61647f619bfca37274527ce1766216264
jindrichSusen <jindrich.susen@gmail.com>
2021-03-04 14:57:52 +0100

    FIX: Tool tips were not interpreting <br/> tags as new lines

--- src/gui/Components/ToolTip/FormatTooltipText.tsx
+++ src/gui/Components/ToolTip/FormatTooltipText.tsx
@@ -6,19 +6,17 @@ export function formatTooltipText(content: string | string[] | undefined) {
   if(!content){
     return "";
   }
-  let lines: string[]=[];
-  if (Array.isArray(content)) {
-    lines = content;
-  } else {
-    lines = content.split("\\r\\n");
-    if (lines.length === 1){
-      lines = lines[0].split("\\n");
-    }
-  }
-  return formatTooltipLines(lines);
+  const lines = Array.isArray(content)
+    ? content.flatMap(line => splitToLines(line))
+    : splitToLines(content);
+  return formatToolTipLines(lines);
 }
 
-function formatTooltipLines(content: string[]) {
+function splitToLines(value: string){
+  return value.split(/\\r\\n|\\n|<br\/>|<BR\/>/);
+}
+
+function formatToolTipLines(content: string[]) {
   const equalLengthLines = content.flatMap(line => line.match(/.{1,72}/g));
   const linesToShow = equalLengthLines.length > 10
     ? equalLengthLines.slice(0, 9).concat(["..."])
