d9b194c96757b306ac1b71047aa2590351fb0387
Pavel Tomasko <ptomask@gmail.com>
2021-03-05 08:53:10 +0100

    INTERNAL: UpdateObject aggregator fix stalled exceptions.

--- src/model/entities/FormScreenLifecycle/UpdateRequestAggregator.tsx
+++ src/model/entities/FormScreenLifecycle/UpdateRequestAggregator.tsx
@@ -1,6 +1,8 @@
 import { getApi } from "model/selectors/getApi";
 import { runInFlowWithHandler } from "utils/runInFlowWithHandler";
 import { IApi, IUpdateData } from "../types/IApi";
+import { interpret, createMachine } from "xstate";
+
 
 interface IUpdateObjectData {
   SessionFormIdentifier: string;
@@ -8,111 +10,178 @@ interface IUpdateObjectData {
   UpdateData: IUpdateData[];
 }
 
-interface IAggregatedRequest {
-  id: string;
-  data: IUpdateData[];
-  sessionFormIdentifier: string;
-  entity: string;
-  promise: Promise<any> | undefined;
-  timeout: NodeJS.Timeout | undefined;
-  promiseResolver: ((value: any | PromiseLike<void>) => void) | undefined;
-  promiseRejector: ((value: any) => void) | undefined;
+interface ExtPromise<T> extends Promise<T> {
+  resolve(result?: T): void;
+  reject(error?: any): void;
+}
+
+function ExtPromise<T>() {
+  let resolveFn;
+  let rejectFn;
+  const promise = new Promise<T>((resolve, reject) => {
+    resolveFn = resolve;
+    rejectFn = reject;
+  });
+  (promise as any).resolve = resolveFn;
+  (promise as any).reject = rejectFn;
+  return promise as ExtPromise<T>;
 }
 
 export class UpdateRequestAggregator {
-  dataMap: Map<string, IAggregatedRequest> = new Map();
-  origamAPI: IApi;
-  ctx: any;
-  previousRequestPromise: Promise<any> | undefined;
-
-  constructor(ctx: any) {
-    this.ctx = ctx;
-    this.origamAPI = getApi(ctx);
+  constructor(
+    private api: { updateObject(data: IUpdateObjectData): Promise<any> }
+  ) {
+    this.interpreter
+      .onTransition((state, event) => {
+        console.log("TR", state, event);
+      })
+      .start();
   }
+
+  registeredRequests: Array<
+    IUpdateObjectData & { promise: ExtPromise<any>; isRunning: boolean }
+  > = [];
+
+  itemJustEnqueued?: IUpdateObjectData & { promise: ExtPromise<any>; isRunning: boolean };
+
+  currentRequest?: IUpdateObjectData & { promise: ExtPromise<any> };
+
+  interpreter = interpret(
+    createMachine(
+      {
+        id: "updateObject",
+        initial: "IDLE",
+        states: {
+          IDLE: {
+            on: {
+              UPDATE_REQUESTED: {
+                actions: ["enqueueRequest"],
+                target: "UPDATE_DEBOUNCING",
+              },
+            },
+          },
+          UPDATE_DEBOUNCING: {
+            on: {
+              UPDATE_REQUESTED: {
+                actions: ["enqueueRequest"],
+                target: "UPDATE_DEBOUNCING",
+              },
+            },
+            after: {
+              5000: "DEQUEUE",
+            },
+          },
+          DEQUEUE: {
+            entry: "dequeueToCurrentRequest",
+            always: "UPDATING",
+          },
+          UPDATING: {
+            initial: "API_RUNNING",
+            states: {
+              API_RUNNING: {
+                invoke: {
+                  src: "apiUpdateObject",
+                  onDone: "API_RESOLVED",
+                  onError: "API_REJECTED",
+                },
+              },
+              API_RESOLVED: {
+                entry: ["resolveCurrentPromise"],
+                type: "final",
+              },
+              API_REJECTED: {
+                entry: ["rejectCurrentPromise"],
+                type: "final",
+              },
+            },
+            on: {
+              UPDATE_REQUESTED: {
+                actions: ["enqueueRequest"],
+              },
+            },
+            onDone: [
+              { cond: "hasMoreItems", target: "#updateObject.DEQUEUE" },
+              { target: "#updateObject.IDLE" },
+            ],
+            exit: ["resetCurrentRequest"],
+          },
+        },
+      },
+      {
+        services: {
+          apiUpdateObject: (ctx, event) => {
+            console.log("Invoking apiUpdateRequest", this.currentRequest);
+            return this.api.updateObject(this.currentRequest!);
+          },
+        },
+        actions: {
+          enqueueRequest: (ctx, event) => {
+            console.log("Enqueuing: ", event.payload);
+            const updateObjectItem = this.mergeToQueue(event.payload);
+            this.itemJustEnqueued = updateObjectItem;
+            console.log(JSON.stringify(this.registeredRequests, null, 2));
+          },
+          dequeueToCurrentRequest: (ctx, event) => {
+            const item = this.registeredRequests.shift();
+            this.currentRequest = item;
+            console.log("Dequeued: ", item);
+          },
+          resolveCurrentPromise: (ctx, event) => {
+            this.currentRequest?.promise.resolve(event.data);
+          },
+          rejectCurrentPromise: (ctx, event) => {
+            this.currentRequest?.promise.reject(event.data);
+          },
+          resetCurrentRequest: (ctx, event) => {
+            this.currentRequest = undefined;
+          },
+        },
+        guards: {
+          hasMoreItems: (ctx, event) => {
+            return this.registeredRequests.length > 0;
+          },
+        },
+      }
+    ),
+    { devTools: true }
+  );
 
   enqueue(data: IUpdateObjectData): Promise<any> {
-    const aggregatedRequest = this.getAggregateRequest(data);
-    if (!aggregatedRequest.timeout) {
-      aggregatedRequest.timeout = setTimeout(async () => {
-        try {
-          await runInFlowWithHandler({
-            ctx: this.ctx,
-            action: async () => {
-              await this.waitForRunningRequest(aggregatedRequest);
-              await this.resolveAggregateRequest(aggregatedRequest, data);
-            },
-          });
-        } catch (e) {
-          aggregatedRequest.promiseRejector?.(e);
-        }
-      }, 130);
-    }
-
-    return aggregatedRequest.promise!;
-  }
-
-  private async resolveAggregateRequest(
-    aggregatedRequest: IAggregatedRequest,
-    data: IUpdateObjectData
-  ) {
-    aggregatedRequest.timeout = undefined;
-    const resolver = aggregatedRequest.promiseResolver;
-    const updateData = aggregatedRequest.data!;
-    this.dataMap.delete(aggregatedRequest.id);
-    const resultData = await this.origamAPI.updateObject({
-      SessionFormIdentifier: data.SessionFormIdentifier,
-      Entity: data.Entity,
-      UpdateData: updateData,
+    
+    this.interpreter.send({
+      type: "UPDATE_REQUESTED",
+      payload: data,
     });
-    resolver?.(resultData);
+    const {itemJustEnqueued} = this;
+    this.itemJustEnqueued = undefined;
+    return itemJustEnqueued!.promise;
   }
 
-  private async waitForRunningRequest(aggregatedRequest: IAggregatedRequest) {
-    if (this.previousRequestPromise) {
-      const previousCall = this.previousRequestPromise;
-      this.previousRequestPromise = aggregatedRequest.promise;
-      await previousCall;
-    } else {
-      this.previousRequestPromise = aggregatedRequest.promise;
+  mergeToQueue(data: IUpdateObjectData) {
+    let updateObjectItem = this.registeredRequests.find(
+      (item) => item.Entity === data.Entity && !item.isRunning
+    );
+    if (!updateObjectItem) {
+      updateObjectItem = {
+        SessionFormIdentifier: data.SessionFormIdentifier,
+        Entity: data.Entity,
+        UpdateData: data.UpdateData,
+        promise: ExtPromise(),
+        isRunning: false,
+      };
+      this.registeredRequests.push(updateObjectItem);
     }
-    this.previousRequestPromise = aggregatedRequest.promise;
-  }
 
-  private getAggregateRequest(data: IUpdateObjectData) {
-    const requestId = data.Entity + data.SessionFormIdentifier;
-    if (!this.dataMap.has(requestId)) {
-      let promiseResolver: ((value: any | PromiseLike<void>) => void) | undefined;
-      let promiseRejector: ((value: any) => void) | undefined;
-      const promise = new Promise<any>((resolve, reject) => {
-        promiseResolver = resolve;
-        promiseRejector = reject;
-      });
-      this.dataMap.set(requestId, {
-        id: requestId,
-        sessionFormIdentifier: data.SessionFormIdentifier,
-        entity: data.Entity,
-        data: [...(this.dataMap.get(requestId)?.data ?? []), ...data.UpdateData],
-        promise: promise,
-        promiseResolver,
-        promiseRejector,
-        timeout: undefined,
-      });
-    } else {
-      const dataByRowId = new Map<any, IUpdateData>();
-      const existingUpdates = this.dataMap.get(requestId)!.data;
-      for (let existingUpdate of existingUpdates) {
-        dataByRowId.set(existingUpdate.RowId, existingUpdate);
+    for (let inputRow of data.UpdateData) {
+      let existingRow = updateObjectItem.UpdateData.find(
+        (item) => item.RowId === inputRow.RowId
+      );
+      if (!existingRow) {
+        existingRow = { RowId: inputRow.RowId, Values: {} };
+        updateObjectItem.UpdateData.push(existingRow);
       }
-      for (let newUpdate of data.UpdateData) {
-        if (dataByRowId.has(newUpdate.RowId)) {
-          const dataItem = dataByRowId.get(newUpdate.RowId)!;
-          dataItem.Values = { ...dataItem.Values, ...newUpdate.Values };
-        } else {
-          dataByRowId.set(newUpdate.RowId, newUpdate);
-        }
-      }
-      this.dataMap.get(requestId)!.data = Array.from(dataByRowId.values());
+      existingRow.Values = { ...existingRow.Values, ...inputRow.Values };
     }
-    return this.dataMap.get(requestId)!;
+    return updateObjectItem;
   }
 }
