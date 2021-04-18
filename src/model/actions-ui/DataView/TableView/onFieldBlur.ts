64fa4131bc05b976409f78b930b7d9ab3e002d9d
Pavel Tomasko <ptomask@gmail.com>
2021-04-18 18:58:08 +0200

    INTERNAL: Added critical section to synchronize leaving a form field and clicking an action.
    It is requested to call updateoject first and then executeaction. This used to be achieved by dom event order (field blur fires before action click) but was broken by introducing updateobject request batching, which yields to the js event loop (includes a delay), letting action click event to be handled first.
    The fix reestablishes the previous approach.

--- src/model/actions-ui/DataView/TableView/onFieldBlur.ts
+++ src/model/actions-ui/DataView/TableView/onFieldBlur.ts
@@ -1,11 +1,16 @@
-import {flushCurrentRowData} from "../../../actions/DataView/TableView/flushCurrentRowData";
-import {flow} from "mobx";
-import {handleError} from "model/actions/handleError";
+import { flushCurrentRowData } from "../../../actions/DataView/TableView/flushCurrentRowData";
+import { flow } from "mobx";
+import { handleError } from "model/actions/handleError";
+import { crs_fieldBlur_ActionClick } from "model/actions/actionSync";
 
 export function onFieldBlur(ctx: any) {
   return flow(function* onFieldBlur(event: any) {
     try {
-      yield* flushCurrentRowData(ctx)();
+      yield* crs_fieldBlur_ActionClick.runGenerator(function*() {
+        console.log("EDITOR BLUR:", ctx);
+        yield* flushCurrentRowData(ctx)();
+        console.log("BLUR EVENT HANDLER FINISHED");
+      })
     } catch (e) {
       yield* handleError(ctx)(e);
       throw e;
