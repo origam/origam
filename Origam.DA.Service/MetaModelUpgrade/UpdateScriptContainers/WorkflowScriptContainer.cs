535ca2f79f5200d72f776e97e9f9961a05ed0823
Jindrich Susen <jindrich.susen@hotmail.com>
2021-04-20 14:27:12 +0200

    INTERNAL: AbstractWorkflowStep meta model upgraded to 6.0.1

--- Origam.DA.Service/MetaModelUpgrade/UpdateScriptContainers/WorkflowScriptContainer.cs
+++ Origam.DA.Service/MetaModelUpgrade/UpdateScriptContainers/WorkflowScriptContainer.cs
@@ -19,5 +19,23 @@ namespace Origam.DA.Service.MetaModelUpgrade.UpdateScriptContainers
                 (node, doc) =>
                     RemoveAttribute(node, "traceLevel")));
         }
+    }  
+    
+    public class AbstractWorkflowStepScriptContainer : UpgradeScriptContainer
+    {
+        public override string FullTypeName { get; } =
+            typeof(Schema.WorkflowModel.AbstractWorkflowStep).FullName;
+
+        public override List<string> OldFullTypeNames { get; }
+        public override string[] OldPropertyXmlNames { get; }
+
+        public AbstractWorkflowStepScriptContainer()
+        {
+            upgradeScripts.Add(new UpgradeScript(
+                new Version("6.0.0"),
+                new Version("6.0.1"),
+                (node, doc) =>
+                    RemoveAttribute(node, "traceLevel")));
+        }
     }
 }
