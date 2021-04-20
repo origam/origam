0af45bb21e530cab9f88c121f7598bf0a2c279ef
Jindrich Susen <jindrich.susen@hotmail.com>
2021-04-20 11:27:10 +0200

    INTERNAL: Value conversion refactored into a new class

---
+++ Origam.DA.Service/InstanceTools.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Xml;
+using Origam.DA.ObjectPersistence;
+
+namespace Origam.DA.Service
+{
+    static class InstanceTools
+    {
+        public static object GetCorrectlyTypedValue(Type memberType, object value)
+        {
+            object correctlyTypedValue;
+            // If member is enum, we have to convert
+            if (memberType.IsEnum )
+            {
+                correctlyTypedValue = value == null ? null :
+                    Enum.Parse(memberType, (string)value);
+            }
+            else if (memberType == typeof(int))
+            {
+                correctlyTypedValue = value == null ? 0 :
+                    XmlConvert.ToInt32((string)value);
+            }
+            else if (memberType == typeof(bool))
+            {
+                correctlyTypedValue = value != null &&
+                                      XmlConvert.ToBoolean((string)value);
+            }
+            else if (memberType == typeof(Guid))
+            {
+                if (value != null)
+                {
+                    correctlyTypedValue 
+                        = value is Guid ? value : new Guid((string)value);
+                }
+                else
+                {
+                    correctlyTypedValue = null;
+                }
+            }
+            else
+            {
+                correctlyTypedValue = value;
+            }
+
+            return correctlyTypedValue;
+        }       
+    }
+}
