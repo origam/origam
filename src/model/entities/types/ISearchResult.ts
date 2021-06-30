14b534ea1177ef093de78c4ac681533e06511848
Jindrich Susen <jindrich.susen@advantages.cz>
2021-06-30 09:47:32 +0200

    INTERNAL: Code cleanup

--- src/model/entities/types/ISearchResult.ts
+++ src/model/entities/types/ISearchResult.ts
@@ -17,7 +17,6 @@ You should have received a copy of the GNU General Public License
 along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import {IMenuItemIcon} from "gui/Workbench/MainMenu/IMenuItemIcon";
 
 export interface ISearchResult
 {
