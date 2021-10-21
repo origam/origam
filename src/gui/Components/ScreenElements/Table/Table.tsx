27a3cbb850c3e0c7dc95188c7bf8086a5e6e75fd
JindrichSusen <33699252+JindrichSusen@users.noreply.github.com>
2021-10-21 09:58:29 +0200

    Column moving (#313)
    * Revert "FIX: Table editor remained open while the modal dialog was open"
    
    This reverts commit c0895e017a5e91f8f6cec29634c1ede09940d861.
    
    * FIX: Table editor remained open while the modal dialog was open
    
    was fixed before but the previous solution broke double clicking
    
    * FIX: Drag and drop on grid headers did not work when the grid was scrolled to the right
    
    * FIX: Column separators were not as tall as the headers
    
    when the aggregations were displayed

--- src/gui/Components/ScreenElements/Table/Table.tsx
+++ src/gui/Components/ScreenElements/Table/Table.tsx
@@ -437,7 +437,10 @@ export class RawTable extends React.Component<ITableProps & { isVisible: boolean
                         <DragDropContext onDragEnd={(result) => this.onColumnDragEnd(result)}>
                           <Droppable droppableId="headers" direction="horizontal">
                             {(provided) => (
-                              <div  {...provided.droppableProps} ref={provided.innerRef}>
+                              // width:"10000%" - when the table was horizontally scrolled to the right the drag
+                              // and drop did not work on the headers that were outside of the originally visible area.
+                              // This div has to be at least as wide as ALL its children, not just the visible ones.
+                              <div style={{width:"10000%"}}  {...provided.droppableProps} ref={provided.innerRef}>
                                 <HeaderRow headerElements={[...this.freeHeaders, provided.placeholder as any]}/>
                               </div>
                             )}
