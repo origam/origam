d5b4a63e3ebdcc2502a081e5fbbe4c3dcbdedf9f
Petr Hrehorovsky <83349812+washibana@users.noreply.github.com>
2021-11-16 10:28:20 +0100

    INTERNAL: ReportHelper refactoring (#365)

--- Origam.BI/ReportHelper.cs
+++ Origam.BI/ReportHelper.cs
@@ -20,7 +20,6 @@ along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
 #endregion
 
 using System;
-using System.Xml;
 using System.Collections;
 using Origam.Rule;
 
@@ -33,73 +32,80 @@ using core = Origam.Workbench.Services.CoreServices;
 
 namespace Origam.BI
 {
-	/// <summary>
-	/// Summary description for ReportHelper.
-	/// </summary>
-	public class ReportHelper
+	public static class ReportHelper
 	{
 		public static void LogInfo(Type type, string message)
 		{
-			log4net.ILog log = log4net.LogManager.GetLogger(type);
-
+			var log = log4net.LogManager.GetLogger(type);
 			if(log.IsInfoEnabled)
 			{
 				log.Info(message);
 			}
 		}
 
-        public static void ComputeXsltValueParameters(AbstractReport report, 
-            Hashtable parameters, TraceTaskInfo traceTaskInfo = null)
+        public static void ComputeXsltValueParameters(
+            AbstractReport report, 
+            Hashtable parameters, 
+            TraceTaskInfo traceTaskInfo = null)
         {
-            if (parameters == null) return;
-            IPersistenceService persistence = ServiceManager.Services
-                .GetService(typeof(IPersistenceService)) as IPersistenceService;
-            RuleEngine ruleEngine = new RuleEngine(null, null);
-
+            if(parameters == null)
+            {
+                return;
+            }
+            var persistence = ServiceManager.Services
+                .GetService<IPersistenceService>();
+            var ruleEngine = new RuleEngine(null, null);
             var transformParams = new Hashtable();
-            foreach (SchemaItemParameter parameter in report.Parameters)
+            foreach(SchemaItemParameter parameter in report.Parameters)
             {
                 // send all ordinary parameters as an input to the Xslt
-                if (parameter != null && !(parameter is XsltInitialValueParameter))
+                if((parameter != null) 
+                && !(parameter is XsltInitialValueParameter))
                 {
-                    transformParams.Add(parameter.Name, parameters[parameter.Name]);
+                    transformParams.Add(
+                        parameter.Name, parameters[parameter.Name]);
                 }
             }
             string oldStepName = null;
-            if (traceTaskInfo != null)
+            if(traceTaskInfo != null)
             {
                 oldStepName = traceTaskInfo.TraceStepName;
             }
-            foreach (SchemaItemParameter parameter in report.Parameters)
+            foreach(SchemaItemParameter parameter in report.Parameters)
             {
-                XsltInitialValueParameter xsltParameter = parameter as XsltInitialValueParameter;
-                if (xsltParameter == null)
+                var xsltParameter = parameter as XsltInitialValueParameter;
+                if(xsltParameter == null)
                 {
                     continue;
                 }
                 // do not recompute parameters if they were sent and they have some value
-                if (parameters.ContainsKey(xsltParameter.Name) 
-                    && parameters[xsltParameter.Name] != null)
+                if(parameters.ContainsKey(xsltParameter.Name) 
+                && (parameters[xsltParameter.Name] != null))
                 {
                     continue;
                 }
-                IXsltEngine transformer = AsTransform.GetXsltEngine(
+                var transformer = AsTransform.GetXsltEngine(
                     persistence.SchemaProvider,
                     xsltParameter.transformationId);
                 IXmlContainer xmlData = new XmlContainer("<ROOT/>");
-                if (traceTaskInfo != null)
+                if(traceTaskInfo != null)
                 {
-                    traceTaskInfo.TraceStepName = string.Format(
-                        "{0}/ComputeParam_{1}", oldStepName,
-                        xsltParameter.Name);
+                    traceTaskInfo.TraceStepName =
+                        $"{oldStepName}/ComputeParam_{xsltParameter.Name}";
                     transformer.SetTraceTaskInfo(traceTaskInfo);
                 }
-                IXmlContainer result = transformer.Transform(xmlData,
+                var result = transformer.Transform(
+                    xmlData,
                     xsltParameter.transformationId,
-                    Guid.Empty, transformParams, null, ruleEngine, null, false);
-                XmlNode resultNode = result.Xml.SelectSingleNode("/ROOT/value");
-                // add a newlu created computed parameter
-                if (resultNode == null)
+                    Guid.Empty, 
+                    transformParams, 
+                    null, 
+                    ruleEngine, 
+                    null, 
+                    false);
+                var resultNode = result.Xml.SelectSingleNode("/ROOT/value");
+                // add a newly created computed parameter
+                if(resultNode == null)
                 {
                     parameters.Add(xsltParameter.Name, null);
                 }
@@ -107,11 +113,12 @@ namespace Origam.BI
                 {
                     object valueToContext = resultNode.InnerText;
                     RuleEngine.ConvertStringValueToContextValue(
-                        xsltParameter.DataType, resultNode.InnerText, ref valueToContext);
+                        xsltParameter.DataType, resultNode.InnerText, 
+                        ref valueToContext);
                     parameters.Add(xsltParameter.Name, valueToContext);
                 }
             }
-            if (traceTaskInfo != null)
+            if(traceTaskInfo != null)
             {
                 traceTaskInfo.TraceStepName = oldStepName;
             }
@@ -120,23 +127,23 @@ namespace Origam.BI
         public static string BuildFileSystemReportFilePath(
             string filePath, Hashtable parameters)
         {
-            foreach (DictionaryEntry entry in parameters)
+            foreach(DictionaryEntry entry in parameters)
             {
-                string sKey = entry.Key.ToString();
-                string sValue = null;
-                if (entry.Value != null)
+                var key = entry.Key.ToString();
+                string value = null;
+                if(entry.Value != null)
                 {
-                    sValue = entry.Value.ToString();
+                    value = entry.Value.ToString();
                 }
-                string replacement = "{" + sKey + "}";
-                if (filePath.IndexOf(replacement) > -1)
+                var replacement = "{" + key + "}";
+                if(filePath.IndexOf(replacement, StringComparison.Ordinal) > -1)
                 {
-                    if (sValue == null)
+                    if(value == null)
                     {
-                        throw new Exception(
-                            "parametry nesedi");
+                        throw new Exception(ResourceUtils.GetString(
+                            "ParametersDontMatch"));
                     }
-                    filePath = filePath.Replace(replacement, sValue);
+                    filePath = filePath.Replace(replacement, value);
                 }
             }
             return filePath;
@@ -145,20 +152,19 @@ namespace Origam.BI
         public static string ExpandCurlyBracketPlaceholdersWithParameters(
             string input, Hashtable parameters)
         {
-            string output = input;
-
-            foreach (DictionaryEntry entry in parameters)
+            var output = input;
+            foreach(DictionaryEntry entry in parameters)
             {
-                string sKey = entry.Key.ToString();
-                string sValue = "";
-                if (entry.Value != null)
+                var key = entry.Key.ToString();
+                var value = "";
+                if(entry.Value != null)
                 {
-                    sValue = entry.Value.ToString();
+                    value = entry.Value.ToString();
                 }
-                string replacement = "{" + sKey + "}";
-                if (output.IndexOf(replacement) > -1)
+                var replacement = "{" + key + "}";
+                if(output.IndexOf(replacement, StringComparison.Ordinal) > -1)
                 {
-                    output = output.Replace(replacement, sValue);
+                    output = output.Replace(replacement, value);
                 }
             }
             return output;
@@ -166,9 +172,8 @@ namespace Origam.BI
 
         public static void LogError(Type type, string message)
 		{
-			log4net.ILog log = log4net.LogManager.GetLogger(type);
-
-			if (log.IsErrorEnabled)
+			var log = log4net.LogManager.GetLogger(type);
+			if(log.IsErrorEnabled)
 			{
 				log.Error(message);
 			}
@@ -176,27 +181,28 @@ namespace Origam.BI
 
 		public static void PopulateDefaultValues(AbstractReport report, 
             Hashtable parameters)
-		{
-			IParameterService pms = ServiceManager.Services
-                .GetService(typeof(IParameterService)) as IParameterService;
-
-			foreach(object parameter in report.Parameters)
+        {
+            var parameterService = ServiceManager.Services
+                .GetService<IParameterService>();
+			foreach(var parameter in report.Parameters)
 			{
-                if (parameter is DefaultValueParameter defaultParam)
+                if(parameter is DefaultValueParameter defaultParam)
                 {
-                    if (parameters.Contains(defaultParam.Name))
+                    if(parameters.Contains(defaultParam.Name))
                     {
-                        object paramValue = parameters[defaultParam.Name];
-                        if (paramValue == null || paramValue == DBNull.Value)
+                        var paramValue = parameters[defaultParam.Name];
+                        if((paramValue == null) || (paramValue == DBNull.Value))
                         {
                             parameters[defaultParam.Name] = 
-                                pms.GetParameterValue(defaultParam.DefaultValue.Id);
+                                parameterService.GetParameterValue(
+                                    defaultParam.DefaultValue.Id);
                         }
                     }
                     else
                     {
                         parameters[defaultParam.Name] =
-                            pms.GetParameterValue(defaultParam.DefaultValue.Id);
+                            parameterService.GetParameterValue(
+                                defaultParam.DefaultValue.Id);
                     }
                 }
             }
@@ -207,50 +213,65 @@ namespace Origam.BI
                 .GetService<IPersistenceService>();
 			var report = persistence.SchemaProvider
                 .RetrieveInstance<T>(reportId);
-			if (report == null)
+			if(report == null)
 			{
-				throw new ArgumentOutOfRangeException(
-                    "reportId",
-                    reportId, 
+				throw new ArgumentException(
+                    @"reportId",
                     ResourceUtils.GetString("DefinitionNotInModel"));
 			}
 			return report;
 		}
 
-		public static string ResolveLanguage(IXmlContainer doc, AbstractDataReport reportElement)
+		public static string ResolveLanguage(
+            IXmlContainer doc, AbstractDataReport reportElement)
 		{
-			if (string.IsNullOrEmpty(reportElement.LocaleXPath)) return null;
-			RuleEngine ruleEngine = new RuleEngine(null, null);
-
-			//XmlDocument xPathXMLDoc = ruleEngine.   GetXmlDocumentFromData(updateTask.XPathContextStore);
-			string cultureString = (string) ruleEngine.EvaluateContext(reportElement.LocaleXPath, doc, OrigamDataType.String, null);
-			//reportElement.localeXPath;
+            if(string.IsNullOrEmpty(reportElement.LocaleXPath))
+            {
+                return null;
+            }
+			var ruleEngine = new RuleEngine(null, null);
+			var cultureString = (string)ruleEngine.EvaluateContext(
+                reportElement.LocaleXPath, doc, OrigamDataType.String, null);
 			return cultureString;
 		}
 
-		public static IDataDocument LoadOrUseReportData(AbstractDataReport r,
-		    IXmlContainer data, Hashtable parameters, string dbTransaction)
+		public static IDataDocument LoadOrUseReportData(
+            AbstractDataReport report,
+		    IXmlContainer data, 
+            Hashtable parameters, 
+            string dbTransaction)
 		{
-			if (data == null && r.DataStructure != null)
-			{
-				QueryParameterCollection qp = new QueryParameterCollection();
-                if (parameters != null)
+			switch(data)
+            {
+                case null when report.DataStructure != null:
                 {
-                    foreach (DictionaryEntry entry in parameters)
+                    var queryParameterCollection 
+                        = new QueryParameterCollection();
+                    if(parameters != null)
                     {
-                        qp.Add(new QueryParameter((string)entry.Key, entry.Value));
+                        foreach (DictionaryEntry entry in parameters)
+                        {
+                            queryParameterCollection.Add(
+                                new QueryParameter(
+                                    (string)entry.Key, entry.Value));
+                        }
                     }
+                    return DataDocumentFactory.New(
+                        core.DataService.LoadData(
+                            report.DataStructureId,
+                            report.DataStructureMethodId, 
+                            Guid.Empty, 
+                            report.DataStructureSortSetId, 
+                            dbTransaction, 
+                            queryParameterCollection));
                 }
-				return DataDocumentFactory.New(
-                    core.DataService.LoadData(r.DataStructureId,
-                    r.DataStructureMethodId, Guid.Empty, 
-                    r.DataStructureSortSetId, dbTransaction, qp));
-			}
-			if (data is IDataDocument)
-			{
-				return data as IDataDocument;
-			}
-		    throw new ArgumentOutOfRangeException("data", data, ResourceUtils.GetString("OnlyXmlDocSupported"));	
-		}
+                case IDataDocument document:
+                    return document;
+                default:
+                    throw new ArgumentException(
+                        @"data", 
+                        ResourceUtils.GetString("OnlyXmlDocSupported"));
+            }
+        }
 	}	
 }
