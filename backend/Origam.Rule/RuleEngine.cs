a934c24df1d860c63d6baf62d719e43c111b77c0
JindrichSusen <33699252+JindrichSusen@users.noreply.github.com>
2021-11-29 18:41:13 +0100

    FIX: Rule ended in an infinite loop after setting RelativePath to true (#30)

--- backend/Origam.Rule/RuleEngine.cs
+++ backend/Origam.Rule/RuleEngine.cs
@@ -3263,6 +3263,10 @@ namespace Origam.Rule
 							t = t.ParentRelations[0].ParentTable;
 							path = t.TableName + "/" + path;
 						}
+						else
+						{
+							break;
+						}
 					}
 
 					path = "/" + data.DataSet.DataSetName + "/" + path;
