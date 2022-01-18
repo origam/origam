da52c083692ee17aeca6e13073b0a9f8088a82a3
Jindrich Susen <jindrich.susen@advantages.cz>
2022-01-18 13:05:11 +0100

    FIX: Filter inputs were loosing focus after data was updated in lazy loaded tables

--- frontend-html/src/model/entities/GridFocusManager.ts
+++ frontend-html/src/model/entities/GridFocusManager.ts
@@ -1,6 +1,7 @@
 import { getTablePanelView } from "../selectors/TablePanelView/getTablePanelView";
 import { getDataView } from "../selectors/DataView/getDataView";
 import { IFocusable } from "./FormFocusManager";
+import { getFilterConfiguration } from "model/selectors/DataView/getFilterConfiguration";
 
 export class GridFocusManager {
 
@@ -10,7 +11,9 @@ export class GridFocusManager {
   public focusTableOnReload: boolean = true;
 
   focusTableIfNeeded() {
-    if (this.focusTableOnReload) {
+    const filtersVisible = getFilterConfiguration(this.parent).isFilterControlsDisplayed;
+    const filterInputHasFocus = filtersVisible && document.activeElement?.tagName === 'INPUT';
+    if (this.focusTableOnReload && !filterInputHasFocus) {
       getTablePanelView(this)?.triggerOnFocusTable();
     } else {
       this.focusTableOnReload = true;
