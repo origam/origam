b136ddcd1c0815fe21ff07a5d260edebadc42262
Jindrich Susen <jindrich.susen@advantages.cz>
2022-01-19 10:57:21 +0100

    INTERNAL: OrigamServerCommon project merged into OrigamServerCore

---
+++ backend/Origam.ServerCore/Common/LookupListExRequest.cs
@@ -0,0 +1,60 @@
+#region license
+/*
+Copyright 2005 - 2021 Advantage Solutions, s. r. o.
+
+This file is part of ORIGAM (http://www.origam.org).
+
+ORIGAM is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ORIGAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+#region license
+/*
+Copyright 2005 - 2021 Advantage Solutions, s. r. o.
+
+This file is part of ORIGAM.
+
+ORIGAM is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ORIGAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with ORIGAM.  If not, see<http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using System.Collections.Generic;
+
+namespace Origam.Server
+{
+    public class LookupListExRequest
+    {
+        public string SessionFormIdentifier; 
+        public string Entity; 
+        public string Property; 
+        public object Id; 
+        public string LookupId; 
+        public IDictionary<string, object> Parameters; 
+        public bool ShowUniqueValues;
+        public string SearchText;
+        public int PageSize = -1;
+        public int PageNumber = -1;
+    }
+}
