bbe87cf6c0fd333b436eacfc31904600c97bceb6
Jiri Prajz <jiri.prajz@advantages.cz>
2022-03-21 09:10:13 +0100

    INTERNAL: removed development message.

--- test/startTests.sh
+++ test/startTests.sh
@@ -4,9 +4,9 @@ cd /home/origam/HTML5
 ./startServerTest.sh 
 echo "TEST DB Connection"
 DATAOUT=$(dotnet origam-utils.dll test-db -t 10 -d 5000 -c "select 1")
-echo "Result IS $DATAOUT !!!!!!!!!!!!!!!!!!" ;
+#echo "Result IS $DATAOUT !!!!!!!!!!!!!!!!!!" ;
 if [[ "$DATAOUT" != True ]]; then
-echo "Problem with connection to database";
+echo "Database connection failed";
 exit 1;
 fi 
 export ASPNETCORE_URLS="http://+:8080"
@@ -14,7 +14,7 @@ dotnet Origam.Server.dll &
 echo "TEST DB Connection"
 DATAOUT=$(dotnet origam-utils.dll test-db -t 5 -d 5000 -c "SELECT 1 FROM dbo.\"OrigamModelVersion\" where \"refSchemaExtensionId\"='${OrigamSettings_SchemaExtensionGuid}'")
 if [[ "$DATAOUT" != True ]]; then
-echo "Problem with connection to database";
+echo "Database connection failed";
 exit 1;
 fi
 cd tests_e2e
