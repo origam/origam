ab417bd191aaff565359776353936e37a6c1e4d6
Jindrich Susen <jindrich.susen@advantages.cz>
2022-03-28 12:31:18 +0200

    INTERNAL: Screen heading is printed

--- frontend-html/src/gui/Components/DataViewHeader/DataViewHeader.tsx
+++ frontend-html/src/gui/Components/DataViewHeader/DataViewHeader.tsx
@@ -22,7 +22,7 @@ import S from "gui/Components/DataViewHeader/DataViewHeader.module.scss";
 import cx from 'classnames';
 
 export const DataViewHeader: React.FC<{ domRef?: any, isVisible: boolean }> = props => (
-  <div className={cx(S.root, {isVisible: props.isVisible})}>
+  <div className={cx(S.root, {isVisible: props.isVisible}, "noPrint")}>
     <div ref={props.domRef} className={S.inner}>
       {props.children}
     </div>
