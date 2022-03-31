37796089ba551046eb1201d067ab06d90fc5fea2
JindrichSusen <33699252+JindrichSusen@users.noreply.github.com>
2022-03-31 20:27:49 +0200

    Screen print (#405)
    * NEW: Only screen area is printed
    
    * INTERNAL: Screen heading is printed
    
    * INTERNAL: Reverted accidental plugin registration

--- frontend-html/src/gui/Components/Splitter/Splitter.tsx
+++ frontend-html/src/gui/Components/Splitter/Splitter.tsx
@@ -105,6 +105,7 @@ class SplitterDivider extends React.Component<{
 export class Splitter extends React.Component<{
   type: "isHoriz" | "isVert";
   sizeOverrideFirstPanel?: number;
+  dontPrintLeftPane?: boolean;
   id?: string;
   panels: Array<[any, number, React.ReactNode]>;
   onSizeChangeFinished?(
@@ -281,7 +282,7 @@ export class Splitter extends React.Component<{
       const panel = this.props.panels[i];
       content.push(
         <SplitterPanel
-          className={(this.props.STYLE || S).panel}
+          className={(this.props.STYLE || S).panel + (i === 0 && this.props.dontPrintLeftPane ? " noPrint" : "")}
           type={this.props.type}
           size={this.sizeMap.get(panel[0])!}
           key={`S${panel[0]}`}
