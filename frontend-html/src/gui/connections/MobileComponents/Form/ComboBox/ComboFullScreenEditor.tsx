9ecc2c671b2c641bd83977553c5944850724e514
Jindrich Susen <jindrich.susen@advantages.cz>
2022-04-06 11:56:15 +0200

    FIX: Mobile dropdown did not expand to the whole screen

--- frontend-html/src/gui/connections/MobileComponents/Form/ComboBox/ComboFullScreenEditor.tsx
+++ frontend-html/src/gui/connections/MobileComponents/Form/ComboBox/ComboFullScreenEditor.tsx
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import S from "gui/connections/MobileComponents/Form/ComboBox/ComboFullScreenEditor.module.scss";
 import cx from "classnames";
 import CS from "@origam/components/src/components/Dropdown/Dropdown.module.scss";
@@ -27,6 +27,7 @@ import { MobileDropdownBehavior } from "gui/connections/MobileComponents/Form/Co
 import { DropdownColumnDrivers, DropdownDataTable } from "modules/Editors/DropdownEditor/DropdownTableModel";
 import { MobileDropdownEditorInput } from "gui/connections/MobileComponents/Form/ComboBox/MobileDropdownEditorInput";
 import { IDropdownEditorData } from "modules/Editors/DropdownEditor/DropdownEditorData";
+import Measure from "react-measure";
 
 
 interface IComboFullScreenEditorProps {
@@ -42,38 +43,48 @@ interface IComboFullScreenEditorProps {
 
 export const ComboFullScreenEditor: React.FC<IComboFullScreenEditorProps> = (props) => {
 
-  const ref = useContext(CtxDropdownRefCtrl);
-
   useEffect(()=>{
     props.behavior.handleInputChange({target:{value:""}});
     props.behavior.makeFocused();
   });
 
+  const [height, setHeight] = useState<number>(150);
+
   return (
-    <div className={cx(CS.control, S.root)} ref={ref}>
-      <MobileDropdownEditorInput
-        backgroundColor={props.backgroundColor}
-        foregroundColor={props.foregroundColor}
-        customStyle={props.customStyle}
-        isLink={props.isLink}
-        behavior={props.behavior}
-        editorData={props.editorData}
-      />
-      <DropdownEditorTable
-        drivers={props.columnDrivers}
-        dataTable={props.dataTable}
-        rectCtrl={{
-          top: 0,
-          left: 0,
-          width: window.screen.width,
-          height: 0,
-          bottom: 0,
-          right: 0,
-        }}
-        beh={props.behavior}
-        rowHeight={30}
-      />
-    </div>
+    <Measure
+      bounds
+      onResize={contentRect => {
+        setHeight(contentRect.bounds?.height ?? 150);
+      }}
+    >
+      {({ measureRef }) => (
+        <div className={cx(CS.control, S.root)} ref={measureRef}>
+          <MobileDropdownEditorInput
+            backgroundColor={props.backgroundColor}
+            foregroundColor={props.foregroundColor}
+            customStyle={props.customStyle}
+            isLink={props.isLink}
+            behavior={props.behavior}
+            editorData={props.editorData}
+          />
+          <DropdownEditorTable
+            drivers={props.columnDrivers}
+            dataTable={props.dataTable}
+            rectCtrl={{
+              top: 0,
+              left: 0,
+              width: window.screen.width,
+              height: 0,
+              bottom: 0,
+              right: 0,
+            }}
+            beh={props.behavior}
+            rowHeight={30}
+            height={height - 25} // 25 is height of MobileDropdownEditorInput
+          />
+        </div>
+      )}
+    </Measure>
   );
 };
 
