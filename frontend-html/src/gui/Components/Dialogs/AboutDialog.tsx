7feb8de9f5194f9d63e7881b587d9f44778fbf94
Jindrich Susen <jindrich.susen@advantages.cz>
2022-04-14 07:46:11 +0200

    FIX: Last commit link in about window was pointing to the old repository

--- frontend-html/src/gui/Components/Dialogs/AboutDialog.tsx
+++ frontend-html/src/gui/Components/Dialogs/AboutDialog.tsx
@@ -35,7 +35,7 @@ export class AboutDialog extends React.Component<{
           <div>Client version: </div>
           <div className={S.version}>
             <div>{"Commit ID: "} 
-              <a href={"https://github.com/origam/origam-html/commit/"+commitId}>{commitId}</a>
+              <a href={"https://github.com/origam/origam/commit/"+commitId}>{commitId}</a>
               </div>
             <div>Commit Date: {commitDate}</div>
           </div>
