d8529eee2db6fb7accf028f01e4c8e408a290fd0
Jindrich Susen <jindrich.susen@advantages.cz>
2022-04-19 16:37:50 +0200

    Revert "INTERNAL: Added test debug logging"
    This reverts commit 10c227c8fa5bb6d1f04554fb9b8b8a4198ce5e9a.

--- backend/Origam.Server/Session Stores/FormSessionStore.cs
+++ backend/Origam.Server/Session Stores/FormSessionStore.cs
@@ -107,7 +107,6 @@ namespace Origam.Server
 
         private void LoadData()
         {
-            log.Debug("LoadData executed");
             if(dataRequested)
             {
                 LoadDataFxServer();
@@ -167,8 +166,6 @@ namespace Origam.Server
 
         private void SetDelayedLoadingParameter(DataStructureMethod method)
         {
-            log.Debug("Setting DelayedLoadingParameterName to: " + CustomParameterService.GetFirstNonCustomParameter(method) +
-                      ", methodId: " + method.Id);
             // set the parameter for delayed data loading - there should be just 1
             DelayedLoadingParameterName = CustomParameterService.GetFirstNonCustomParameter(method);
         }
@@ -522,7 +519,6 @@ namespace Origam.Server
 
         private DataSet LoadDataPiece(object parentId)
         {
-            log.Debug("DelayedLoadingParameterName " + DelayedLoadingParameterName);
             if (DelayedLoadingParameterName == null)
             {
                 LoadData();
