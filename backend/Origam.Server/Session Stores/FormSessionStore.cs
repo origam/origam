a6ab7fe587443dafeb033b8997175015e726c686
Jindrich Susen <jindrich.susen@advantages.cz>
2022-04-19 16:37:55 +0200

    Revert "INTERNAL: Attempt to solve a testing problem"
    This reverts commit bb1b7000671ac4bed95f86b29ee03e0f78b35ab3.

--- backend/Origam.Server/Session Stores/FormSessionStore.cs
+++ backend/Origam.Server/Session Stores/FormSessionStore.cs
@@ -519,11 +519,6 @@ namespace Origam.Server
 
         private DataSet LoadDataPiece(object parentId)
         {
-            if (DelayedLoadingParameterName == null)
-            {
-                LoadData();
-            }
-
             return core.DataService.LoadData(DataStructureId, _menuItem.MethodId, 
                 _menuItem.DefaultSetId, Guid.Empty, null, 
                 DelayedLoadingParameterName, parentId);
