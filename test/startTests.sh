bcb673b117ac5949ccd2317db7ea5f1d6b89a364
Petr Hrehorovsky <washi@volny.cz>
2022-06-16 17:02:22 +0200

    Updated test script
    - origam-utils test-db arguments were previously refactored

--- test/startTests.sh
+++ test/startTests.sh
@@ -3,7 +3,7 @@ sudo node /root/https-proxy/index.js &
 cd /home/origam/HTML5
 ./startServerTest.sh 
 echo "TEST DB Connection"
-DATAOUT=$(dotnet origam-utils.dll test-db -t 10 -d 5000 -c "select 1")
+DATAOUT=$(dotnet origam-utils.dll test-db -a 10 -d 5000 -c "select 1")
 #echo "Result IS $DATAOUT !!!!!!!!!!!!!!!!!!" ;
 if [[ "$DATAOUT" != True ]]; then
 echo "Database connection failed";
@@ -12,7 +12,7 @@ fi
 export ASPNETCORE_URLS="http://+:8080"
 dotnet Origam.Server.dll &
 echo "TEST DB Connection"
-DATAOUT=$(dotnet origam-utils.dll test-db -t 5 -d 5000 -c "SELECT 1 FROM dbo.\"OrigamModelVersion\" where \"refSchemaExtensionId\"='${OrigamSettings_SchemaExtensionGuid}'")
+DATAOUT=$(dotnet origam-utils.dll test-db -a 5 -d 5000 -c "SELECT 1 FROM dbo.\"OrigamModelVersion\" where \"refSchemaExtensionId\"='${OrigamSettings_SchemaExtensionGuid}'")
 if [[ "$DATAOUT" != True ]]; then
 echo "Database connection failed";
 exit 1;
