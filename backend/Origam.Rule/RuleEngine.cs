79b903cc4c8cd652f6ade3968eefb46080480384
Jindrich Susen <jindrich.susen@advantages.cz>
2022-07-08 15:35:09 +0200

    FIX: AddMinutes function was not available when evaluating XPath expressions

--- backend/Origam.Rule/RuleEngine.cs
+++ backend/Origam.Rule/RuleEngine.cs
@@ -5160,6 +5160,39 @@ namespace Origam.Rule
 		}
 	}
 
+	class AddMinutesFunction : IXsltContextFunction 
+	{
+		private XPathResultType[] _argTypes = null;
+		public RuleEngine Engine = null;
+
+		public AddMinutesFunction(XPathResultType[] argTypes)
+		{
+			_argTypes = argTypes;
+		}
+		public int Minargs    
+		{ 
+			get { return 2; }
+		}
+		public int Maxargs    
+		{ 
+			get { return 2; }
+		}
+		public XPathResultType ReturnType 
+		{ 
+			get { return XPathResultType.String; }
+		}
+		public XPathResultType[] ArgTypes
+		{ 
+			get { return _argTypes; }
+		}
+
+		public object Invoke(XsltContext xsltContext, 
+			object[] args, XPathNavigator docContext) 
+		{
+			return RuleEngine.AddMinutes(XmlTools.XPathArgToString(args[0]), XmlTools.XPathArgToString(args[1]));
+		}
+	}
+
 	class DifferenceInDaysFunction : IXsltContextFunction 
 	{
 		private XPathResultType[] _argTypes = null;
@@ -6035,6 +6068,12 @@ namespace Origam.Rule
 			else if (name == "AddHours")
 			{
 				AddHoursFunction f = new AddHoursFunction(ArgTypes);
+				f.Engine = this.Engine;
+				return f;
+			}
+			else if (name == "AddMinutes")
+			{
+				AddMinutesFunction f = new AddMinutesFunction(ArgTypes);
 				f.Engine = this.Engine;
 				return f;
 			}
