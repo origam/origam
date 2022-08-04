5483e8158dcf34c181b22fdb5459abb52e8c4563
Jindrich Susen <jindrich.susen@advantages.cz>
2022-08-04 15:32:36 +0200

    INTERNAL: XsltFunctionCollection properties made mandatory

--- backend/Origam.Schema.EntityModel/XsltFunctionCollection.cs
+++ backend/Origam.Schema.EntityModel/XsltFunctionCollection.cs
@@ -27,19 +27,23 @@ namespace Origam.Schema.EntityModel
 		{
 		}
 
+		[StringNotEmptyModelElementRule]
 		[Description("C# namespace followed by a \".\" and a class name.")]
 		[XmlAttribute("fullClassName")]
 		public string FullClassName { get; set; }
 		
+		[StringNotEmptyModelElementRule]
 		[Description("Assembly name (without extension) where the class is defined.")]
 		[XmlAttribute("assemblyName")]
 		public string AssemblyName { get; set; }
 		
+		[StringNotEmptyModelElementRule]
 		[Description("Xslt functions found in the provided class will be defined in this xslt namespace.")]
 		[XmlAttribute("xslNameSpaceUri")]
 		public string XslNameSpaceUri { get; set; }	
 		
-		[Description("Xslt namespace prefix in xpath. Prefix in Xslt transformations will be declares in the Xslt templates.")]
+		[StringNotEmptyModelElementRule]
+		[Description("Xslt namespace prefix in xpath. Prefix in Xslt transformations will be declared in the Xslt templates.")]
 		[XmlAttribute("xslNameSpacePrefix")]
 		public string XslNameSpacePrefix { get; set; }
 	}
