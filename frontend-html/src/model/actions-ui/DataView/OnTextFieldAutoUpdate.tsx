39dc92353b44ab3ad1929fa43130737f9606c6ac
JindrichSusen <33699252+JindrichSusen@users.noreply.github.com>
2022-08-26 14:57:52 +0200

    INTERNAL: Some UpdateObject requests were executed outside of UpdateRequestAggregator (#850)

--- frontend-html/src/model/actions-ui/DataView/OnTextFieldAutoUpdate.tsx
+++ frontend-html/src/model/actions-ui/DataView/OnTextFieldAutoUpdate.tsx
@@ -20,35 +20,27 @@ along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
 import { IProperty } from "../../entities/types/IProperty";
 import { getDataView } from "../../selectors/DataView/getDataView";
 import { getSessionId } from "../../selectors/getSessionId";
-import { getApi } from "../../selectors/getApi";
-import { runInFlowWithHandler } from "../../../utils/runInFlowWithHandler";
+import { getFormScreenLifecycle } from "model/selectors/FormScreen/getFormScreenLifecycle";
 
-export function onTextFieldAutoUpdate(property: IProperty, value: string) {
-
-  const dataView = getDataView(property);
-  if (!dataView.selectedRowId) {
-    return;
-  }
-  const sessionId = getSessionId(dataView);
-  let api = getApi(dataView);
-
-  const valueObj = {} as any;
-  valueObj[property.id] = value;
-
-  const updateData = [
-    {
-      RowId: dataView.selectedRowId,
-      Values: valueObj,
+export async function onTextFieldAutoUpdate(property: IProperty, value: string) {
+    const dataView = getDataView(property);
+    if (!dataView.selectedRowId) {
+        return;
     }
-  ];
-  runInFlowWithHandler({
-    ctx: dataView,
-    action: async () => {
-      await api.updateObject({
+    const sessionId = getSessionId(dataView);
+    const valueObj = {} as any;
+    valueObj[property.id] = value;
+
+    const updateData = [
+        {
+            RowId: dataView.selectedRowId,
+            Values: valueObj,
+        }
+    ];
+    const formScreenLifecycle = getFormScreenLifecycle(property);
+    await formScreenLifecycle.updateRequestAggregator.enqueue({
         SessionFormIdentifier: sessionId,
         Entity: dataView.entity,
         UpdateData: updateData,
-      });
-    }
-  });
+    });
 }
