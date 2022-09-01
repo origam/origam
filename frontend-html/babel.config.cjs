8b3fa38997098a08e3b87d391861dc7f35434738
Jindrich Susen <jindrich.susen@advantages.cz>
2022-09-01 13:19:59 +0200

    INTERNAL: Unit tests fixed

--- frontend-html/babel.config.cjs
+++ frontend-html/babel.config.cjs
@@ -11,4 +11,8 @@ module.exports = {
     "@babel/preset-react",
     "@babel/preset-typescript",
   ],
+  "plugins": [
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    "@babel/plugin-proposal-class-properties"
+  ],
 };
