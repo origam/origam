d48b7ae3cdc779588981de2423d6cdcf2c0a512f
Jindrich Susen <jindrich.susen@advantages.cz>
2022-09-28 12:49:43 +0200

    INTERNAL: Error message improved

--- test/tests_e2e/testTools.js
+++ test/tests_e2e/testTools.js
@@ -74,7 +74,7 @@ async function waitForRowCountData(page, dataViewId, expectedRowCount) {
     }
     await sleep(50);
   }
-  throw new Error(`Row count did not change before timeout, expectedRowCount: ${expectedRowCount}, countData.rowCount ${countData.rowCount}`);
+  throw new Error(`Row count did not change before timeout, expectedRowCount: ${expectedRowCount},  countData.rowCount: ${countData.rowCount}`);
 }
 
 async function getTableData(page, dataViewId){
