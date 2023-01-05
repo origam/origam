c48f4092ff7bb14630a1031f5bf63b8c8cedc6f5
Jindrich Susen <jindrich.susen@advantages.cz>
2023-01-05 10:50:56 +0100

    INTERNAL: Added tests checking that user is prompted with a dialog when changing active row in a dirty grid

--- test/tests_e2e/modelIds.js
+++ test/tests_e2e/modelIds.js
@@ -5,13 +5,13 @@ const masterDetailMenuItemId = "menu_12bef472-a744-4f5a-98f8-17c163137e9f";
 const masterDetailLazyLoadedMenuItemId = "menu_d893b185-c46b-4f73-8599-6d630c4ce1ba";
 const allDataTypesMenuId = "menu_423a08e5-b1cf-4341-a342-d9b57667d1b9";
 const allDataTypesLazyMenuItemsId = "menu_30322a63-f242-45d5-a3ff-adaa3e4cb28a";
-const allDataTypesLazyDatViewId = "dataView_e67865b0-ce91-413c-bed7-1da486399633";
+const allDataTypesLazyDataViewId = "dataView_e67865b0-ce91-413c-bed7-1da486399633";
 const masterDataViewId = "dataView_775fa5ea-fa75-40a7-8c39-7828f7cdf508";
 const detailDataViewId = "dataView_b11ffa85-7507-475c-af50-ef08fd56072c";
 const detailEditorId = "editor_89be97a4-86e8-4036-b57a-36155e3f2322";
 const detailTabHandelId = "tabHandle_823ea459-bca5-476f-ab6f-9cb07769923e";
 
-module.exports = {widgetsMenuItemId, sectionsMenuItemId, masterDetailMenuItemId: masterDetailMenuItemId,
+module.exports = {widgetsMenuItemId, sectionsMenuItemId, masterDetailMenuItemId,
   allDataTypesMenuId, allDataTypesLazyMenuItemsId, topMenuHeader, masterDataViewId,
-  detailDataViewId, detailEditorId, detailTabHandelId, masterDetailLazyLoadedMenuItemId: masterDetailLazyLoadedMenuItemId,
-  allDataTypesLazyDatViewId};
+  detailDataViewId, detailEditorId, detailTabHandelId, masterDetailLazyLoadedMenuItemId,
+  allDataTypesLazyDataViewId};
