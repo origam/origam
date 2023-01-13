d789255fbb1b6ac41418c9936b1db5c2d7c7a3b0
Jindrich Susen <jindrich.susen@advantages.cz>
2023-01-13 16:01:43 +0100

    NEW: Trace can be switched on/off using new ModelService methods

--- backend/Origam.Workflow/Service Agents/ModelServiceAgent.cs
+++ backend/Origam.Workflow/Service Agents/ModelServiceAgent.cs
@@ -27,6 +27,7 @@ using Origam.Schema;
 using Origam.Services;
 using Origam.Workbench.Services;
 using System.Data;
+using Origam.Schema.WorkflowModel;
 using Origam.Service.Core;
 
 namespace Origam.Workflow
@@ -40,6 +41,12 @@ namespace Origam.Workflow
         const string GENERATED_PACKAGE_NAME = "_generated";
         public ModelServiceAgent()
         {
+            persistenceService = ServiceManager
+                .Services
+                .GetService<IPersistenceService>();
+            tracingService = ServiceManager
+                .Services
+                .GetService<TracingService>();
         }
 
         #region Private Methods
@@ -96,6 +103,9 @@ namespace Origam.Workflow
 
         #region IServiceAgent Members
         private object _result;
+        private readonly TracingService tracingService;
+        private IPersistenceService persistenceService;
+
         public override object Result
         {
             get
@@ -123,6 +133,31 @@ namespace Origam.Workflow
                     _result = ElementList(
                         Parameters.Get<Guid>("ParentId"),
                         Parameters.Get<string>("ItemType"));
+                    break;  
+                
+                case "SetTrace":
+                    tracingService.Enabled = Parameters.Get<bool>("Enabled");
+                    _result = tracingService.Enabled;
+                    break;  
+                
+                case "GetTrace":
+                    _result = tracingService.Enabled;
+                    break;             
+                
+                case "TraceObject":
+                    Guid objectId = Parameters.Get<Guid>("ObjectId");
+                    var step = persistenceService.SchemaProvider
+                        .RetrieveInstance<IWorkflowStep>(
+                            objectId, 
+                            useCache: true);
+                    string traceType = Parameters.Get<string>("TraceType");
+                    if (!Enum.TryParse(traceType, out Trace trace))
+                    {
+                        throw new ArgumentException($"\"{traceType}\" cannot be parsed to {nameof(Trace)}");
+                    }
+                    step.TraceLevel = trace;
+                    persistenceService.SchemaProvider.Persist(step);
+                    _result = step.TraceLevel;
                     break;
 
                 default:
