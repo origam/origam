709c007cb971d009d1f4d57679846e9a4f26df01
Jiri Prajz <jiri.prajz@advantages.cz>
2023-01-30 07:14:08 +0100

    use the UserTools.IsInitialSetupNeeded method directly

--- backend/Origam.Server/Configuration/IdentityGuiConfig.cs
+++ backend/Origam.Server/Configuration/IdentityGuiConfig.cs
@@ -21,13 +21,10 @@ along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
 
 #endregion
 
-using Origam.Server.Authorization;
-
 namespace Origam.Server.Configuration
 {
     public class IdentityGuiConfig
     {
         public bool AllowPasswordReset { get; set; }
-        public bool IsInitialSetupNeeded => UserTools.IsInitialSetupNeeded();
     }
 }
