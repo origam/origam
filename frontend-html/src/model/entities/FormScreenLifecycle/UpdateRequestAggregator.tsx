01118b391d3091b562c4b80bc5036a1c22052ef3
JindrichSusen <33699252+JindrichSusen@users.noreply.github.com>
2023-02-03 17:42:21 +0100

    Polyfills (#1333)
    * INTERNAL: Added polyfills that vite 2.x.x used to handle but 3.x.x does not anymore
    
    * INTERNAL: Added predictableActionArguments parameter to suppress a warning
    
    * INTERNAL: yarn.lock regenerated might fix some security issues
    
    * INTERNAL: Fixed build issue with the Buffer module
    
    * INTERNAL: yarn.lock updated

--- frontend-html/src/model/entities/FormScreenLifecycle/UpdateRequestAggregator.tsx
+++ frontend-html/src/model/entities/FormScreenLifecycle/UpdateRequestAggregator.tsx
@@ -61,6 +61,7 @@ export class UpdateRequestAggregator {
   interpreter = interpret(
     createMachine(
       {
+        predictableActionArguments: true,
         id: "updateObject",
         initial: "IDLE",
         states: {
