cf012cec925fc7011011f0b3c1ba5afe5fe3b814
Jindrich Susen <jindrich.susen@advantages.cz>
2023-02-27 16:42:25 +0100

    INTERNAL: Dataset updated

---
+++ backend/Origam.Workflow/WorkQueue/WorkQueueData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Origam.DA;
+
+namespace Origam.Workflow.WorkQueue
+{
+
+
+    public partial class WorkQueueData
+    {
+        partial class WorkQueueEntryRow
+        {
+            public WorkQueueEntryRow Clone()
+            {
+                DataSet oneRowDataSet = DatasetTools.CloneDataSet(Table.DataSet);
+                DatasetTools.GetDataSlice(oneRowDataSet, new List<DataRow> { this });
+                DataRow oneRow = oneRowDataSet.Tables[0].Rows[0];
+                return (WorkQueueEntryRow)oneRow;
+            }
+        }
+    }
+}
