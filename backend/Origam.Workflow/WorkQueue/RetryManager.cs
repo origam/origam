7d2e44cc82dedf333f7b4ac59882fae8066cfc01
Jindrich Susen <jindrich.susen@advantages.cz>
2023-03-13 09:15:33 +0100

    INTERNAL: License added

--- backend/Origam.Workflow/WorkQueue/RetryManager.cs
+++ backend/Origam.Workflow/WorkQueue/RetryManager.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿#region license
+/*
+Copyright 2005 - 2023 Advantage Solutions, s. r. o.
+
+This file is part of ORIGAM (http://www.origam.org).
+
+ORIGAM is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ORIGAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using System;
 using System.Data;
 
 namespace Origam.Workflow.WorkQueue;
@@ -93,19 +114,7 @@ public class RetryManager
 
         return (DateTime)queueEntryRow["NextAttemptTime"] <= DateTime.Now;
     }
-
-    // private (Guid, int, int, int?, int?) GetRetryData(
-    //     WorkQueueData.WorkQueueRow queue)
-    // {
-    //     return (
-    //         retryType: (Guid)queue["refWorkQueueRetryTypeId"],
-    //         maxRetries: (int)queue["MaxRetries"],
-    //         retryIntervalSeconds: (int)queue["RetryIntervalSeconds"],
-    //         (int)queue["MaxRetryIntervalSeconds"],
-    //         (int)queue["MinRetryIntervalSeconds"]
-    //     );
-    // }
-
+    
     private int GetAttemptCount(DataRow queueEntryRow)
     {
         return queueEntryRow["AttemptCount"] == DBNull.Value
