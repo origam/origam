4a0eb19d895063165ec792472bc92c659b8533f2
Jindrich Susen <jindrich.susen@advantages.cz>
2023-05-10 14:43:09 +0200

    NEW: Workflow stack trace added to workflow exceptions

---
+++ backend/Origam.Workflow/WorkFlowStackTrace.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace Origam.Workflow;
+
+public class WorkFlowStackTrace
+{
+    private readonly StringBuilder messages = new("Workflow stack trace\n");
+    
+    public void RecordStepStart(string workflowName, string stepName)
+    {
+        messages.Append($"\tStep: '{workflowName}/{stepName}'\n");
+    }
+
+    public override string ToString()
+    {
+        return messages.ToString();
+    }
+}
