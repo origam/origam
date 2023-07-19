08112d19e89f64514643e251109ec0bf309a9a67
Jindrich Susen <jindrich.susen@advantages.cz>
2023-07-19 12:40:22 +0200

    forcedLastRowId check was moved
    because it caused a test to fail

--- frontend-html/src/model/entities/ListRowContainer.ts
+++ frontend-html/src/model/entities/ListRowContainer.ts
@@ -125,10 +125,7 @@ export class ListRowContainer implements IRowsContainer {
         yield*self.preloadLookups();
         let rows = self.allRows;
         if (self.filterConfiguration.filteringFunction()) {
-          rows = rows.filter((row) =>
-            self.filterConfiguration.filteringFunction()(row) ||
-            self.forcedLastRowId && self.forcedLastRowId == self.rowIdGetter(row)
-          );
+          rows = rows.filter((row) => self.filterConfiguration.filteringFunction()(row, self.forcedLastRowId));
         }
         if (self.orderingConfiguration.orderings.length !== 0) {
           rows = rows.sort((row1: any[], row2: any[]) => self.internalRowOrderingFunc(row1, row2));
