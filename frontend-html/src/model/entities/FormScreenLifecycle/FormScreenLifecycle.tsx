21fc0fbd77c0e5d098daea4e6d7d41df3f273327
Jindrich Susen <jindrich.susen@advantages.cz>
2023-08-01 10:13:39 +0200

    Method simplified

--- frontend-html/src/model/entities/FormScreenLifecycle/FormScreenLifecycle.tsx
+++ frontend-html/src/model/entities/FormScreenLifecycle/FormScreenLifecycle.tsx
@@ -786,7 +786,7 @@ export class FormScreenLifecycle02 implements IFormScreenLifecycle02 {
       Entity: dataView.entity,
       UpdateData: updateData,
     });
-    if(updateObjectResult == null){
+    if(updateObjectResult === null){
       return false;
     }
     dataView.formFocusManager.stopAutoFocus();
@@ -819,7 +819,7 @@ export class FormScreenLifecycle02 implements IFormScreenLifecycle02 {
           },
         ],
       });
-      if(updateObjectResult == null){
+      if(updateObjectResult === null){
         return;
       }
       yield*processCRUDResult(dataView, updateObjectResult, false, dataView);
