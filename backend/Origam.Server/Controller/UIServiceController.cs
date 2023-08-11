992e14fc2eb49ef08a67ecf2086c3bd67c1e93ea
Jindrich Susen <jindrich.susen@advantages.cz>
2023-08-11 09:34:01 +0200

    RowStatesDebouncingDelayMilliSeconds added to appsettings.json

--- backend/Origam.Server/Controller/UIServiceController.cs
+++ backend/Origam.Server/Controller/UIServiceController.cs
@@ -1217,6 +1217,7 @@ namespace Origam.Server.Controller
                 : chatConfig.ChatRefreshInterval;
             result.CustomAssetsRoute = customAssetsConfig.RouteToCustomAssetsFolder;
             result.ShowToolTipsForMemoFieldsOnly = htmlClientConfig.ShowToolTipsForMemoFieldsOnly;
+            result.RowStatesDebouncingDelayMilliSeconds = htmlClientConfig.RowStatesDebouncingDelayMilliSeconds;
             result.FilteringConfig = clientFilteringConfig;
         }
     }
