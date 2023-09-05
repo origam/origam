f58c2239d8315e8744e7c0308fe962bfc47aba17
Petr Hrehorovsky <washi@volny.cz>
2023-09-05 17:25:50 +0200

    Code formatting.

--- backend/Origam.Server/Common/ChangeInfo.cs
+++ backend/Origam.Server/Common/ChangeInfo.cs
@@ -19,27 +19,6 @@ along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
 */
 #endregion
 
-#region license
-/*
-Copyright 2005 - 2021 Advantage Solutions, s. r. o.
-
-This file is part of ORIGAM.
-
-ORIGAM is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-ORIGAM is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with ORIGAM.  If not, see<http://www.gnu.org/licenses/>.
-*/
-#endregion
-
 using System;
 using Origam.Rule;
 
@@ -164,9 +143,18 @@ namespace Origam.Server
 
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
             return Equals((ChangeInfo)obj);
         }
 
