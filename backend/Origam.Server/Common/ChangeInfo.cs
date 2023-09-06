a103fe38c249c58fcc86ee220a893745380dfdc8
JindrichSusen <33699252+JindrichSusen@users.noreply.github.com>
2023-09-06 12:52:50 +0200

    DEV: UpdateObject duplicate changes (#1861)
    * UpdateObject returned duplicate entries
    * Equality overriden to avoid getting duplicates from UpdateObject
    * Code formatting.
    
    ---------
    
    Co-authored-by: Petr Hrehorovsky <washi@volny.cz>

--- backend/Origam.Server/Common/ChangeInfo.cs
+++ backend/Origam.Server/Common/ChangeInfo.cs
@@ -19,27 +19,7 @@ along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
 */
 #endregion
 
-#region license
-/*
-Copyright 2005 - 2021 Advantage Solutions, s. r. o.
-
-This file is part of ORIGAM.
-
-ORIGAM is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-ORIGAM is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with ORIGAM.  If not, see<http://www.gnu.org/licenses/>.
-*/
-#endregion
-
+using System;
 using Origam.Rule;
 
 namespace Origam.Server
@@ -154,6 +134,33 @@ namespace Origam.Server
             ChangeInfo ci = new ChangeInfo();
             ci.Operation = Operation.DeleteAllData;
             return ci;
+        }
+
+        protected bool Equals(ChangeInfo other)
+        {
+            return entity == other.entity && operation == other.operation && Equals(objectId, other.objectId);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            return Equals((ChangeInfo)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(entity, (int)operation, objectId);
         }
     }
 }
