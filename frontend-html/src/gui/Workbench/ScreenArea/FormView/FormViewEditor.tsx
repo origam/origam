0cabceb68aa3b64d8cab21bd693b3ed615444c1f
JindrichSusen <33699252+JindrichSusen@users.noreply.github.com>
2023-10-03 11:10:08 +0200

    FIX: Ctrl + S shortcut was not working when an editor was active (#1972)
    * Redundant field removed
    * Ctrl + S shortcut was not working when an editor was active
    * User was not asked what to do with unsaved data after pressing refresh when focus was still in the edited field editor when pressing the refresh button. And it was the only change in the current row.
    * User was not asked what to do with unsaved data after Ctrl + R

--- frontend-html/src/gui/Workbench/ScreenArea/FormView/FormViewEditor.tsx
+++ frontend-html/src/gui/Workbench/ScreenArea/FormView/FormViewEditor.tsx
@@ -42,14 +42,16 @@ import { DomEvent } from "leaflet";
 import { onDropdownEditorClick } from "model/actions/DropdownEditor/onDropdownEditorClick";
 import { shadeHexColor } from "utils/colorUtils";
 import { getIsFormScreenDirty } from "model/selectors/FormScreen/getisFormScreenDirty";
-import { runInFlowWithHandler } from "utils/runInFlowWithHandler";
+import { runGeneratorInFlowWithHandler, runInFlowWithHandler } from "utils/runInFlowWithHandler";
 import ColorEditor from "gui/Components/ScreenElements/Editors/ColorEditor";
 import { CellAlignment } from "gui/Components/ScreenElements/Table/TableRendering/cells/cellAlignment";
 import { flashColor2htmlColor, htmlColor2FlashColor } from "@origam/utils";
 import {
   isAddRecordShortcut,
   isDeleteRecordShortcut,
-  isDuplicateRecordShortcut, isFilterRecordShortcut,
+  isDuplicateRecordShortcut, 
+  isFilterRecordShortcut,
+  isRefreshShortcut,
   isSaveShortcut
 } from "utils/keyShortcuts";
 import { onCreateRowClick } from "model/actions-ui/DataView/onCreateRowClick";
@@ -58,6 +60,7 @@ import { flushCurrentRowData } from "model/actions/DataView/TableView/flushCurre
 import { onDeleteRowClick } from "model/actions-ui/DataView/onDeleteRowClick";
 import { onCopyRowClick } from "model/actions-ui/DataView/onCopyRowClick";
 import { onFilterButtonClick } from "model/actions-ui/DataView/onFilterButtonClick";
+import { getFormScreenLifecycle } from "model/selectors/FormScreen/getFormScreenLifecycle";
 
 
 @inject(({property, formPanelView}) => {
@@ -346,6 +349,28 @@ export class FormViewEditor extends React.Component<{
           }
           if (isAddRecordShortcut(event)) {
             await onCreateRowClick(dataView)(event);
+            return;
+          }
+          if (isSaveShortcut(event)){
+            await runGeneratorInFlowWithHandler({
+              ctx: dataView,
+              generator: function*() {
+                yield*flushCurrentRowData(dataView)();
+                const formScreenLifecycle = getFormScreenLifecycle(dataView);
+                yield*formScreenLifecycle.onSaveSession();
+              }()
+            });
+            return;
+          }
+          if (isRefreshShortcut(event)){
+            await runGeneratorInFlowWithHandler({
+              ctx: dataView,
+              generator: function*() {
+                yield*flushCurrentRowData(dataView)();
+                const formScreenLifecycle = getFormScreenLifecycle(dataView);
+                yield*formScreenLifecycle.onRequestScreenReload();
+              }()
+            });
             return;
           }
           if (isAddRecordShortcut(event)) {
