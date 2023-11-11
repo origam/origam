57fc01523c49da50af6abc73ef2315e96611328d
Jindrich Susen <jindrich.susen@advantages.cz>
2023-11-11 18:29:40 +0100

    Message improved

--- frontend-html/src/modules/Editors/DropdownEditor/Cells/TextCellDriver.tsx
+++ frontend-html/src/modules/Editors/DropdownEditor/Cells/TextCellDriver.tsx
@@ -42,7 +42,7 @@ export class TextCellDriver implements IBodyCellDriver {
   render(rowIndex: number) {
     const rowId = this.dataTable.getRowIdentifierByIndex(rowIndex);
     if(rowId === null || rowId === ""){
-      console.error("There are rows with missing identifier in the drop down. This causes confusing coloring in the drop down. Fix your model!")
+      console.error("There are rows with missing identifier in the dropdown. This causes confusing row coloring. Fix your model!")
     }
     return (
       <div
