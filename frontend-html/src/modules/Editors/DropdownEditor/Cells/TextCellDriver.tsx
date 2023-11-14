5ebb0c7b7d5247b08fc7de9db736d0bf7e703811
JindrichSusen <33699252+JindrichSusen@users.noreply.github.com>
2023-11-14 09:41:00 +0100

    FEATURE: Console error is displayed if there are rows with missing identifier in dropdown (#2186)
    Added error to console when missing rowId in dropdown cause multiple rows to be highlighted

--- frontend-html/src/modules/Editors/DropdownEditor/Cells/TextCellDriver.tsx
+++ frontend-html/src/modules/Editors/DropdownEditor/Cells/TextCellDriver.tsx
@@ -41,6 +41,9 @@ export class TextCellDriver implements IBodyCellDriver {
 
   render(rowIndex: number) {
     const rowId = this.dataTable.getRowIdentifierByIndex(rowIndex);
+    if(rowId === null || rowId === ""){
+      console.error("There are rows with missing identifier in the dropdown. This causes confusing row coloring. Fix your model!")
+    }
     return (
       <div
         className={bodyCellClass(
