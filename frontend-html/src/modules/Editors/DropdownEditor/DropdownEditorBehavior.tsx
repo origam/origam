e7c52b4262f425963102e65f3569a844c05c3337
jsusen <jindrich.susen@advantages.cz>
2023-11-17 15:43:06 +0100

    Making sure the GetLookupList promise will not be canceled
    in case the setup().dropdownType is LazilyLoadedGrid

--- frontend-html/src/modules/Editors/DropdownEditor/DropdownEditorBehavior.tsx
+++ frontend-html/src/modules/Editors/DropdownEditor/DropdownEditorBehavior.tsx
@@ -289,6 +289,7 @@ export class DropdownEditorBehavior implements IDropdownEditorBehavior{
       return;
     }
     setTimeout(async () => {
+      this.handleInputChangeDeb.cancel();
       if (!this.runningPromise) {
         this.ensureRequestRunning();
       }
