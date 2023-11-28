cd4f0b89b44f33ebe274ddf8590efef0e276f4ef
Jindrich Susen <jindrich.susen@advantages.cz>
2023-11-28 15:22:39 +0100

    Tests updated

--- test/tests_e2e/09_workflow.test.js
+++ test/tests_e2e/09_workflow.test.js
@@ -1,30 +1,48 @@
 ﻿const { sleep, openMenuItem, login, afterEachTest,
   beforeEachTest, catchRequests, waitForRowCount
 } = require('./testTools');
-const {widgetsMenuItemId, topMenuHeader, allDataTypesMenuId
+const {widgetsMenuItemId, topMenuHeader, allDataTypesMenuId, sectionsMenuItemId, masterDetailMenuItemId,
+  workflowTestItemId
 } = require("./modelIds");
-const { restoreWidgetSectionTestMaster, clearScreenConfiguration} = require("./dbTools");
+const { restoreWidgetSectionTestMaster, clearScreenConfiguration, restoreAllDataTypesTable} = require("./dbTools");
 const {installMouseHelper} = require("./instalMouseHelper_");
 
 let browser;
 let page;
 
+// beforeAll(async() => {
+//   await restoreWidgetSectionTestMaster();
+//   await clearScreenConfiguration();
+//   [browser, page] = await beforeEachTest(); // all tests run in the same browser instance unlike the others. That is why the beforeEachTest is called here.
+//   await login(page);
+//   // await openMenuItem(
+//   //   page,
+//   //   [
+//   //     topMenuHeader,
+//   //     widgetsMenuItemId,
+//   //     allDataTypesMenuId
+//   //   ]);
+// });
+//
+// afterAll(async() => {
+//   await sleep(1000); // let the last workflow screen close
+//   await afterEachTest(browser);
+//   browser = undefined;
+// });
+
+
+
 beforeAll(async() => {
-  await restoreWidgetSectionTestMaster();
+  await restoreAllDataTypesTable();
   await clearScreenConfiguration();
-  [browser, page] = await beforeEachTest(); // all tests run in the same browser instance unlike the others. That is why the beforeEachTest is called here.
-  await login(page);
-  await openMenuItem(
-    page,
-    [
-      topMenuHeader,
-      widgetsMenuItemId,
-      allDataTypesMenuId
-    ]);
 });
 
-afterAll(async() => {
-  await sleep(1000); // let the last workflow screen close
+beforeEach(async () => {
+  [browser, page] = await beforeEachTest();
+  await login(page);
+});
+
+afterEach(async () => {
   await afterEachTest(browser);
   browser = undefined;
 });
@@ -88,105 +106,212 @@ async function waitForWorkflowMessage(messageBegging) {
 }
 
 const dataViewId = "dataView_e67865b0-ce91-413c-bed7-1da486399633";
+const firstStepFailItemId ="menu_1119a229-eafa-4afa-8986-39487eb78cca"
+const secondStepFailItemId ="menu_3e5a6435-b4cb-4338-ad16-04ad24d1402c"
+const uiThirdStepFail ="menu_6d3f26b6-1dbc-44f3-851b-5a119f22b925"
+const callServiceWorkflowSub1StepItemId ="menu_e70afa1f-c511-4e5c-bcdf-a8041717d2c3"
+const callServiceWorkflowSub2StepItemId ="menu_d948a606-9ff4-46eb-b259-7fcb420eb59c"
+const callServiceWorkflowSubUiStepItemId ="menu_88fed471-a2ed-4fd0-9ae7-d57ffbbe5f91"
+const callSubWorkflow1StepItemId ="menu_44e1b7cc-0e83-4379-8227-c78982eaf71d"
+const callSubWorkflow2StepItemId ="menu_f907f6c6-13f3-4c58-913c-a0e8dfc52f0a"
+const callSubWorkflowUiStepItemId ="menu_f066f5e3-320a-42b4-b2ec-39f8670921e9"
+const uiWFCallServiceStep1ItemId ="menu_3c857341-72f3-4835-83ea-43b320579bec"
+const uiWFCallServiceStep2ItemId ="menu_486315ce-e6d0-4a92-b6bd-451d3eff5fa7"
+const uiWFServiceSub1stepItemId ="menu_f68209ca-75c4-499d-859a-1ff8c3ac22cf"
+const uiWFServiceSub2stepItemId ="menu_931a64cd-b3d2-412b-a888-be4203c62292"
+const uiWFCallServiceSubUiStepItemId ="menu_e01a1eff-43e0-4dee-97db-d616e6926a10"
+const uiWFServiceSubUiItemId ="menu_22ca65ba-a587-4cfc-a1cc-aeda9d3e588e"
 
 describe("Html client", () => {
   it("Should run 'First step fail' workflow", async () => {
-    await clickElement("Run Test Workflow")
-    await clickElement("First step fail")
-    await waitForErrorWindowWithMessageAndClose(
-      "Server error occurred. Please check server log for more details:\n" +
-      "Merge context 'AllDataTypes', Step 'Basic WF_1stepfail/0100_StepFail' failed.");
+    await openMenuItem(
+      page,
+      [
+        topMenuHeader,
+        widgetsMenuItemId,
+        workflowTestItemId,
+        firstStepFailItemId
+      ]);
+    await waitForWorkflowMessage("Merge context 'AllDataTypes', Step 'Basic WF_1stepfail/0100_StepFail' failed.");
+    await clickElement("Close","button");
   });
   it("Should run 'Second step fail' workflow", async () => {
-    await clickElement("Run Test Workflow")
-    await clickElement("Second step fail")
-    await waitForErrorWindowWithMessageAndClose("Fail Step");
+    await openMenuItem(
+      page,
+      [
+        topMenuHeader,
+        widgetsMenuItemId,
+        workflowTestItemId,
+        secondStepFailItemId
+      ]);
+    await waitForWorkflowMessage("Fail Step");
+    await clickElement("Close","button");
   });
   it("Should run 'UI third step fail' workflow", async () => {
-    await clickElement("Run Test Workflow");
-    await clickElement("UI third step fail");
+    await openMenuItem(
+      page,
+      [
+        topMenuHeader,
+        widgetsMenuItemId,
+        workflowTestItemId,
+        uiThirdStepFail
+      ]);
     await clickElement("Next");
     await waitForWorkflowMessage('Merge context \'AllDataTypes\', Step \'Basic WF_UI_NextStepFail/0300_StepFail\' failed.');
     await clickElement("Close","button");
   });
-  it("Should run 'UI WFCall step 1' workflow", async () => {
-    await clickElement("Run Test Workflow");
-    await clickElement("UI WFCall step 1");
+  it("Should run 'CallServiceWorkflow Sub 1 step' workflow", async () => {
+    await openMenuItem(
+      page,
+      [
+        topMenuHeader,
+        widgetsMenuItemId,
+        workflowTestItemId,
+        callServiceWorkflowSub1StepItemId
+      ]);
+    await waitForWorkflowMessage('Merge context \'AllDataTypes\', Step \'Basic WF_1stepfail/0100_StepFail\' failed.');
+    await clickElement("Close","button");
+  });
+  it("Should run 'CallServiceWorkflow Sub 2 step' workflow", async () => {
+    await openMenuItem(
+      page,
+      [
+        topMenuHeader,
+        widgetsMenuItemId,
+        workflowTestItemId,
+        callServiceWorkflowSub2StepItemId
+      ]);
+    await waitForWorkflowMessage('Fail Step');
+    await clickElement("Close","button");
+  });
+  it("Should run 'CallServiceWorkflow Sub UI step' workflow", async () => {
+    await openMenuItem(
+      page,
+      [
+        topMenuHeader,
+        widgetsMenuItemId,
+        workflowTestItemId,
+        callServiceWorkflowSubUiStepItemId
+      ]);
+    await clickElement("Next");
+    await waitForWorkflowMessage('Merge context \'AllDataTypes\', Step \'Basic WF_UI_NextStepFail/0300_StepFail\' failed.');
+    await clickElement("Close","button");
+  });
+  it("Should run 'CallSubWorkflow 1 step' workflow", async () => {
+    await openMenuItem(
+      page,
+      [
+        topMenuHeader,
+        widgetsMenuItemId,
+        workflowTestItemId,
+        callSubWorkflow1StepItemId
+      ]);
+    await waitForWorkflowMessage('Merge context \'AllDataTypes\', Step \'Basic WF_1stepfail/0100_StepFail\' failed.');
+    await clickElement("Close","button");
+  });
+  it("Should run 'CallSubWorkflow 2 step' workflow", async () => {
+    await openMenuItem(
+      page,
+      [
+        topMenuHeader,
+        widgetsMenuItemId,
+        workflowTestItemId,
+        callSubWorkflow2StepItemId
+      ]);
+    await waitForWorkflowMessage('Fail Step');
+    await clickElement("Close","button");
+  });
+  it("Should run 'CallSubWorkflow UI step' workflow", async () => {
+    await openMenuItem(
+      page,
+      [
+        topMenuHeader,
+        widgetsMenuItemId,
+        workflowTestItemId,
+        callSubWorkflowUiStepItemId
+      ]);
     await clickElement("Next");
     await waitForWorkflowMessage('Merge context \'AllDataTypes\', Step \'Basic WF_1stepfail/0100_StepFail\' failed.');
     await clickElement("Close","button");
   });
-  it("Should run 'UI WFCall step 2' workflow", async () => {
-    await clickWorkflowActionButton("UI WFCall step 2");
-    await clickElement("Next");
-    await waitForWorkflowMessage('Fail Step');
-    await clickElement("Close","button");
-  });
-  it("Should run 'UI WFCall step UI fail' workflow", async () => {
-    await clickElement("Run Test Workflow");
-    await clickElement("UI WFCall step UI fail");
-    await clickElement("Next");
-    await waitForWorkflowMessage('Item has already been added. Key in dictionary:');
-    await clickElement("Close","button");
-  });
-  it("Should run 'UI WFService Sub 1 step' workflow", async () => {
-    await clickElement("Run Test Workflow");
-    await clickElement("UI WFService Sub 1 step");
+  it("Should run 'UI WFCallService step 1' workflow", async () => {
+    await openMenuItem(
+      page,
+      [
+        topMenuHeader,
+        widgetsMenuItemId,
+        workflowTestItemId,
+        uiWFCallServiceStep1ItemId
+      ]);
     await clickElement("Next");
     await waitForWorkflowMessage('Merge context \'AllDataTypes\', Step \'Basic WF_1stepfail/0100_StepFail\' failed.');
     await clickElement("Close","button");
   });
-  it("Should run 'UI WFService Sub 2 steps' workflow", async () => {
-    await clickElement("Run Test Workflow");
-    await clickElement("UI WFService Sub 2 steps");
+  it("Should run 'UI WFCallService step 2' workflow", async () => {
+    await openMenuItem(
+      page,
+      [
+        topMenuHeader,
+        widgetsMenuItemId,
+        workflowTestItemId,
+        uiWFCallServiceStep2ItemId
+      ]);
     await clickElement("Next");
     await waitForWorkflowMessage('Fail Step');
     await clickElement("Close","button");
   });
-  it("Should run 'UI WFService Sub UI' workflow", async () => {
-    await clickElement("Run Test Workflow");
-    await clickElement("UI WFService Sub UI");
+  it("Should run 'UI WFCallService Sub UI step' workflow", async () => {
+    await openMenuItem(
+      page,
+      [
+        topMenuHeader,
+        widgetsMenuItemId,
+        workflowTestItemId,
+        uiWFCallServiceSubUiStepItemId
+      ]);
     await clickElement("Next");
     await waitForRowCount(page, dataViewId, 2);
     await clickElement("Next");
     await waitForWorkflowMessage('Merge context \'AllDataTypes\', Step \'Basic WF_UI_NextStepFail/0300_StepFail\' failed. ');
     await clickElement("Close","button");
   });
-  it("Should run 'CallServiceWorkflow Sub 1 step' workflow", async () => {
-    await clickElement("Run Test Workflow")
-    await clickElement("CallServiceWorkflow Sub 1 step")
-    await waitForErrorWindowWithMessageAndClose(
-      "Server error occurred. Please check server log for more details:\n" +
-      "Merge context 'AllDataTypes', Step 'Basic WF_1stepfail/0100_StepFail' failed.");
+  it("Should run 'UI WFService Sub 1 step' workflow", async () => {
+    await openMenuItem(
+      page,
+      [
+        topMenuHeader,
+        widgetsMenuItemId,
+        workflowTestItemId,
+        uiWFServiceSub1stepItemId
+      ]);
+    await clickElement("Next");
+    await waitForWorkflowMessage('Merge context \'AllDataTypes\', Step \'Basic WF_1stepfail/0100_StepFail\' failed.');
+    await clickElement("Close","button");
   });
-  it("Should run 'CallServiceWorkflow Sub 2 step' workflow", async () => {
-    await clickElement("Run Test Workflow");
-    await clickElement("CallServiceWorkflow Sub 2 step");
-    await waitForErrorWindowWithMessageAndClose("Fail Step");
-  });
-  it("Should run 'CallServiceWorkflow Sub UI step' workflow", async () => {
-    await clickElement("Run Test Workflow");
-    await clickElement("CallServiceWorkflow Sub UI step");
+  it("Should run 'UI WFService Sub 2 steps' workflow", async () => {
+    await openMenuItem(
+      page,
+      [
+        topMenuHeader,
+        widgetsMenuItemId,
+        workflowTestItemId,
+        uiWFServiceSub2stepItemId
+      ]);
+    await clickElement("Next");
     await waitForWorkflowMessage('Fail Step');
     await clickElement("Close","button");
   });
-  it("Should run 'CallSubWorkflow 1 step' workflow", async () => {
-    await clickElement("Run Test Workflow")
-    await clickElement("CallSubWorkflow 1 step")
-    await waitForErrorWindowWithMessageAndClose(
-      "Server error occurred. Please check server log for more details:\n" +
-      "Merge context 'AllDataTypes', Step 'Basic WF_1stepfail/0100_StepFail' failed.");
-  });
-  it("Should run 'CallSubWorkflow 2 step' workflow", async () => {
-    await clickElement("Run Test Workflow");
-    await clickElement("CallSubWorkflow 2 step");
-    await waitForErrorWindowWithMessageAndClose("Fail Step");
-  });
-  it("Should run 'CallSubWorkflow UI step' workflow", async () => {
-    await clickElement("Run Test Workflow");
-    await clickElement("CallSubWorkflow UI step");
+  it("Should run 'UI WFService Sub UI' workflow", async () => {
+    await openMenuItem(
+      page,
+      [
+        topMenuHeader,
+        widgetsMenuItemId,
+        workflowTestItemId,
+        uiWFServiceSubUiItemId
+      ]);
     await clickElement("Next");
-    await waitForWorkflowMessage('Merge context \'AllDataTypes\', Step \'Basic WF_UI_NextStepFail/0300_StepFail\' failed.');
+    await waitForWorkflowMessage('Item has already been added. Key in dictionary:');
     await clickElement("Close","button");
   });
 });
