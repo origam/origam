d5ff2efa6d79bd62a0a9ace729f50ef524725290
Jindrich Susen <jindrich.susen@advantages.cz>
2023-12-21 13:36:54 +0100

    Removed all passing tests

--- test/tests_e2e/02_filter.test.js
+++ test/tests_e2e/02_filter.test.js
@@ -38,330 +38,330 @@ const comboPropertyId ="14be2199-ad7f-43c3-83bf-a27c1fa66f7c";
 const tagPropertyId ="3c685902-b55b-45cb-807c-01e8386bb313";
 
 describe("Html client", () => {
-  it("Should perform basic text filter tests", async () => {
-    await login(page);
-    await openMenuItem(
-      page,
-      [
-        topMenuHeader,
-        widgetsMenuItemId,
-        allDataTypesMenuId
-      ]);
-
-    await waitForRowCount(page, dataViewId,30);
-
-    await sleep(300);
-
-    await openFilters({
-      page: page,
-      dataViewId: dataViewId,
-      aPropertyId: date1PropertyId
-    });
-
-    await sleep(300);
-
-    await setFilter({
-      page: page,
-      propertyId: text1PropertyId ,
-      comboOptionText: "contains",
-      value: "2"
-    });
-
-    await waitForRowCount(page, dataViewId,12);
-
-    await setFilter({
-      page: page,
-      propertyId: text1PropertyId ,
-      comboOptionText: "begins with",
-      value: "txt3"
-    })
-
-    await waitForRowCount(page, dataViewId,10);
-
-    await setFilter({
-      page: page,
-      propertyId: text1PropertyId ,
-      comboOptionText: "not begins with",
-      value: "txt3"
-    })
-
-    await waitForRowCount(page, dataViewId,20);
-
-    await setFilter({
-      page: page,
-      propertyId: text1PropertyId ,
-      comboOptionText: "not contains",
-      value: "2"
-    })
-
-    await waitForRowCount(page, dataViewId,18);
-
-    await setFilter({
-      page: page,
-      propertyId: text1PropertyId ,
-      comboOptionText: "ends with",
-      value: "5"
-    })
-
-    await waitForRowCount(page, dataViewId,3);
-
-    await setFilter({
-      page: page,
-      propertyId: text1PropertyId ,
-      comboOptionText: "not ends with",
-      value: "5"
-    })
-
-    await waitForRowCount(page, dataViewId,27);
-
-    await setFilter({
-      page: page,
-      propertyId: text1PropertyId ,
-      comboOptionText: "=",
-      value: "txt25"
-    })
-
-    await waitForRowCount(page, dataViewId,1);
-
-    await setFilter({
-      page: page,
-      propertyId: text1PropertyId ,
-      comboOptionText: "≠",
-      value: "txt25"
-    })
-
-    await waitForRowCount(page, dataViewId,29);
-
-    await setFilter({
-      page: page,
-      propertyId: text1PropertyId ,
-      comboOptionText: "is null",
-      value: undefined
-    })
-
-    await waitForRowCount(page, dataViewId,0);
-
-    await setFilter({
-      page: page,
-      propertyId: text1PropertyId ,
-      comboOptionText: "is not null",
-      value: undefined
-    })
-
-    await waitForRowCount(page, dataViewId,30);
-  });
-  it("Should perform basic number filter tests", async () => {
-    await login(page);
-    await openMenuItem(
-      page,
-      [
-        topMenuHeader,
-        widgetsMenuItemId,
-        allDataTypesMenuId
-      ]);
-
-    await waitForRowCount(page, dataViewId,30);
-
-    await sleep(300);
-
-    await openFilters({
-      page: page,
-      dataViewId: dataViewId,
-      aPropertyId: date1PropertyId
-    });
-
-    await sleep(300);
-
-    await setFilter({
-      page: page,
-      propertyId: integer1PropertyId ,
-      comboOptionText: "=",
-      value: "20"
-    })
-
-    await waitForRowCount(page, dataViewId,1);
-
-    await setFilter({
-      page: page,
-      propertyId: integer1PropertyId ,
-      comboOptionText: "≠",
-      value: "20"
-    })
-
-    await waitForRowCount(page, dataViewId,29);
-
-    await setFilter({
-      page: page,
-      propertyId: integer1PropertyId ,
-      comboOptionText: "≤",
-      value: "11"
-    })
-
-    await waitForRowCount(page, dataViewId,11);
-
-    await setFilter({
-      page: page,
-      propertyId: integer1PropertyId ,
-      comboOptionText: "≥",
-      value: "11"
-    })
-
-    await waitForRowCount(page, dataViewId,20);
-
-    await setFilter({
-      page: page,
-      propertyId: integer1PropertyId ,
-      comboOptionText: "<",
-      value: "11"
-    })
-
-    await waitForRowCount(page, dataViewId,10);
-
-    await setFilter({
-      page: page,
-      propertyId: integer1PropertyId ,
-      comboOptionText: ">",
-      value: "11"
-    })
-
-    await waitForRowCount(page, dataViewId,19);
-
-    await setTwoFieldFilter({
-      page: page,
-      propertyId: integer1PropertyId ,
-      comboOptionText: "between",
-      fromValue: "5",
-      toValue: "14",
-    })
-
-    await waitForRowCount(page, dataViewId,9);
-
-    await setTwoFieldFilter({
-      page: page,
-      propertyId: integer1PropertyId ,
-      comboOptionText: "not between",
-      fromValue: "5",
-      toValue: "14",
-    })
-
-    await waitForRowCount(page, dataViewId,21);
-
-      await setFilter({
-        page: page,
-        propertyId: integer1PropertyId ,
-        comboOptionText: "is null",
-        value: undefined
-      })
-
-    await waitForRowCount(page, dataViewId,0);
-
-      await setFilter({
-        page: page,
-        propertyId: integer1PropertyId ,
-        comboOptionText: "is not null",
-        value: undefined
-      })
-
-    await waitForRowCount(page, dataViewId,30);
-  });
-  it("Should perform basic bool filter tests", async () => {
-    await login(page);
-    await openMenuItem(
-      page,
-      [
-        topMenuHeader,
-        widgetsMenuItemId,
-        allDataTypesMenuId
-      ]);
-
-    await waitForRowCount(page, dataViewId,30);
-
-    await sleep(300);
-
-    await openFilters({
-      page: page,
-      dataViewId: dataViewId,
-      aPropertyId: date1PropertyId
-    });
-
-    await sleep(300);
-
-    let boolFilterCheckBox = await page.waitForSelector(`#input_${boolean1PropertyId}`);
-    await boolFilterCheckBox.click();
-
-    await waitForRowCount(page, dataViewId,1);
-
-    await boolFilterCheckBox.click();
-
-    await waitForRowCount(page, dataViewId,29);
-  });
-  it("Should perform basic date filter tests1", async () => {
-    await login(page);
-    await openMenuItem(
-      page,
-      [
-        topMenuHeader,
-        widgetsMenuItemId,
-        allDataTypesMenuId
-      ]);
-
-    await waitForRowCountData(page, dataViewId,30);
-
-    await sleep(300);
-
-    await openFilters({
-      page: page,
-      dataViewId: dataViewId,
-      aPropertyId: date1PropertyId
-    });
-
-    await setDateFilter({
-      page: page,
-      propertyId: date1PropertyId ,
-      comboOptionText: ">",
-      value: "03.07.2021"
-    })
-    await sleep(300);
-    await waitForRowCountData(page, dataViewId,19);
-
-    await setTwoFieldDateFilter({
-      page: page,
-      propertyId: date1PropertyId ,
-      comboOptionText: "between",
-      fromValue: "03.07.2021",
-      toValue: "15.07.2021",
-    })
-
-    await waitForRowCountData(page, dataViewId,13);
-
-    await setTwoFieldDateFilter({
-      page: page,
-      propertyId: date1PropertyId ,
-      comboOptionText: "not between",
-      fromValue: "03.07.2021",
-      toValue: "15.07.2021",
-    })
-
-    await waitForRowCountData(page, dataViewId,17);
-
-    await setDateFilter({
-      page: page,
-      propertyId: date1PropertyId ,
-      comboOptionText: "is null",
-      value: undefined
-    })
-
-    await waitForRowCountData(page, dataViewId,0);
-
-    await setDateFilter({
-      page: page,
-      propertyId: date1PropertyId ,
-      comboOptionText: "is not null",
-      value: undefined
-    })
-
-    await waitForRowCountData(page, dataViewId,30);
-  });
+  // it("Should perform basic text filter tests", async () => {
+  //   await login(page);
+  //   await openMenuItem(
+  //     page,
+  //     [
+  //       topMenuHeader,
+  //       widgetsMenuItemId,
+  //       allDataTypesMenuId
+  //     ]);
+  //
+  //   await waitForRowCount(page, dataViewId,30);
+  //
+  //   await sleep(300);
+  //
+  //   await openFilters({
+  //     page: page,
+  //     dataViewId: dataViewId,
+  //     aPropertyId: date1PropertyId
+  //   });
+  //
+  //   await sleep(300);
+  //
+  //   await setFilter({
+  //     page: page,
+  //     propertyId: text1PropertyId ,
+  //     comboOptionText: "contains",
+  //     value: "2"
+  //   });
+  //
+  //   await waitForRowCount(page, dataViewId,12);
+  //
+  //   await setFilter({
+  //     page: page,
+  //     propertyId: text1PropertyId ,
+  //     comboOptionText: "begins with",
+  //     value: "txt3"
+  //   })
+  //
+  //   await waitForRowCount(page, dataViewId,10);
+  //
+  //   await setFilter({
+  //     page: page,
+  //     propertyId: text1PropertyId ,
+  //     comboOptionText: "not begins with",
+  //     value: "txt3"
+  //   })
+  //
+  //   await waitForRowCount(page, dataViewId,20);
+  //
+  //   await setFilter({
+  //     page: page,
+  //     propertyId: text1PropertyId ,
+  //     comboOptionText: "not contains",
+  //     value: "2"
+  //   })
+  //
+  //   await waitForRowCount(page, dataViewId,18);
+  //
+  //   await setFilter({
+  //     page: page,
+  //     propertyId: text1PropertyId ,
+  //     comboOptionText: "ends with",
+  //     value: "5"
+  //   })
+  //
+  //   await waitForRowCount(page, dataViewId,3);
+  //
+  //   await setFilter({
+  //     page: page,
+  //     propertyId: text1PropertyId ,
+  //     comboOptionText: "not ends with",
+  //     value: "5"
+  //   })
+  //
+  //   await waitForRowCount(page, dataViewId,27);
+  //
+  //   await setFilter({
+  //     page: page,
+  //     propertyId: text1PropertyId ,
+  //     comboOptionText: "=",
+  //     value: "txt25"
+  //   })
+  //
+  //   await waitForRowCount(page, dataViewId,1);
+  //
+  //   await setFilter({
+  //     page: page,
+  //     propertyId: text1PropertyId ,
+  //     comboOptionText: "≠",
+  //     value: "txt25"
+  //   })
+  //
+  //   await waitForRowCount(page, dataViewId,29);
+  //
+  //   await setFilter({
+  //     page: page,
+  //     propertyId: text1PropertyId ,
+  //     comboOptionText: "is null",
+  //     value: undefined
+  //   })
+  //
+  //   await waitForRowCount(page, dataViewId,0);
+  //
+  //   await setFilter({
+  //     page: page,
+  //     propertyId: text1PropertyId ,
+  //     comboOptionText: "is not null",
+  //     value: undefined
+  //   })
+  //
+  //   await waitForRowCount(page, dataViewId,30);
+  // });
+  // it("Should perform basic number filter tests", async () => {
+  //   await login(page);
+  //   await openMenuItem(
+  //     page,
+  //     [
+  //       topMenuHeader,
+  //       widgetsMenuItemId,
+  //       allDataTypesMenuId
+  //     ]);
+  //
+  //   await waitForRowCount(page, dataViewId,30);
+  //
+  //   await sleep(300);
+  //
+  //   await openFilters({
+  //     page: page,
+  //     dataViewId: dataViewId,
+  //     aPropertyId: date1PropertyId
+  //   });
+  //
+  //   await sleep(300);
+  //
+  //   await setFilter({
+  //     page: page,
+  //     propertyId: integer1PropertyId ,
+  //     comboOptionText: "=",
+  //     value: "20"
+  //   })
+  //
+  //   await waitForRowCount(page, dataViewId,1);
+  //
+  //   await setFilter({
+  //     page: page,
+  //     propertyId: integer1PropertyId ,
+  //     comboOptionText: "≠",
+  //     value: "20"
+  //   })
+  //
+  //   await waitForRowCount(page, dataViewId,29);
+  //
+  //   await setFilter({
+  //     page: page,
+  //     propertyId: integer1PropertyId ,
+  //     comboOptionText: "≤",
+  //     value: "11"
+  //   })
+  //
+  //   await waitForRowCount(page, dataViewId,11);
+  //
+  //   await setFilter({
+  //     page: page,
+  //     propertyId: integer1PropertyId ,
+  //     comboOptionText: "≥",
+  //     value: "11"
+  //   })
+  //
+  //   await waitForRowCount(page, dataViewId,20);
+  //
+  //   await setFilter({
+  //     page: page,
+  //     propertyId: integer1PropertyId ,
+  //     comboOptionText: "<",
+  //     value: "11"
+  //   })
+  //
+  //   await waitForRowCount(page, dataViewId,10);
+  //
+  //   await setFilter({
+  //     page: page,
+  //     propertyId: integer1PropertyId ,
+  //     comboOptionText: ">",
+  //     value: "11"
+  //   })
+  //
+  //   await waitForRowCount(page, dataViewId,19);
+  //
+  //   await setTwoFieldFilter({
+  //     page: page,
+  //     propertyId: integer1PropertyId ,
+  //     comboOptionText: "between",
+  //     fromValue: "5",
+  //     toValue: "14",
+  //   })
+  //
+  //   await waitForRowCount(page, dataViewId,9);
+  //
+  //   await setTwoFieldFilter({
+  //     page: page,
+  //     propertyId: integer1PropertyId ,
+  //     comboOptionText: "not between",
+  //     fromValue: "5",
+  //     toValue: "14",
+  //   })
+  //
+  //   await waitForRowCount(page, dataViewId,21);
+  //
+  //     await setFilter({
+  //       page: page,
+  //       propertyId: integer1PropertyId ,
+  //       comboOptionText: "is null",
+  //       value: undefined
+  //     })
+  //
+  //   await waitForRowCount(page, dataViewId,0);
+  //
+  //     await setFilter({
+  //       page: page,
+  //       propertyId: integer1PropertyId ,
+  //       comboOptionText: "is not null",
+  //       value: undefined
+  //     })
+  //
+  //   await waitForRowCount(page, dataViewId,30);
+  // });
+  // it("Should perform basic bool filter tests", async () => {
+  //   await login(page);
+  //   await openMenuItem(
+  //     page,
+  //     [
+  //       topMenuHeader,
+  //       widgetsMenuItemId,
+  //       allDataTypesMenuId
+  //     ]);
+  //
+  //   await waitForRowCount(page, dataViewId,30);
+  //
+  //   await sleep(300);
+  //
+  //   await openFilters({
+  //     page: page,
+  //     dataViewId: dataViewId,
+  //     aPropertyId: date1PropertyId
+  //   });
+  //
+  //   await sleep(300);
+  //
+  //   let boolFilterCheckBox = await page.waitForSelector(`#input_${boolean1PropertyId}`);
+  //   await boolFilterCheckBox.click();
+  //
+  //   await waitForRowCount(page, dataViewId,1);
+  //
+  //   await boolFilterCheckBox.click();
+  //
+  //   await waitForRowCount(page, dataViewId,29);
+  // });
+  // it("Should perform basic date filter tests1", async () => {
+  //   await login(page);
+  //   await openMenuItem(
+  //     page,
+  //     [
+  //       topMenuHeader,
+  //       widgetsMenuItemId,
+  //       allDataTypesMenuId
+  //     ]);
+  //
+  //   await waitForRowCountData(page, dataViewId,30);
+  //
+  //   await sleep(300);
+  //
+  //   await openFilters({
+  //     page: page,
+  //     dataViewId: dataViewId,
+  //     aPropertyId: date1PropertyId
+  //   });
+  //
+  //   await setDateFilter({
+  //     page: page,
+  //     propertyId: date1PropertyId ,
+  //     comboOptionText: ">",
+  //     value: "03.07.2021"
+  //   })
+  //   await sleep(300);
+  //   await waitForRowCountData(page, dataViewId,19);
+  //
+  //   await setTwoFieldDateFilter({
+  //     page: page,
+  //     propertyId: date1PropertyId ,
+  //     comboOptionText: "between",
+  //     fromValue: "03.07.2021",
+  //     toValue: "15.07.2021",
+  //   })
+  //
+  //   await waitForRowCountData(page, dataViewId,13);
+  //
+  //   await setTwoFieldDateFilter({
+  //     page: page,
+  //     propertyId: date1PropertyId ,
+  //     comboOptionText: "not between",
+  //     fromValue: "03.07.2021",
+  //     toValue: "15.07.2021",
+  //   })
+  //
+  //   await waitForRowCountData(page, dataViewId,17);
+  //
+  //   await setDateFilter({
+  //     page: page,
+  //     propertyId: date1PropertyId ,
+  //     comboOptionText: "is null",
+  //     value: undefined
+  //   })
+  //
+  //   await waitForRowCountData(page, dataViewId,0);
+  //
+  //   await setDateFilter({
+  //     page: page,
+  //     propertyId: date1PropertyId ,
+  //     comboOptionText: "is not null",
+  //     value: undefined
+  //   })
+  //
+  //   await waitForRowCountData(page, dataViewId,30);
+  // });
   it("Should perform basic combo input filter tests", async () => {
     await login(page);
     await openMenuItem(
@@ -451,60 +451,60 @@ describe("Html client", () => {
 
     await waitForRowCountData(page, dataViewId,6);
   });
-  it("Should perform basic tag input filter tests", async () => {
-    await login(page);
-    await openMenuItem(
-      page,
-      [
-        topMenuHeader,
-        widgetsMenuItemId,
-        allDataTypesMenuId
-      ]);
-
-    await waitForRowCountData(page, dataViewId,30);
-
-    await sleep(300);
-
-    await openFilters({
-      page: page,
-      dataViewId: dataViewId,
-      aPropertyId: date1PropertyId
-    });
-
-    await setComboFilter({
-      page: page,
-      propertyId: tagPropertyId ,
-      comboOptionText: "=",
-      value: "Label1"
-    })
-
-    await waitForRowCountData(page, dataViewId,2);
-
-    await setComboFilter({
-      page: page,
-      propertyId: tagPropertyId ,
-      comboOptionText: "≠",
-    })
-
-    await waitForRowCountData(page, dataViewId,28);
-
-    await setFilter({
-      page: page,
-      propertyId: tagPropertyId ,
-      comboOptionText: "is null",
-      value: undefined
-    })
-
-    await waitForRowCountData(page, dataViewId,26);
-
-    await setFilter({
-      page: page,
-      propertyId: tagPropertyId ,
-      comboOptionText: "is not null",
-      value: undefined
-    })
-
-    await waitForRowCountData(page, dataViewId,4);
-  });
+  // it("Should perform basic tag input filter tests", async () => {
+  //   await login(page);
+  //   await openMenuItem(
+  //     page,
+  //     [
+  //       topMenuHeader,
+  //       widgetsMenuItemId,
+  //       allDataTypesMenuId
+  //     ]);
+  //
+  //   await waitForRowCountData(page, dataViewId,30);
+  //
+  //   await sleep(300);
+  //
+  //   await openFilters({
+  //     page: page,
+  //     dataViewId: dataViewId,
+  //     aPropertyId: date1PropertyId
+  //   });
+  //
+  //   await setComboFilter({
+  //     page: page,
+  //     propertyId: tagPropertyId ,
+  //     comboOptionText: "=",
+  //     value: "Label1"
+  //   })
+  //
+  //   await waitForRowCountData(page, dataViewId,2);
+  //
+  //   await setComboFilter({
+  //     page: page,
+  //     propertyId: tagPropertyId ,
+  //     comboOptionText: "≠",
+  //   })
+  //
+  //   await waitForRowCountData(page, dataViewId,28);
+  //
+  //   await setFilter({
+  //     page: page,
+  //     propertyId: tagPropertyId ,
+  //     comboOptionText: "is null",
+  //     value: undefined
+  //   })
+  //
+  //   await waitForRowCountData(page, dataViewId,26);
+  //
+  //   await setFilter({
+  //     page: page,
+  //     propertyId: tagPropertyId ,
+  //     comboOptionText: "is not null",
+  //     value: undefined
+  //   })
+  //
+  //   await waitForRowCountData(page, dataViewId,4);
+  // });
 });
 
