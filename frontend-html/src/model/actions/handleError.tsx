63651179720d5418510708890df813cafb3901cf
Jindrich Susen <jindrich.susen@advantages.cz>
2024-01-02 14:24:41 +0100

    Errors raised right after the application startup were not shown to the user

--- frontend-html/src/model/actions/handleError.tsx
+++ frontend-html/src/model/actions/handleError.tsx
@@ -30,10 +30,12 @@ const HANDLED = Symbol("_$ErrorHandled");
 export function handleError(ctx: any) {
   return function*handleError(error: any) {
     const openedScreen = getOpenedScreen(ctx);
-    const openedScreens = getOpenedScreens(openedScreen);
-    if (!openedScreens.items.includes(openedScreen)) {
-      // error on an already closed screen
-      return;
+    if(openedScreen){
+      const openedScreens = getOpenedScreens(openedScreen);
+      if (!openedScreens.items.includes(openedScreen)) {
+        // error on an already closed screen
+        return;
+      }
     }
     if (error.response && error.response.status === 474) {
       // 747 ~ ServerObjectDisposed happens when the user closes a form before all pending requests have
@@ -60,26 +62,13 @@ export function handleError(ctx: any) {
       );
       return;
     }
-    if (error.response && error.response.status === 401) {
-      yield*stopWorkQueues(ctx)();
-      selectors.error.getDialogController(ctx).dismissErrors();
-      yield*selectors.error.getDialogController(ctx).pushError(
-        T(
-          `Your request is no longer authorized, which means that 
-          you were either logged out or your session expired. Please log in again.`,
-          "request_no_longer_authorized"
-        )
-      );
-      yield*performLogout(ctx)();
+    if (error[HANDLED]) {
+      yield error[HANDLED];
       return;
-    } else {
-      if (error[HANDLED]) {
-        yield error[HANDLED];
-        return;
-      }
-      const promise = flow(() => selectors.error.getDialogController(ctx).pushError(error))()
-      error[HANDLED] = promise;
-      yield promise;
     }
+    const promise = flow(() => selectors.error.getDialogController(ctx).pushError(error))()
+    error[HANDLED] = promise;
+    yield promise;
+
   };
 }
