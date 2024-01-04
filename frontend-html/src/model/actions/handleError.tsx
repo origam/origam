2304b363d548179c21fb00bf61bd0753d063b4b4
Jindrich Susen <jindrich.susen@advantages.cz>
2024-01-04 09:17:16 +0100

    Accidentally removed code returned

--- frontend-html/src/model/actions/handleError.tsx
+++ frontend-html/src/model/actions/handleError.tsx
@@ -62,6 +62,19 @@ export function handleError(ctx: any) {
       );
       return;
     }
+    if (error.response && error.response.status === 401) {
+      yield*stopWorkQueues(ctx)();
+      selectors.error.getDialogController(ctx).dismissErrors();
+      yield*selectors.error.getDialogController(ctx).pushError(
+        T(
+          `Your request is no longer authorized, which means that 
+          you were either logged out or your session expired. Please log in again.`,
+          "request_no_longer_authorized"
+        )
+      );
+      yield*performLogout(ctx)();
+      return;
+    }
     if (error[HANDLED]) {
       yield error[HANDLED];
       return;
