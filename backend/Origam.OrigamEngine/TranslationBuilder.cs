ea17ae6bb789009055ff74c246ae61c404ad187e
Jindrich Susen <jindrich.susen@advantages.cz>
2024-02-02 12:20:58 +0100

    Only documenation categories found in LocalizationIncludedDocumentationElements are localized

--- backend/Origam.OrigamEngine/TranslationBuilder.cs
+++ backend/Origam.OrigamEngine/TranslationBuilder.cs
@@ -28,6 +28,7 @@ using System.ComponentModel;
 using Origam.DA.ObjectPersistence;
 using Origam.Schema;
 using Origam.Workbench.Services;
+using System.Linq;
 
 namespace Origam.OrigamEngine
 {
@@ -37,95 +38,110 @@ namespace Origam.OrigamEngine
 	public static class TranslationBuilder
 	{
         public static void Build(Stream stream, LocalizationCache currentTranslations, string locale, Guid packageId)
-		{
-			XmlTextWriter xtw = new XmlTextWriter(stream, System.Text.Encoding.UTF8);
-			xtw.Formatting = Formatting.Indented;
-			xtw.WriteStartDocument(true);
-			xtw.WriteStartElement("OrigamLocalization");
+        {
+            XmlTextWriter xtw = new XmlTextWriter(stream, System.Text.Encoding.UTF8);
+            xtw.Formatting = Formatting.Indented;
+            xtw.WriteStartDocument(true);
+            xtw.WriteStartElement("OrigamLocalization");
 
-			IPersistenceService persistence = ServiceManager.Services.GetService(typeof(IPersistenceService)) as IPersistenceService;
-			IDocumentationService docSvc = ServiceManager.Services.GetService(typeof(IDocumentationService)) as IDocumentationService;
+            IPersistenceService persistence = ServiceManager.Services.GetService(typeof(IPersistenceService)) as IPersistenceService;
+            IDocumentationService docSvc = ServiceManager.Services.GetService(typeof(IDocumentationService)) as IDocumentationService;
 
-			List<AbstractSchemaItem> list = persistence
-				.SchemaProvider
-				.RetrieveListByPackage<AbstractSchemaItem>(packageId);
+            List<AbstractSchemaItem> list = persistence
+                .SchemaProvider
+                .RetrieveListByPackage<AbstractSchemaItem>(packageId);
 
-			foreach(AbstractSchemaItem item in list)
-			{
-				IList memberList = Reflector.FindMembers(item.GetType(), typeof(LocalizableAttribute), new Type[]{});
-				Hashtable values = new Hashtable();
-				IQueryLocalizable ql = item as IQueryLocalizable;
+            foreach (AbstractSchemaItem item in list)
+            {
+                IList memberList = Reflector.FindMembers(item.GetType(), typeof(LocalizableAttribute), new Type[] { });
+                Hashtable values = new Hashtable();
+                IQueryLocalizable ql = item as IQueryLocalizable;
 
-				foreach(MemberAttributeInfo mai in memberList)
-				{
-					bool isLocalizable = true;
-					
-					if(ql != null)
-					{
-						isLocalizable = ql.IsLocalizable(mai.MemberInfo.Name);
-					}
+                foreach (MemberAttributeInfo mai in memberList)
+                {
+                    bool isLocalizable = true;
 
-					if(isLocalizable)
-					{
-						object translatableValue = Reflector.GetValue(mai.MemberInfo, item);
+                    if (ql != null)
+                    {
+                        isLocalizable = ql.IsLocalizable(mai.MemberInfo.Name);
+                    }
 
-						if(translatableValue != null && translatableValue.ToString() != "")
-						{
-							values.Add(mai.MemberInfo.Name, translatableValue);
-						}
-					}
-				}
-				DocumentationComplete docData = docSvc.LoadDocumentation(item.Id);
+                    if (isLocalizable)
+                    {
+                        object translatableValue = Reflector.GetValue(mai.MemberInfo, item);
 
-				if(values.Count > 0 || docData.Documentation.Count > 0)
-				{
-					xtw.WriteStartElement("Element");
-					xtw.WriteAttributeString("Id", item.Id.ToString());
-					xtw.WriteAttributeString("Path", item.Path);
+                        if (translatableValue != null && translatableValue.ToString() != "")
+                        {
+                            values.Add(mai.MemberInfo.Name, translatableValue);
+                        }
+                    }
+                }
+                DocumentationComplete docData = docSvc.LoadDocumentation(item.Id);
 
-					foreach(DictionaryEntry entry in values)
-					{
-						xtw.WriteStartElement((string)entry.Key);
-						xtw.WriteAttributeString("OriginalText", entry.Value.ToString());
-						string translation;
-						if(currentTranslations != null)
-						{
-							translation = currentTranslations.GetLocalizedString(item.Id, (string)entry.Key, entry.Value.ToString(), locale);
-						}
-						else
-						{
-							translation = entry.Value.ToString();
-						}
-						xtw.WriteString(translation);
-						xtw.WriteEndElement();
-					}
-					
+                if (values.Count > 0 || docData.Documentation.Count > 0)
+                {
+                    xtw.WriteStartElement("Element");
+                    xtw.WriteAttributeString("Id", item.Id.ToString());
+                    xtw.WriteAttributeString("Path", item.Path);
 
-					foreach(DocumentationComplete.DocumentationRow docRow in docData.Documentation)
-					{
-						xtw.WriteStartElement("Documentation");
-						xtw.WriteAttributeString("Category", docRow.Category);
-						xtw.WriteAttributeString("OriginalText",  docRow.Data.ToString());
-						string translation;
-						if(currentTranslations != null)
-						{
-							translation = currentTranslations.GetLocalizedString(item.Id, "Documentation " + docRow.Category, docRow.Data.ToString(), locale);
-						}
-						else
-						{
-							translation = docRow.Data.ToString();
-						}
-						xtw.WriteString(translation);
-						xtw.WriteEndElement();
-					}
-					
-					xtw.WriteEndElement();
-				}
-			}
+                    foreach (DictionaryEntry entry in values)
+                    {
+                        xtw.WriteStartElement((string)entry.Key);
+                        xtw.WriteAttributeString("OriginalText", entry.Value.ToString());
+                        string translation;
+                        if (currentTranslations != null)
+                        {
+                            translation = currentTranslations.GetLocalizedString(item.Id, (string)entry.Key, entry.Value.ToString(), locale);
+                        }
+                        else
+                        {
+                            translation = entry.Value.ToString();
+                        }
+                        xtw.WriteString(translation);
+                        xtw.WriteEndElement();
+                    }
+                    string[] categoriesToInclude = GetDocumentationCategoriesToInclude();
+                    foreach (DocumentationComplete.DocumentationRow docRow in docData.Documentation)
+                    {
+                        if (!categoriesToInclude.Contains(docRow.Category)) {
+                            continue;
+                        }
+                        xtw.WriteStartElement("Documentation");
+                        xtw.WriteAttributeString("Category", docRow.Category);
+                        xtw.WriteAttributeString("OriginalText", docRow.Data.ToString());
+                        string translation;
+                        if (currentTranslations != null)
+                        {
+                            translation = currentTranslations.GetLocalizedString(item.Id, "Documentation " + docRow.Category, docRow.Data.ToString(), locale);
+                        }
+                        else
+                        {
+                            translation = docRow.Data.ToString();
+                        }
+                        xtw.WriteString(translation);
+                        xtw.WriteEndElement();
+                    }
 
-			xtw.WriteEndElement();
-			xtw.WriteEndDocument();
-			xtw.Flush();
-		}
-	}
+                    xtw.WriteEndElement();
+                }
+            }
+
+            xtw.WriteEndElement();
+            xtw.WriteEndDocument();
+            xtw.Flush();
+        }
+
+        private static string[] GetDocumentationCategoriesToInclude()
+        {
+            OrigamSettings settings = ConfigurationManager.GetActiveConfiguration();
+            if (settings.LocalizationIncludedDocumentationElements == null) { 
+                return new string[0];
+            }
+            return settings.LocalizationIncludedDocumentationElements
+                .Split(',')
+                .Select(x => x.Trim())
+                .Where(x => x != "")
+                .ToArray();
+        }
+    }
 }
