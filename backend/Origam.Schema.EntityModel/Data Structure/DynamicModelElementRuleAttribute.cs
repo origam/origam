578e87d19fd2451933123a170c97b56beb5b7226
Jiri Prajz <jiri.prajz@advantages.cz>
2024-03-14 13:41:11 +0100

    FEATURE: Added a check for dynamic filters to Architect

---
+++ backend/Origam.Schema.EntityModel/Data Structure/DynamicModelElementRuleAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Origam.DA.ObjectPersistence;
+using System;
+using System.Collections;
+
+namespace Origam.Schema.EntityModel
+{
+    internal class DynamicModelElementRuleAttribute : AbstractModelElementRuleAttribute
+    {
+        public override Exception CheckRule(object instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Exception CheckRule(object instance, string memberName)
+        {
+            if (memberName == string.Empty | memberName == null) CheckRule(instance);
+
+            var filterset = (DataStructureFilterSet)instance;
+            ArrayList filters = filterset.ChildItemsRecursive;
+            foreach (DataStructureFilterSetFilter filter in filters)
+            {
+                if ((filter.IgnoreFilterConstantId != Guid.Empty || 
+                        !string.IsNullOrEmpty(filter.IgnoreFilterParameterName) ||
+                        filter.PassWhenParameterMatch) && 
+                        !filterset.IsDynamic)
+                {
+                    return new Exception(ResourceUtils.GetString("ErrorDynamicParameter", memberName));
+                }
+            }
+            return null;
+        }
+    }
+}
