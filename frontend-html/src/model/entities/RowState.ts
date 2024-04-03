e80914d884306b273fdff4a02afeb959efa58016
Jindrich Susen <jindrich.susen@advantages.cz>
2024-04-03 12:16:03 +0200

    Duplicate code removed

--- frontend-html/src/model/entities/RowState.ts
+++ frontend-html/src/model/entities/RowState.ts
@@ -236,11 +236,7 @@ export class RowState implements IRowState {
   clearValue(rowId: string){
     const rowStateRequest = this.requests.get(rowId);
     if (rowStateRequest) {
-      rowStateRequest.atom?.onBecomeUnobservedListeners?.clear();
-      rowStateRequest.atom?.onBecomeObservedListeners?.clear();
-      rowStateRequest.atom = undefined;
-      rowStateRequest.isValid = false;
-      rowStateRequest.processingSate = undefined;
+      rowStateRequest.dispose();
       this.requests.delete(rowId);
     }
   }
@@ -250,21 +246,13 @@ export class RowState implements IRowState {
     this.temporaryRequestsValues = new Map(this.requests.entries());
     // This actually causes reloading of the values (by views calling getValue(...) )
     for (let rowStateRequest of this.requests.values()) {
-      rowStateRequest.atom?.onBecomeUnobservedListeners?.clear();
-      rowStateRequest.atom?.onBecomeObservedListeners?.clear();
-      rowStateRequest.atom = undefined;
-      rowStateRequest.isValid = false;
-      rowStateRequest.processingSate = undefined;
+      rowStateRequest.dispose();
     }
   }
 
   @action.bound clearAll() {
     for (let rowStateRequest of this.requests.values()) {
-      rowStateRequest.atom?.onBecomeUnobservedListeners?.clear();
-      rowStateRequest.atom?.onBecomeObservedListeners?.clear();
-      rowStateRequest.atom = undefined;
-      rowStateRequest.isValid = false;
-      rowStateRequest.processingSate = undefined;
+      rowStateRequest.dispose();
     }
     this.requests.clear();
     this.firstLoadingPerformed = false;
@@ -322,6 +310,14 @@ class RowStateRequest {
     public atom?: IAtom
   ) {
     this.rowId = rowId;
+  }
+
+  dispose() {
+    this.atom?.onBecomeUnobservedListeners?.clear();
+    this.atom?.onBecomeObservedListeners?.clear();
+    this.atom = undefined;
+    this.isValid = false;
+    this.processingSate = undefined;
   }
 }
 
