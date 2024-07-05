9d20f4dac7aa9c45f705cf4910b3617b926c2d08
Jindrich Susen <jindrich.susen@advantages.cz>
2024-07-05 10:18:03 +0200

    console.log removed from test files

--- test/tests_e2e/04_infiniteScroll.test.js
+++ test/tests_e2e/04_infiniteScroll.test.js
@@ -62,7 +62,6 @@ describe("Html client", () => {
       `#${dataViewId} .horiz-scrollbar`,
       { visible: true, timeout: 3000 }
     );
-    console.log("table: " + table);
     await sleep(500);
     await waitForRequests
 
