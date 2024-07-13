1992e7fa3c6b28361bce2c6cee6398bf0e9d4335
Jindrich Susen <jindrich.susen@advantages.cz>
2024-07-13 15:31:54 +0200

    Tooltip shown above truncated search results

--- frontend-html/src/gui/Components/Search/ResultItem.tsx
+++ frontend-html/src/gui/Components/Search/ResultItem.tsx
@@ -1,22 +1,3 @@
-/*
-Copyright 2005 - 2021 Advantage Solutions, s. r. o.
-
-This file is part of ORIGAM (http://www.origam.org).
-
-ORIGAM is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-ORIGAM is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
-*/
-
 import { observer } from "mobx-react";
 import React, { RefObject } from "react";
 import { ISearchResult } from "model/entities/types/ISearchResult";
@@ -36,13 +17,27 @@ export class ResultItem extends React.Component<{
   mouseOver = false;
 
   divRef: RefObject<HTMLDivElement> = React.createRef();
+  descriptionRef: RefObject<HTMLDivElement> = React.createRef();
+
+  @observable
+  isDescriptionTruncated = false;
 
   componentDidMount() {
     this.props.registerElementRef(this.props.result.id, this.divRef);
+    this.checkDescriptionTruncation();
   }
 
   componentDidUpdate() {
     this.props.registerElementRef(this.props.result.id, this.divRef);
+    this.checkDescriptionTruncation();
+  }
+
+  checkDescriptionTruncation = () => {
+    if (this.descriptionRef.current) {
+      const { offsetWidth, scrollWidth } = this.descriptionRef.current;
+      debugger;
+      this.isDescriptionTruncated = scrollWidth > offsetWidth;
+    }
   }
 
   @action.bound
@@ -50,7 +45,8 @@ export class ResultItem extends React.Component<{
     this.props.onResultItemClick();
     this.props.result.onClick();
   }
- render() {
+
+  render() {
     return (
       <div
         className={S.resultIemRow + " " + (this.mouseOver ? S.resultIemRowHovered : "") + " " +
@@ -67,7 +63,11 @@ export class ResultItem extends React.Component<{
             {this.props.result.label}
           </div>
           <div className={S.itemTextSeparator}/>
-          <div className={S.itemDescription}>
+          <div
+            className={S.itemDescription}
+            ref={this.descriptionRef}
+            title={this.isDescriptionTruncated ? this.props.result.description : undefined}
+          >
             {this.props.result.description}
           </div>
         </div>
