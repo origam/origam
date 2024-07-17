c2d8d62284189d97b5477215ea7a2067d62bcb3e
Jindrich Susen <jindrich.susen@advantages.cz>
2024-07-17 21:47:10 +0200

    Added Origam.Architect.Server

--- architect-html/src/App.tsx
+++ architect-html/src/App.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -18,8 +17,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={async () => await axios.get('/WeatherForecast').then(r => console.log(r.data))}>
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
