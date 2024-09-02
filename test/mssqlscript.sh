4690d930d3152b23aa2294f46e407b8dddee0df1
JindrichSusen <33699252+JindrichSusen@users.noreply.github.com>
2024-09-02 14:10:51 +0200

    DEV: Tests not running 2024.6 (#2806)
    sqlcmd location updated

--- test/mssqlscript.sh
+++ test/mssqlscript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [[ -f "first" ]]; then
-until /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -Q "SELECT 1"
-do 
+until /opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -Q "SELECT 1" -No
+do
   sleep 1
-done 
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -Q "CREATE DATABASE origam"
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -Q "CREATE LOGIN origam WITH PASSWORD = '$USER_PASSWORD'; USE origam;CREATE USER origam FOR LOGIN origam; EXEC sp_addrolemember N'db_owner', N'origam'"
+done
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -Q "CREATE DATABASE origam" -No
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -Q "CREATE LOGIN origam WITH PASSWORD = '$USER_PASSWORD'; USE origam;CREATE USER origam FOR LOGIN origam; EXEC sp_addrolemember N'db_owner', N'origam'" -No
  rm first
 fi
