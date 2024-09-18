53220ed9c552486b8cd7544883bbca2bb737e4ce
Jindrich Susen <jindrich.susen@advantages.cz>
2024-09-18 15:30:10 +0200

    License updated

--- backend/Origam.Server/Common/OrigamValidationException.cs
+++ backend/Origam.Server/Common/OrigamValidationException.cs
@@ -1,6 +1,6 @@
 ﻿#region license
 /*
-Copyright 2005 - 2021 Advantage Solutions, s. r. o.
+Copyright 2005 - 2024 Advantage Solutions, s. r. o.
 
 This file is part of ORIGAM (http://www.origam.org).
 
