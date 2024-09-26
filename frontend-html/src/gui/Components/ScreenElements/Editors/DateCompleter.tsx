52bc6e632ca14cd14a58205f5fab872aafe1d901
Jindrich Susen <jindrich.susen@advantages.cz>
2024-09-26 09:08:55 +0200

    It was not possible to set time other than 00:00
    when the date editor format was d. MMMM yyyy HH:mm

--- frontend-html/src/gui/Components/ScreenElements/Editors/DateCompleter.tsx
+++ frontend-html/src/gui/Components/ScreenElements/Editors/DateCompleter.tsx
@@ -50,7 +50,7 @@ export default class DateCompleter {
       t.match(/^\d\d\d\s+\d\d\d\d$/) ||
       t.match(/^\d\d\d\d?\s+\d\d$/) ||
       t.match(/^\d\d\d\d\d\d\d\d\s+\d\d\d\d\d\d$/) ||
-      t.match(new RegExp("^\\d\\d?\\" + this.dateSeparator + "\\d?\\d?\\" + this.dateSeparator + "?\\d?\\d?\\d?\\d?")) ||
+      t.match(new RegExp("^\\d\\d?\\" + this.dateSeparator + "\\d?\\d?\\" + this.dateSeparator + "?\\d?\\d?\\d?\\d?$")) ||
       t.match(new RegExp("^\\d\\d?\\" + this.dateSeparator + "\\d?\\d?\\" + this.dateSeparator + "?\\d?\\d?\\d?\\d? +\\d?\\d?:?\\d?\\d?:?\\d?\\d?$"))
     );
   }
@@ -65,7 +65,7 @@ export default class DateCompleter {
       t.match(/^\d\d\d\d\s+\d\d$/) ||
       t.match(/^\d\d\d\d\d\d\d\d \d\d\d\d\d\d$/) ||
       t.match(/^\d\d?$/) ||
-      t.match(new RegExp("^\\d\\d?\\" + this.dateSeparator + "\\d?\\d?\\" + this.dateSeparator + "?\\d?\\d?\\d?\\d?")) ||
+      t.match(new RegExp("^\\d\\d?\\" + this.dateSeparator + "\\d?\\d?\\" + this.dateSeparator + "?\\d?\\d?\\d?\\d?$")) ||
       t.match(new RegExp("^\\d\\d?\\" + this.dateSeparator + "\\d?\\d?\\" + this.dateSeparator + "?\\d?\\d?\\d?\\d? +\\d?\\d?:?\\d?\\d?:?\\d?\\d?$"))
     );
   }
