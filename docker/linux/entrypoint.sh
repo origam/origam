605278a34e7c0c38195b86217c51be6a17aa61fc
jiri.prajz <jiri.prajz@advantages.cz>
2024-11-14 22:04:51 +0100

    CHG: This allows the application to receive any Unix signals sent to the container.

---
+++ docker/linux/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /etc/nginx/ssl
+sudo /etc/nginx/ssl/createSslCertificate.sh
+sudo /etc/init.d/nginx start
+sudo /root/updateTimezone.sh
+cd /home/origam/HTML5
+./startServer.sh
+export ASPNETCORE_URLS="http://+:8080"
+exec dotnet Origam.Server.dll
