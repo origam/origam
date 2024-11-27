7c077da698ff175c7189c0d49666ef6dda106ed1
Jindrich Susen <jindrich.susen@advantages.cz>
2024-11-27 09:20:16 +0100

    GetStandardValues refactored

--- backend/Origam.Schema.EntityModel/Editor/Converters.cs
+++ backend/Origam.Schema.EntityModel/Editor/Converters.cs
@@ -197,40 +197,39 @@ public class DataStructureEntityConverter : TypeConverter
 		//but allow free-form entry
 		return true;
 	}
-	public override System.ComponentModel.TypeConverter.StandardValuesCollection 
+	public override StandardValuesCollection 
 		GetStandardValues(ITypeDescriptorContext context)
 	{
-		List<ISchemaItem> columnArray;
 		List<ISchemaItem> entities;
-		DataStructureEntity parentEntity = (context.Instance as DataStructureEntity).ParentItem as DataStructureEntity;
-		if(parentEntity == null)
+		DataStructureEntity parentEntity = (context.Instance as DataStructureEntity)?.ParentItem as DataStructureEntity;
+		if (parentEntity == null)
 		{
 			// Root entity, we display all available entities
-			entities =
-				(_schema.GetProvider(typeof(EntityModelSchemaItemProvider))
-					as EntityModelSchemaItemProvider).ChildItems
-				.ToList();
+			entities = _schema.GetProvider<EntityModelSchemaItemProvider>()
+					.ChildItems
+					.ToList();
 		}
 		else
 		{
 			// Sub-entity (relation), we return only available relations
-			if(parentEntity.Entity is IDataEntity)
+			if (parentEntity.Entity is IDataEntity entity)
+			{
 				// Parent is root entity
-				entities = (parentEntity.Entity as IDataEntity).EntityRelations.ToList<ISchemaItem>();
-			else if(parentEntity.Entity is IAssociation)
+				entities = entity.EntityRelations.ToList<ISchemaItem>();
+			}
+			else if (parentEntity.Entity is IAssociation association)
+			{
 				// Parent is relation
-				entities = (parentEntity.Entity as IAssociation).AssociatedEntity.EntityRelations.ToList<ISchemaItem>();
+				entities = association.AssociatedEntity.EntityRelations
+					.ToList<ISchemaItem>();
+			}
 			else
+			{
 				throw new ArgumentOutOfRangeException("ParentItem", parentEntity, ResourceUtils.GetString("ErrorParentNotIDataEntity"));
+			}
 		}
-		columnArray = new List<ISchemaItem>(entities.Count);
-		
-		foreach(ISchemaItem entity in entities)
-		{
-			columnArray.Add(entity);
-		}
-		columnArray.Sort();
-		return new StandardValuesCollection(columnArray);
+		entities.Sort();
+		return new StandardValuesCollection(entities);
 	}
 	public override bool CanConvertFrom(System.ComponentModel.ITypeDescriptorContext context, System.Type sourceType)
 	{
