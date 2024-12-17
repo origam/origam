eb19979978efc09d0cfb143d61387a0c532cc76f
Jindrich Susen <jindrich.susen@advantages.cz>
2024-12-17 17:05:50 +0100

    SheduleTimer converted to netstandard2.0

--- backend/ScheduleTimer/AssemblyInfo.cs
+++ backend/ScheduleTimer/AssemblyInfo.cs
@@ -14,34 +14,8 @@
  **************************************************************************/
 
 using System.Reflection;
-
-//
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-//
-[assembly: AssemblyTitle("Schedule Timer")]
-[assembly: AssemblyDescription("Timer for specifing absolute schedules.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Andrew Brummer 2004")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
-
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
