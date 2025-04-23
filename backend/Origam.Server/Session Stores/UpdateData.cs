b5ebe4e91636b254d2486600e5b9f96f7bfeeee3
Jindrich Susen <jindrich.susen@advantages.cz>
2025-04-23 08:47:47 +0200

    Update Data was expecting RowId to be Guid

--- backend/Origam.Server/Session Stores/UpdateData.cs
+++ backend/Origam.Server/Session Stores/UpdateData.cs
@@ -4,6 +4,6 @@ using System.Collections.Generic;
 namespace Origam.Server;
 public class UpdateData
 {
-    public Guid RowId { get; set; }
+    public string RowId { get; set; }
     public Dictionary<string, object> Values { get; set; }
 }
