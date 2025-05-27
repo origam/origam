fb1a1c71fd9fd782593f5bf03cb75617cfd3d906
jindr <jindrich.susen@advantages.cz>
2025-05-27 10:52:04 +0200

    Import fixed

--- architect-html/src/components/saveButton/SaveButton.tsx
+++ architect-html/src/components/saveButton/SaveButton.tsx
@@ -17,7 +17,6 @@ You should have received a copy of the GNU General Public License
 along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { RootStoreContext } from "src/main.tsx";
 import { RootStoreContext, T } from "src/main.tsx";
 import { useContext } from "react";
 import { observer } from "mobx-react-lite";
