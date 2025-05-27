1938947c8b7cb3f4cc133e93b841fa41d298be30
JindrichSusen <jindrich.susen@advantages.cz>
2025-05-27 14:10:38 +0200

    Code formatting error enabled

---
+++ architect-html/eslint.config.js
@@ -0,0 +1,49 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  resolvePluginsRelativeTo: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ["dist/**", "**/*.config.js", "**/*.config.cjs", "**/*.d.ts"],
+  },
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ),
+
+  {
+    files: ["src/**/*.{ts,tsx,js,jsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.eslint.json",
+        tsconfigRootDir: __dirname,
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    rules: {
+      "prettier/prettier": ["error"],
+      "@typescript-eslint/no-unused-vars": ["error"],
+    },
+    settings: {
+      react: { version: "detect" },
+    },
+  },
+];
