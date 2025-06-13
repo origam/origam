1e6262d2830617eda1ba720dfa9c0fd3e5542254
JindrichSusen <jindrich.susen@advantages.cz>
2025-06-13 09:52:53 +0200

    License added

--- backend/Origam.Architect.Server/Controllers/EditorController.cs
+++ backend/Origam.Architect.Server/Controllers/EditorController.cs
@@ -1,4 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿#region license
+/*
+Copyright 2005 - 2025 Advantage Solutions, s. r. o.
+
+This file is part of ORIGAM (http://www.origam.org).
+
+ORIGAM is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ORIGAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Origam.Architect.Server.Models;
 using Origam.Architect.Server.ReturnModels;
@@ -153,7 +174,7 @@ public class EditorController(
         object data = treeNode.DefaultEditor switch
         {
             EditorSubType.GridEditor => propertyService.GetEditorProperties(item),
-            EditorSubType.XslTEditor => propertyService.GetEditorProperties(item),
+            EditorSubType.XsltEditor => propertyService.GetEditorProperties(item),
             EditorSubType.ScreenSectionEditor => sectionService
                 .GetSectionEditorData(item),
             EditorSubType.ScreenEditor => sectionService
