56a6dac1c31dd9ba7f645f35f41121c86c549e48
JindrichSusen <jindrich.susen@advantages.cz>
2025-08-19 14:44:02 +0200

    redundant variable removed

--- backend/Origam.Common/XmlTools.cs
+++ backend/Origam.Common/XmlTools.cs
@@ -205,9 +205,8 @@ public class XmlTools
         {
                 TimeSpan offset = TimeZoneInfo.Local.GetUtcOffset(date);
                 int hours = offset.Duration().Hours;
-                int finalHours = hours;
-                string sign = finalHours >= 0 ? "+" : "-";
-                string result = date.ToString("yyyy-MM-dd") + "T00:00:00.0000000" + sign + finalHours.ToString("00") + ":" + offset.Minutes.ToString("00");
+                string sign = hours >= 0 ? "+" : "-";
+                string result = date.ToString("yyyy-MM-dd") + "T00:00:00.0000000" + sign + hours.ToString("00") + ":" + offset.Minutes.ToString("00");
                 return result;
         }
         else
