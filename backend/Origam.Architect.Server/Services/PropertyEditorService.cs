2e8c2fb84107f38570cad3ff8e4f22d7b3a269d2
Hxn <1769554+hxn@users.noreply.github.com>
2025-08-21 13:15:26 +0200

    FIX: Improves XSLT editor property handling + missing tab label (#3435)
    Updates the XSLT editor to dynamically determine the correct property name ("TextStore" or "Xsl") based on available properties, ensuring proper data binding and filtering.

--- backend/Origam.Architect.Server/Services/PropertyEditorService.cs
+++ backend/Origam.Architect.Server/Services/PropertyEditorService.cs
@@ -66,7 +66,7 @@ public class PropertyEditorService(EditorPropertyFactory propertyFactory)
         {
             IEnumerable<EditorProperty> xsltProperties = GetEditorPropertiesByName(
                 xslRule,
-                new[] { "Xsl" }
+                ["Xsl", "Name"]
             );
             return xsltProperties;
         }
