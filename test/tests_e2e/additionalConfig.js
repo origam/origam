8e7c9f206b93135af5e2a9c892b27e7c62e73ae1
JindrichSusen <jindrich.susen@advantages.cz>
2025-08-27 17:00:55 +0200

    Revert "State test works"
    This reverts commit 1e2546918cdc3a2010c3fbeeb342dfe28e929128.

--- test/tests_e2e/additionalConfig.js
+++ test/tests_e2e/additionalConfig.js
@@ -1,13 +1,5 @@
-
 const userName = "washi";
 const password = "blabla";
-const backEndUrl = "https://localhost:44357";
+const backEndUrl = "https://localhost";
 
-const sqlDatabase = "origam-demo";
-const sqlServer = "localhost";
-const sqlPort = 1433;
-const sqlUser = undefined;
-const sqlPassword  = undefined;
-const sqlOptions  = {trustedConnection: true};
-
-module.exports = {userName, password, backEndUrl,sqlDatabase,sqlServer,sqlPort,sqlUser,sqlPassword,sqlOptions};
+module.exports = {userName, password, backEndUrl};
