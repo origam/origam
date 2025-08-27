1e2546918cdc3a2010c3fbeeb342dfe28e929128
JindrichSusen <jindrich.susen@advantages.cz>
2025-08-27 16:26:12 +0200

    State test works

--- test/tests_e2e/additionalConfig.js
+++ test/tests_e2e/additionalConfig.js
@@ -1,5 +1,13 @@
+
 const userName = "washi";
 const password = "blabla";
-const backEndUrl = "https://localhost";
+const backEndUrl = "https://localhost:44357";
 
-module.exports = {userName, password, backEndUrl};
+const sqlDatabase = "origam-demo";
+const sqlServer = "localhost";
+const sqlPort = 1433;
+const sqlUser = undefined;
+const sqlPassword  = undefined;
+const sqlOptions  = {trustedConnection: true};
+
+module.exports = {userName, password, backEndUrl,sqlDatabase,sqlServer,sqlPort,sqlUser,sqlPassword,sqlOptions};
