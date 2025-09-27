36802901ea63dd5955a5e27708c4699346c0cbd0
Pavel Dietrich <1769554+hxn@users.noreply.github.com>
2025-09-28 00:04:16 +0200

    Adds database permissions builder

---
+++ backend/Origam.Composer/Builders/ApplyDatabasePermissionsBuilder.cs
@@ -0,0 +1,77 @@
+﻿#region license
+/*
+Copyright 2005 - 2025 Advantage Solutions, s. r. o.
+
+This file is part of ORIGAM (http://www.origam.org).
+
+ORIGAM is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ORIGAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using Origam.Composer.DTOs;
+using Origam.Composer.Services;
+using Spectre.Console;
+using static Origam.DA.Common.Enums;
+
+namespace Origam.Composer.Builders;
+
+public class ApplyDatabasePermissionsBuilder : AbstractDatabaseBuilder
+{
+    public override string Name => "Apply database permissions";
+
+    private DatabaseType DatabaseType;
+    private string LoginName;
+    private bool IsIntegratedAuthentication;
+
+    public override void Execute(Project project)
+    {
+        AnsiConsole.MarkupLine($"[orange1][bold]Executing:[/][/] {Name}");
+
+        DatabaseType = project.DatabaseType;
+
+        DataService(DatabaseType).DbUser = project.Name;
+        LoginName = DataService(DatabaseType).DbUser;
+        IsIntegratedAuthentication = project.DatabaseIntegratedAuthentication;
+
+        DataService(DatabaseType).ConnectionString = BuildConnectionString(project);
+        DataService(DatabaseType)
+            .CreateDatabaseUser(
+                LoginName,
+                project.UserPassword,
+                project.DatabaseName,
+                project.DatabaseIntegratedAuthentication
+            );
+    }
+
+    private string BuildConnectionString(Project project)
+    {
+        var dataService = DataService(DatabaseType)
+            .BuildConnectionString(
+                project.DatabaseHost,
+                project.DatabasePort,
+                project.DatabaseName,
+                project.DatabaseUserName,
+                project.DatabasePassword,
+                project.DatabaseIntegratedAuthentication,
+                false
+            );
+
+        return dataService;
+    }
+
+    public override void Rollback()
+    {
+        DataService(DatabaseType).DeleteUser(LoginName, IsIntegratedAuthentication);
+    }
+}
