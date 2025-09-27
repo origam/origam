9bcce82bb69db587ea6ab32196005fce60a6c115
Pavel Dietrich <1769554+hxn@users.noreply.github.com>
2025-09-27 23:33:29 +0200

    Add base project files + commons + enums

---
+++ backend/Origam.Composer/OrigamTypeRegistrar.cs
@@ -0,0 +1,49 @@
+﻿#region license
+/*
+Copyright 2005 - 2025 Advantage Solutions, s. r. o.
+
+This file is part of ORIGAM (http://www.origam.org).
+
+ORIGAM is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ORIGAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using Microsoft.Extensions.DependencyInjection;
+using Spectre.Console.Cli;
+
+namespace Origam.Composer;
+
+public class OrigamTypeRegistrar(IServiceCollection services) : ITypeRegistrar
+{
+    public void Register(Type service, Type implementation)
+    {
+        services.AddSingleton(service, implementation);
+    }
+
+    public void RegisterInstance(Type service, object implementation)
+    {
+        services.AddSingleton(service, implementation);
+    }
+
+    public void RegisterLazy(Type service, Func<object> factory)
+    {
+        services.AddSingleton(service, provider => factory());
+    }
+
+    public ITypeResolver Build()
+    {
+        var provider = services.BuildServiceProvider();
+        return new OrigamTypeResolver(provider);
+    }
+}
---
+++ backend/Origam.Composer/Program.cs
@@ -0,0 +1,48 @@
+﻿#region license
+/*
+Copyright 2005 - 2025 Advantage Solutions, s. r. o.
+
+This file is part of ORIGAM (http://www.origam.org).
+
+ORIGAM is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ORIGAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using Microsoft.Extensions.DependencyInjection;
+using Origam.Composer.Commands;
+using Origam.Composer.Interfaces.Services;
+using Origam.Composer.Services;
+using Spectre.Console.Cli;
+
+namespace Origam.Composer;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton<IVisualService, VisualService>();
+        services.AddSingleton<IProjectStarterService, ProjectStarterService>();
+
+        var registrar = new OrigamTypeRegistrar(services);
+
+        var app = new CommandApp(registrar);
+        app.Configure(config =>
+        {
+            config.AddCommand<CreateCommand>("create");
+        });
+
+        await app.RunAsync(args);
+    }
+}
