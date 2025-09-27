b865d7ab0529d62f6ab2c5d3a4f6ae937db04f81
Pavel Dietrich <1769554+hxn@users.noreply.github.com>
2025-09-27 23:51:39 +0200

    Adds interfaces and abstract classes

---
+++ backend/Origam.Composer/Interfaces/Services/IProjectStarterService.cs
@@ -0,0 +1,12 @@
+﻿using Origam.Composer.DTOs;
+
+namespace Origam.Composer.Interfaces.Services;
+
+public interface IProjectStarterService
+{
+    void Create(Project project);
+
+    void CreateTasks(Project project);
+
+    List<IProjectBuilder> GetTasks();
+}
