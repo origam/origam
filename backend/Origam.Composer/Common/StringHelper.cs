06a8808b2a701813c4e2d7c473b97eed60d7753d
Pavel Dietrich <1769554+hxn@users.noreply.github.com>
2025-10-01 00:36:26 +0200

    Add StringHelper.RemoveAllWhitespace

---
+++ backend/Origam.Composer/Common/StringHelper.cs
@@ -0,0 +1,26 @@
+﻿namespace Origam.Composer.Common;
+
+using System.Text;
+
+public static class StringHelper
+{
+    public static string RemoveAllWhitespace(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return input;
+        }
+
+        var sb = new StringBuilder();
+
+        foreach (char c in input)
+        {
+            if (!char.IsWhiteSpace(c))
+            {
+                sb.Append(c);
+            }
+        }
+
+        return sb.ToString();
+    }
+}
