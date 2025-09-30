747190381808a7c8389254a7c399d3f792c595af
Pavel Dietrich <1769554+hxn@users.noreply.github.com>
2025-09-30 18:04:21 +0200

    Makes constants readonly
    Changes `static` fields to `const` to ensure immutability and improve code clarity.
    This prevents accidental modification of these values during runtime.

--- backend/Origam.Composer/Common/Constants.cs
+++ backend/Origam.Composer/Common/Constants.cs
@@ -23,10 +23,10 @@ namespace Origam.Composer.Common;
 
 public static class Constants
 {
-    public static int DefaultHttpsPort = 443;
+    public const int DefaultHttpsPort = 443;
 
     /// <summary>
     /// Super User (Built-in)
     /// </summary>
-    public static string OrigamRoleSuperUserId = "E0AD1A0B-3E05-4B97-BE38-12FF63E7F2F2";
+    public const string OrigamRoleSuperUserId = "E0AD1A0B-3E05-4B97-BE38-12FF63E7F2F2";
 }
