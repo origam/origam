36aaf3a27074886a4b7f1ad322ce885163565eb3
JindrichSusen <jindrich.susen@advantages.cz>
2025-10-07 19:43:30 +0200

    Code rules enforces in ScheduleTimer

--- backend/ScheduleTimer/ScheduledItems/EventInstance.cs
+++ backend/ScheduleTimer/ScheduledItems/EventInstance.cs
@@ -1,6 +1,6 @@
 /***************************************************************************
  * Copyright Andy Brummer 2004-2005
- * 
+ *
  * This code is provided "as is", with absolutely no warranty expressed
  * or implied. Any use is at your own risk.
  *
@@ -10,12 +10,13 @@
  * providing that this notice and the authors name is included. If
  * the source code in  this file is used in any commercial application
  * then a simple email would be nice.
- * 
+ *
  **************************************************************************/
 
 using System;
 
 namespace Schedule;
+
 /// <summary>
 /// There have been quite a few requests to allow scheduling of multiple delegates and method parameter data
 /// from the same timer.  This class allows you to match the event with the time that it fired.  I want to keep
@@ -24,21 +25,23 @@ namespace Schedule;
 /// </summary>
 public class EventInstance : IComparable
 {
-	public EventInstance(DateTime time, IScheduledItem scheduleItem, object data)
-	{
-		Time = time;
-		ScheduleItem = scheduleItem;
-		Data = data;
-	}
-	public DateTime Time;
-	public IScheduledItem ScheduleItem;
-	public object Data;
-	public int CompareTo(object obj)
-	{
-		if (obj is EventInstance)
-			return Time.CompareTo(((EventInstance)obj).Time);
-		if (obj is DateTime)
-			return Time.CompareTo((DateTime)obj);
-		return 0;
-	}
+    public EventInstance(DateTime time, IScheduledItem scheduleItem, object data)
+    {
+        Time = time;
+        ScheduleItem = scheduleItem;
+        Data = data;
+    }
+
+    public DateTime Time;
+    public IScheduledItem ScheduleItem;
+    public object Data;
+
+    public int CompareTo(object obj)
+    {
+        if (obj is EventInstance)
+            return Time.CompareTo(((EventInstance)obj).Time);
+        if (obj is DateTime)
+            return Time.CompareTo((DateTime)obj);
+        return 0;
+    }
 }
--- backend/ScheduleTimer/ScheduledItems/EventQueue.cs
+++ backend/ScheduleTimer/ScheduledItems/EventQueue.cs
@@ -1,6 +1,6 @@
 /***************************************************************************
  * Copyright Andy Brummer 2004-2005
- * 
+ *
  * This code is provided "as is", with absolutely no warranty expressed
  * or implied. Any use is at your own risk.
  *
@@ -10,7 +10,7 @@
  * providing that this notice and the authors name is included. If
  * the source code in  this file is used in any commercial application
  * then a simple email would be nice.
- * 
+ *
  **************************************************************************/
 
 using System;
@@ -18,6 +18,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 namespace Schedule;
+
 /// <summary>
 /// The event queue is a collection of scheduled items that represents the union of all child scheduled items.
 /// This is useful for events that occur every 10 minutes or at multiple intervals not covered by the simple
@@ -25,53 +26,58 @@ namespace Schedule;
 /// </summary>
 public class EventQueue : IScheduledItem
 {
-	public EventQueue()
-	{
-		_List = new List<IScheduledItem>();
-	}
-	/// <summary>
-	/// Adds a ScheduledTime to the queue.
-	/// </summary>
-	/// <param name="time">The scheduled time to add</param>
-	public void Add(IScheduledItem time)
-	{
-		_List.Add(time);
-	}
-	/// <summary>
-	/// Clears the list of scheduled times.
-	/// </summary>
-	public void Clear()
-	{
-		_List.Clear();
-	}
-	/// <summary>
-	/// Adds the running time for all events in the list.
-	/// </summary>
-	/// <param name="Begin">The beginning time of the interval</param>
-	/// <param name="End">The end time of the interval</param>
-	/// <param name="List">The list to add times to.</param>
-	public void AddEventsInInterval(DateTime Begin, DateTime End, List<DateTime> List)
-	{
-		foreach(IScheduledItem st in _List)
-			st.AddEventsInInterval(Begin, End, List);
-		List.Sort();
-	}
-	/// <summary>
-	/// Returns the first time after the starting time for all events in the list.
-	/// </summary>
-	/// <param name="time">The starting time.</param>
-	/// <param name="AllowExact">If this is true then it allows the return time to match the time parameter, false forces the return time to be greater then the time parameter</param>
-	/// <returns>Either the next event after the input time or greater or equal to depending on the AllowExact parameter.</returns>
-	public DateTime NextRunTime(DateTime time, bool AllowExact)
-	{
-		DateTime next = DateTime.MaxValue;
-		//Get minimum datetime from the list.
-		foreach(IScheduledItem st in _List)
-		{
-			DateTime Proposed = st.NextRunTime(time, AllowExact);
-			next = (Proposed < next) ? Proposed : next;
-		}
-		return next;
-	}
-	private List<IScheduledItem> _List;
-}	 
+    public EventQueue()
+    {
+        _List = new List<IScheduledItem>();
+    }
+
+    /// <summary>
+    /// Adds a ScheduledTime to the queue.
+    /// </summary>
+    /// <param name="time">The scheduled time to add</param>
+    public void Add(IScheduledItem time)
+    {
+        _List.Add(time);
+    }
+
+    /// <summary>
+    /// Clears the list of scheduled times.
+    /// </summary>
+    public void Clear()
+    {
+        _List.Clear();
+    }
+
+    /// <summary>
+    /// Adds the running time for all events in the list.
+    /// </summary>
+    /// <param name="Begin">The beginning time of the interval</param>
+    /// <param name="End">The end time of the interval</param>
+    /// <param name="List">The list to add times to.</param>
+    public void AddEventsInInterval(DateTime Begin, DateTime End, List<DateTime> List)
+    {
+        foreach (IScheduledItem st in _List)
+            st.AddEventsInInterval(Begin, End, List);
+        List.Sort();
+    }
+
+    /// <summary>
+    /// Returns the first time after the starting time for all events in the list.
+    /// </summary>
+    /// <param name="time">The starting time.</param>
+    /// <param name="AllowExact">If this is true then it allows the return time to match the time parameter, false forces the return time to be greater then the time parameter</param>
+    /// <returns>Either the next event after the input time or greater or equal to depending on the AllowExact parameter.</returns>
+    public DateTime NextRunTime(DateTime time, bool AllowExact)
+    {
+        DateTime next = DateTime.MaxValue;
+        //Get minimum datetime from the list.
+        foreach (IScheduledItem st in _List)
+        {
+            DateTime Proposed = st.NextRunTime(time, AllowExact);
+            next = (Proposed < next) ? Proposed : next;
+        }
+        return next;
+    }
+
+    private List<IScheduledItem> _List;
+}
