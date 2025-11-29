a6f43a72db5a5bda8f359d5378bff222e99e0272
Pavel Dietrich <1769554+hxn@users.noreply.github.com>
2025-11-29 22:01:56 +0100

    Add generator editor

---
+++ architect-html/src/components/editors/DeploymentScriptsGeneratorEditor/DeploymentScriptsGeneratorEditor.tsx
@@ -0,0 +1,106 @@
+/*
+Copyright 2005 - 2025 Advantage Solutions, s. r. o.
+
+This file is part of ORIGAM (http://www.origam.org).
+
+ORIGAM is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ORIGAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import ActionPanelV2 from '@/components/ActionPanelV2/ActionPanelV2';
+import Button from '@/components/Button/Button';
+import { TabView } from '@/components/tabView/TabView';
+import { TabViewState } from '@/components/tabView/TabViewState';
+import { T } from '@/main';
+import S from '@editors/DeploymentScriptsGeneratorEditor/DeploymentScriptsGeneratorEditor.module.scss';
+import DeploymentScriptsGeneratorEditorState from '@editors/DeploymentScriptsGeneratorEditor/DeploymentScriptsGeneratorEditorState';
+
+const DeploymentScriptsGeneratorEditor = ({
+  editorState,
+}: {
+  editorState: DeploymentScriptsGeneratorEditorState;
+}) => {
+  return (
+    <div className={S.root}>
+      <TabView
+        width={400}
+        state={new TabViewState()}
+        items={[
+          {
+            label: T('Deployment scripts', 'editor_DeploymentScriptsGenerator_TabLabel_List'),
+            node: (
+              <div className={S.editorBox}>
+                <ActionPanelV2
+                  title={T(
+                    'Deployment scripts: ({0})',
+                    'editor_DeploymentScriptsGenerator_ActionPanelTitle_List',
+                    editorState.results.length,
+                  )}
+                  buttons={
+                    <Button
+                      title="Add to Deployment"
+                      type="primary"
+                      isDisabled
+                      onClick={function (): void {
+                        throw new Error('Function not implemented.');
+                      }}
+                    />
+                  }
+                />
+                <div className={S.tableWrapper}>
+                  <table className={S.resultsTable}>
+                    <thead>
+                      <tr>
+                        <th>{T('Result', 'editor_DeploymentScriptsGenerator_Column_Result')}</th>
+                        <th>
+                          {T('Platform', 'editor_DeploymentScriptsGenerator_Column_Platform')}
+                        </th>
+                        <th>
+                          {T('Item Name', 'editor_DeploymentScriptsGenerator_Column_ItemName')}
+                        </th>
+                        <th>{T('Type', 'editor_DeploymentScriptsGenerator_Column_Type')}</th>
+                        <th>{T('Script', 'editor_DeploymentScriptsGenerator_Column_Script')}</th>
+                        <th>{T('Script2', 'editor_DeploymentScriptsGenerator_Column_Script2')}</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {editorState.results.map((x, index) => (
+                        <tr key={x.schemaItemId || index}>
+                          <td>{x.resultType}</td>
+                          <td>{x.platformName}</td>
+                          <td>
+                            <strong>{x.itemName}</strong>
+                          </td>
+                          <td>{x.schemaItemType}</td>
+
+                          <td className={S.truncate} title={x.script}>
+                            {x.script}
+                          </td>
+                          <td className={S.truncate} title={x.script2}>
+                            {x.script2}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            ),
+          },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default DeploymentScriptsGeneratorEditor;
