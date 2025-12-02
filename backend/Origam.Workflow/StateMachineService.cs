4fbfb936c920ba64755d9e3662da4bb4b6252cb4
Václav Urbánek <vaclav.urbanek@advantages.cz>
2025-12-02 14:52:42 +0100

    FIX: Fix processing of state machine work queue hooks on workqueues with Queue Processor set
    (cherry picked from commit 4c58f9cc82b49e630013338712c24815798cf015)

--- backend/Origam.Workflow/StateMachineService.cs
+++ backend/Origam.Workflow/StateMachineService.cs
@@ -943,6 +943,7 @@ public class StateMachineService : AbstractService, IStateMachineService
         if (_WorkQueueLastRefreshed.AddMinutes(1).CompareTo(DateTime.Now) < 0)
         {
             _WorkQueueCache = (WQService as WorkQueueService).GetQueues(
+                ignoreQueueProcessors: true,
                 transactionId: transactionId);
             _WorkQueueLastRefreshed = DateTime.Now;
         }
