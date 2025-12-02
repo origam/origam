b83e824372c5c246d955e72e78fd4fc959f278cc
Pavel Dietrich <1769554+hxn@users.noreply.github.com>
2025-12-02 16:59:34 +0100

    Fix: Editor import location
    Moves the import statements for `DeploymentScriptsGeneratorEditor` and `DeploymentScriptsGeneratorEditorState` to the top of the file to resolve import errors.

--- architect-html/src/components/editors/GetEditor.tsx
+++ architect-html/src/components/editors/GetEditor.tsx
@@ -31,6 +31,8 @@ import { IEditorState } from '@components/editorTabView/IEditorState';
 import { EditorData } from '@components/modelTree/EditorData';
 import { PropertiesState } from '@components/properties/PropertiesState';
 import DeploymentScriptsEditor from '@editors/DeploymentScriptsEditor/DeploymentScriptsEditor';
+import DeploymentScriptsGeneratorEditor from '@editors/DeploymentScriptsGeneratorEditor/DeploymentScriptsGeneratorEditor';
+import DeploymentScriptsGeneratorEditorState from '@editors/DeploymentScriptsGeneratorEditor/DeploymentScriptsGeneratorEditorState';
 import ScreenEditor from '@editors/designerEditor/screenEditor/ScreenEditor';
 import { ScreenEditorState } from '@editors/designerEditor/screenEditor/ScreenEditorState';
 import { ScreenToolboxState } from '@editors/designerEditor/screenEditor/ScreenToolboxState';
@@ -45,8 +47,6 @@ import XsltEditor from '@editors/xsltEditor/XsltEditor';
 import { FlowHandlerInput } from '@errors/runInFlowWithHandler';
 import { CancellablePromise } from 'mobx/dist/api/flow';
 import React from 'react';
-import DeploymentScriptsGeneratorEditor from './DeploymentScriptsGeneratorEditor/DeploymentScriptsGeneratorEditor';
-import DeploymentScriptsGeneratorEditorState from './DeploymentScriptsGeneratorEditor/DeploymentScriptsGeneratorEditorState';
 
 export function getEditor(args: {
   editorType: EditorType;
