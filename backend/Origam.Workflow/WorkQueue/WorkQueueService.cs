08f9289e6d8dd72f4d31d683b2aebd2831fb5a64
Václav Urbánek <vaclav.urbanek@centrum.cz>
2025-12-03 21:56:37 +0100

    INTERNAL: coding style

--- backend/Origam.Workflow/WorkQueue/WorkQueueService.cs
+++ backend/Origam.Workflow/WorkQueue/WorkQueueService.cs
@@ -61,7 +61,7 @@ public class WorkQueueService : IWorkQueueService, IBackgroundService
     private readonly Timer _loadExternalWorkQueuesTimer = new(60000);
     private readonly Timer _queueAutoProcessTimer;
     private Boolean serviceBeingUnloaded = false;
-    private readonly RetryManager retryManager = new ();
+    private readonly RetryManager retryManager = new();
     private static readonly Guid DS_METHOD_WQ_GETACTIVEQUEUES = new Guid(
         "0b45c721-65d2-4305-b34a-cd0d07387ea1");
     private static readonly Guid DS_METHOD_WQ_GETACTIVEQUEUESBYPROCESSOR = new Guid(
