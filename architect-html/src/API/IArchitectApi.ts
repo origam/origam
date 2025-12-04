dd77f2469d2eeb8223907fb24a6ca0e4c7156924
Pavel Dietrich <1769554+hxn@users.noreply.github.com>
2025-12-04 11:41:48 +0100

    Add openDeploymentScriptsGeneratorEditor

--- architect-html/src/API/IArchitectApi.ts
+++ architect-html/src/API/IArchitectApi.ts
@@ -108,7 +108,14 @@ export interface IArchitectApi {
 }
 
 export interface IDatabaseResultResponse {
+  deploymentVersions: IDeploymentVersion[];
+  currentDeploymentVersionId: string | null;
   results: IDatabaseResult[];
+}
+
+export interface IDeploymentVersion {
+  id: string;
+  name: string;
 }
 
 export interface IDatabaseResult {
@@ -265,6 +272,8 @@ export interface IPackage {
 export type PropertyType = 'boolean' | 'enum' | 'string' | 'integer' | 'float' | 'looukup';
 
 export interface IDeploymentScriptsGeneratorEditorData {
+  possibleDeploymentVersions: IDeploymentVersion[];
+  currentDeploymentVersionId: string | null;
   results: IDatabaseResult[];
 }
 
