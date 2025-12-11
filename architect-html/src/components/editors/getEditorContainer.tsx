05342bc36adbdbfec7135fb41aedf80b9a32ba96
Pavel Dietrich <1769554+hxn@users.noreply.github.com>
2025-12-11 19:18:34 +0100

    Fix imports

--- architect-html/src/components/editors/getEditorContainer.tsx
+++ architect-html/src/components/editors/getEditorContainer.tsx
@@ -31,6 +31,8 @@ import { EditorData } from '@components/modelTree/EditorData';
 import { ModelTreeState } from '@components/modelTree/ModelTreeState';
 import { PropertiesState } from '@components/properties/PropertiesState';
 import DeploymentScriptsEditor from '@editors/DeploymentScriptsEditor/DeploymentScriptsEditor';
+import DeploymentScriptsGeneratorEditor from '@editors/DeploymentScriptsGeneratorEditor/DeploymentScriptsGeneratorEditor';
+import DeploymentScriptsGeneratorEditorState from '@editors/DeploymentScriptsGeneratorEditor/DeploymentScriptsGeneratorEditorState';
 import ScreenEditor from '@editors/designerEditor/screenEditor/ScreenEditor';
 import { ScreenEditorState } from '@editors/designerEditor/screenEditor/ScreenEditorState';
 import { ScreenToolboxState } from '@editors/designerEditor/screenEditor/ScreenToolboxState';
@@ -46,8 +48,6 @@ import { XsltEditorState } from '@editors/gridEditor/XsltEditorState.ts';
 import XsltEditor from '@editors/xsltEditor/XsltEditor';
 import { FlowHandlerInput } from '@errors/runInFlowWithHandler';
 import { CancellablePromise } from 'mobx/dist/api/flow';
-import DeploymentScriptsGeneratorEditor from './DeploymentScriptsGeneratorEditor/DeploymentScriptsGeneratorEditor';
-import DeploymentScriptsGeneratorEditorState from './DeploymentScriptsGeneratorEditor/DeploymentScriptsGeneratorEditorState';
 
 export function getEditorContainer(args: {
   editorType: EditorType;
