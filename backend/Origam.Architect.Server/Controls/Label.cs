3416d703066eee0cbac7e761c2e319dba7177996
JindrichSusen <jindrich.susen@advantages.cz>
2025-12-23 17:15:49 +0100

    Backedn implementation of missing Controls added
    frontend implementations still has to be added

---
+++ backend/Origam.Architect.Server/Controls/Label.cs
@@ -0,0 +1,53 @@
+#region license
+/*
+Copyright 2005 - 2025 Advantage Solutions, s. r. o.
+
+This file is part of ORIGAM (http://www.origam.org).
+
+ORIGAM is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ORIGAM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using System.ComponentModel;
+using Origam.Schema.GuiModel;
+
+namespace Origam.Architect.Server.Controls;
+
+public class Label : IControl
+{
+    [Browsable(false)]
+    public int Left { get; set; }
+
+    [Browsable(false)]
+    public int Width { get; set; }
+
+    [Browsable(false)]
+    public int Height { get; set; }
+
+    [Localizable(true)]
+    [MergableProperty(false)]
+    public int TabIndex { get; set; }
+
+    [Browsable(false)]
+    public int Top { get; set; }
+
+    [Editor(
+        "System.Drawing.Design.UITypeEditor, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a",
+        "System.ComponentModel.Design.MultilineStringEditor, System.Design, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
+    )]
+    [SettingsBindable(true)]
+    public string Text { get; set; }
+
+    public void Initialize(ControlSetItem controlSetItem) { }
+}
