c8fcb92025ad8e2d9c00e978f0543d3039b4d842
tvavrda <tvavrda@gmail.com>
2025-12-27 15:07:08 +0100

    DEV: Fixed code formatting

--- backend/Origam.Gui.Win/Commands/ShowDataStructureSql.cs
+++ backend/Origam.Gui.Win/Commands/ShowDataStructureSql.cs
@@ -21,8 +21,6 @@ along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
 
 using System.Collections;
 using System.Text;
-
-
 namespace Origam.Gui.Win.Commands;
 
 public class ShowDataStructureSql : AbstractMenuCommand
