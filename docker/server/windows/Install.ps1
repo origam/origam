94fcde1ca198f72f5a86923352fa4ffc849fa38d
Petr Hrehorovsky <83349812+washibana@users.noreply.github.com>
2026-01-28 12:43:36 +0100

    BUILD: Openssl for windows docker updated to 3.5.4

--- docker/server/windows/Install.ps1
+++ docker/server/windows/Install.ps1
@@ -28,7 +28,7 @@ try {
 
 # Install portable OpenSSL
 Write-Host "Installing portable OpenSSL..."
-$opensslUrl = "https://www.firedaemon.com/download-firedaemon-openssl-3-5-zip"
+$opensslUrl = "https://download.firedaemon.com/FireDaemon-OpenSSL/openssl-3.5.4.zip"
 $opensslZip = "$env:TEMP\openssl.zip"
 $opensslPath = "$installDir\openssl-3\x64\bin"
 $opensslInstallFolder = "$installDir\openssl-3"
