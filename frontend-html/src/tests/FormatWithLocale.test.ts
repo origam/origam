c44a0205ea5b2a70a2f5d76956cfcc2003b275ed
JindrichSusen <jindrich.susen@advantages.cz>
2026-03-26 09:49:33 +0100

    Test fix

--- frontend-html/src/tests/FormatWithLocale.test.ts
+++ frontend-html/src/tests/FormatWithLocale.test.ts
@@ -25,10 +25,11 @@ test.each([
   ["#.000", 123456.7, "de-CH", "123456.700"],
   ["#.###", 123456.7890123, "de-CH", "123456.789"],
   ["#.###", 123456.7, "de-CH", "123456.7"],
-  ["### ###.###", 123456.7, "de-CH", "123’456.7"],
+  ["### ###.###", 123456.7, "de-CH", "123[''’]456.7"],
   ["######.###", 123456.7, "de-CH", "123456.7"],
   ["######.00#", 123456.7, "de-CH", "123456.70"],
 ])('Format %s to: %s', (customNumericFormat: string, value: number, locale: string, expected: string) => {
   const formattedValue = formatNumberWithLocale(customNumericFormat, value, locale)
-  expect(formattedValue).toBe(expected);
+  expect(formattedValue).toMatch(new RegExp(`^${expected}$`));
 });
+
