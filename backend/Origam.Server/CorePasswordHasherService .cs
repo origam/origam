fc572c274f7d7ca7f6b42b3edd3e1e6a671d9230
jakub.hruby <jakub.hruby@synergine.cz>
2026-04-20 09:56:10 +0200

    #51 Přidán CorePasswordHasherService.

--- backend/Origam.Server/CorePasswordHasherService .cs
+++ backend/Origam.Server/CorePasswordHasherService .cs
@@ -2,6 +2,7 @@
 using System.Globalization;
 using System.Security.Cryptography;
 using BrockAllen.IdentityReboot;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Identity;
 using Origam.Security.Common;
 
@@ -9,10 +10,13 @@ namespace Origam.Server
 {
     public class CorePasswordHasherService : IPasswordHasher<IOrigamUser>
     {
-        private const int SALT_SIZE     = 16; // 128 bit
+        private const int SALT_SIZE = 16; // 128 bit
         private const int SUBKEY_LENGTH = 32; // 256 bit
+        private const int ITERATION_COUNT = 250000;
+        private const string KEY_PREFIX = "$pbkdf2-sha256$";
 
         private IPasswordHasher<IOrigamUser> legacyPasswordHasher;
+
         public CorePasswordHasherService()
         {
             legacyPasswordHasher = new CorePasswordHasher();
@@ -27,7 +31,52 @@ namespace Origam.Server
             if (!isKnownFunction(hashedPassword))
             {
                 // Old password format, using legacy hasher
-                return legacyPasswordHasher.VerifyHashedPassword(user, hashedPassword, providedPassword);
+                return legacyPasswordHasher.VerifyHashedPassword(
+                    user,
+                    hashedPassword,
+                    providedPassword
+                );
+            }
+
+            var parts = hashedPassword.Split(".");
+            int count;
+            Int32.TryParse(parts[0], NumberStyles.HexNumber, null, out count);
+            hashedPassword = parts[1];
+            byte[] hashedPasswordBytes = Convert.FromBase64String(hashedPassword);
+            byte[] salt = new byte[SALT_SIZE];
+            Buffer.BlockCopy(hashedPasswordBytes, 1, salt, 0, SALT_SIZE);
+            byte[] storedSubkey = new byte[SUBKEY_LENGTH];
+            Buffer.BlockCopy(hashedPasswordBytes, 1 + SALT_SIZE, storedSubkey, 0, SUBKEY_LENGTH);
+            byte[] generatedSubkey;
+
+            generatedSubkey = Rfc2898DeriveBytes.Pbkdf2(
+                providedPassword,
+                salt,
+                count,
+                HashAlgorithmName.SHA256,
+                SUBKEY_LENGTH
+            );
+            var verificationResult =
+                (ByteArraysEqual(storedSubkey, generatedSubkey))
+                    ? VerificationResult.Success
+                    : VerificationResult.Failed;
+            return ToAspNetCoreResult(verificationResult);
+        }
+
+        public PasswordVerificationResult VerifyHashedPasswordA(
+            IOrigamUser user,
+            string hashedPassword,
+            string providedPassword
+        )
+        {
+            if (!isKnownFunction(hashedPassword))
+            {
+                // Old password format, using legacy hasher
+                return legacyPasswordHasher.VerifyHashedPassword(
+                    user,
+                    hashedPassword,
+                    providedPassword
+                );
             }
 
             var parts = hashedPassword.Split(".");
@@ -57,48 +106,30 @@ namespace Origam.Server
 
         public string HashPassword(IOrigamUser user, string password)
         {
-            int iterationCount = 250000;
-
             if (password == null)
             {
-                throw new ArgumentNullException("password");
+                throw new ArgumentNullException(nameof(password));
             }
 
-            byte[] salt = RandomNumberGenerator.GetBytes(SALT_SIZE);
-            byte[] subkey = Rfc2898DeriveBytes.Pbkdf2(
+            byte[] salt = new byte[SALT_SIZE];
+            RandomNumberGenerator.Fill(salt);
+            byte[] subkey = KeyDerivation.Pbkdf2(
                 password,
                 salt,
-                iterationCount,
-                HashAlgorithmName.SHA256,
+                KeyDerivationPrf.HMACSHA256,
+                ITERATION_COUNT,
                 SUBKEY_LENGTH
             );
-
-            using (var rng = RandomNumberGenerator.Create())
-            {
-                rng.GetBytes(salt);
-            }
-
-            var derivedBytes = Microsoft.AspNetCore.Cryptography.KeyDerivation.KeyDerivation.Pbkdf2(
-                password,
-                salt,
-                Microsoft.AspNetCore.Cryptography.KeyDerivation.KeyDerivationPrf.HMACSHA512,
-                iterationCount,
-                SUBKEY_LENGTH
-            );
-
-            // [ver][salt][subkey]
-            byte[] outputBytes = new byte[1 + SALT_SIZE + SUBKEY_LENGTH];
-            outputBytes[0] = 0x01; // version marker JE TO POTREBA?????
-
-            Buffer.BlockCopy(salt, 0, outputBytes, 1, SALT_SIZE);
-            Buffer.BlockCopy(subkey, 0, outputBytes, 1 + SALT_SIZE, SUBKEY_LENGTH);
-
-            return Convert.ToBase64String(outputBytes);
+            // [header].[salt][subkey]
+            byte[] outputBytes = new byte[1 + salt.Length + subkey.Length];
+            Buffer.BlockCopy(salt, 0, outputBytes, 1, salt.Length);
+            Buffer.BlockCopy(subkey, 0, outputBytes, 1 + salt.Length, subkey.Length);
+            return $"{KEY_PREFIX}.{Convert.ToBase64String(outputBytes)}";
         }
 
         public bool isKnownFunction(string password)
         {
-            return password.StartsWith("$pbkdf2-sha5");
+            return password.StartsWith(KEY_PREFIX);
         }
 
         // CpyPaste from CorePasswordHasher.cs
