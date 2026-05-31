5666b0986c9a0006a419b7368d396f2812dae4b7
tvavrda <tvavrda@gmail.com>
2026-05-31 17:10:31 +0200

    DEV: Fixed license header

--- backend/Origam.Schema.GuiModel/Reports/IDataReport.cs
+++ backend/Origam.Schema.GuiModel/Reports/IDataReport.cs
@@ -1,5 +1,6 @@
-﻿/*
-Copyright 2005 - 2026 Advantage Solutions, s. r. o.
+﻿#region license
+/*
+Copyright 2005 - 2025 Advantage Solutions, s. r. o.
 
 This file is part of ORIGAM (http://www.origam.org).
 
@@ -16,6 +17,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
 */
+#endregion
 using Origam.Schema.EntityModel;
 
 namespace Origam.Schema.GuiModel
