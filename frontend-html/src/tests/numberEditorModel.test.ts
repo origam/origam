195313aad411b405b5b045621c0b15d123eca2a3
Jindrich Susen <jindrich.susen@advantages.cz>
2026-07-07 21:38:17 +0200

    Redundant test removed

--- frontend-html/src/tests/numberEditorModel.test.ts
+++
@@ -1,5 +0,0 @@
-import { createNumberEditorModel } from "gui/Components/ScreenElements/Editors/NumberEditorModel";
-
-test("loads number editor model decorators without redecorating overrides", () => {
-  expect(createNumberEditorModel).toBeDefined();
-});
