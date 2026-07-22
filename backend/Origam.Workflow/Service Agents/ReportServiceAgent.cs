922bedebb301355c557b07ee940bcf480071f550
Tomas Vavrda <tomas.vavrda@advantages.cz>
2026-07-22 19:14:44 +0200

    FEATURE: XSL-FO based PDF generator ReportService implementation (#4414)
    NEW: XSL-FO based PDF generator ReportService implementation, including new metamodel element. Requires https://github.com/origam/xsl-fo-server running

--- backend/Origam.Workflow/Service Agents/ReportServiceAgent.cs
+++ backend/Origam.Workflow/Service Agents/ReportServiceAgent.cs
@@ -71,6 +71,7 @@ public class ReportServiceAgent : AbstractServiceAgent
             ExcelReport => "Origam.BI.Excel.ExcelService,Origam.BI.Excel",
             SSRSReport => "Origam.BI.SSRS.SSRSService,Origam.BI.SSRS",
             FastReport => "Origam.BI.FastReport.FastReportService,Origam.BI.FastReport",
+            XslFOReport => "Origam.BI.XslFO.XslFOReportService,Origam.BI.XslFO",
             _ => throw new ArgumentOutOfRangeException(
                 nameof(report),
                 report,
