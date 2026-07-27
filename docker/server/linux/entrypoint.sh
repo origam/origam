48555f40a9faf1b0167336a552922dcd290757d3
tvavrda <tvavrda@gmail.com>
2026-07-27 17:13:12 +0200

    Simplify nginx-free server startup

--- docker/server/linux/entrypoint.sh
+++ docker/server/linux/entrypoint.sh
@@ -11,26 +11,21 @@ fi
 
 container_mode="${ContainerMode:-server}"
 
-if [ "$container_mode" != "server-direct" ]; then
-  sudo /root/updateTimezone.sh
-fi
 cd /home/origam/Setup
 ./cleanUpEnvironment.sh
-if [ "$container_mode" != "server-direct" ]; then
+
+if [ "${SKIP_NGINX:-false}" = "false" ]; then
+  sudo /root/updateTimezone.sh
   sudo ./cleanUpEnvironmentRoot.sh
 fi
 
 if [ "$container_mode" = "server" ]; then
-  cd /etc/nginx/ssl
-  sudo /etc/nginx/ssl/createSslCertificate.sh
-  sudo /etc/init.d/nginx start
+  if [ "${SKIP_NGINX:-false}" = "false" ]; then
+    cd /etc/nginx/ssl
+    sudo /etc/nginx/ssl/createSslCertificate.sh
+    sudo /etc/init.d/nginx start
+  fi
   cd /home/origam/server_bin
-  ./configureServer.sh
-  export ASPNETCORE_URLS="http://+:8080"
-  exec dotnet Origam.Server.dll
-elif [ "$container_mode" = "server-direct" ]; then
-  cd /home/origam/server_bin
-  export ORIGAM_SKIP_NGINX=true
   ./configureServer.sh
   export ASPNETCORE_URLS="http://+:8080"
   exec dotnet Origam.Server.dll
