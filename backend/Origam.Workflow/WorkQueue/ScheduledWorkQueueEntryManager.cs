46ded5a70ad6ddb1b4ee6fd28ba174387f06c24e
Petr Hrehorovsky <washi@volny.cz>
2026-08-31 16:55:30 +0200

    Fixed license position.

--- backend/Origam.Workflow/WorkQueue/ScheduledWorkQueueEntryManager.cs
+++ backend/Origam.Workflow/WorkQueue/ScheduledWorkQueueEntryManager.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Data;
-
-namespace Origam.Workflow.WorkQueue;
-
+#region license
 /*
-Copyright 2005 - 2026 Advantage Solutions, s. r. o.
+Copyright 2005 - 2025 Advantage Solutions, s. r. o.
 
 This file is part of ORIGAM (http://www.origam.org).
 
@@ -21,6 +17,12 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with ORIGAM. If not, see <http://www.gnu.org/licenses/>.
 */
+#endregion
+
+using System;
+using System.Data;
+
+namespace Origam.Workflow.WorkQueue;
 
 public class ScheduledWorkQueueEntryManager
 {
